f5e0cc982e39fc037323eafa228edf75da1ea1c3
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2005-11-21 22:06:47 +0000

    Reinserted optional normals (now working)

--- src/meshlab/meshmodel.h
+++ src/meshlab/meshmodel.h
@@ -23,9 +23,11 @@
 /****************************************************************************
   History
 $Log$
+Revision 1.6  2005/11/21 22:06:47  cignoni
+Reinserted optional normals (now working)
+
 Revision 1.5  2005/11/21 12:09:33  cignoni
 Added copyright info
-
 ****************************************************************************/
 
 #include <stdio.h>
@@ -58,7 +60,7 @@ class CVertexO;
 // Opt stuff
 
 class CVertexO  : public VertexSimp2< CVertexO, CEdge, CFaceO, vert::Coord3f, vert::Normal3f, vert::BitFlags >{};
-class CFaceO    : public FaceSimp2< CVertexO, CEdge, CFaceO, face::InfoOcf, face::FFAdjOcf, face::WedgeTexture2f, face::VertexRef, face::BitFlags, face::Normal3f > {};
+class CFaceO    : public FaceSimp2< CVertexO, CEdge, CFaceO, face::InfoOcf, face::FFAdjOcf, face::WedgeTexture2f, face::VertexRef, face::BitFlags, face::Normal3fOcf > {};
 class CMeshO    : public vcg::tri::TriMesh< vector<CVertexO>, face::vector_ocf<CFaceO> > {};
 
 /*
