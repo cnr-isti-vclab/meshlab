dcc9172eb84b255224084b5b66debd3bec8720cd
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2005-11-23 15:57:20 +0000

    changed vertex to vertexplus and face to faceplus for MyMesh

--- src/test/loop/trimesh_smooth.cpp
+++ src/test/loop/trimesh_smooth.cpp
@@ -1,32 +1,43 @@
 
-	
-// mesh definition 
-#include <vcg/simplex/vertex/with/afvn.h>
-#include <vcg/simplex/face/with/afav.h>
-#include <vcg/complex/trimesh/base.h>
-
-#include <vcg/complex/trimesh/update/topology.h>
-#include <vcg/complex/trimesh/update/normal.h>
-#include <vcg/complex/trimesh/update/flag.h>
-
 // new_refine just includes refine.h
 #include "new_refine.h"
-
-// input output
-#include <wrap/io_trimesh/import_ply.h>
-#include <wrap/io_trimesh/export_ply.h>
 
 // std
 #include <vector>
 
+#include<vcg/simplex/vertexplus/base.h>
+#include<vcg/simplex/faceplus/base.h>
+#include<vcg/simplex/face/topology.h>
+
+#include<vcg/simplex/vertexplus/component_ocf.h>
+#include<vcg/simplex/faceplus/component_ocf.h>
+#include<vcg/complex/trimesh/base.h>
+#include<vcg/complex/trimesh/create/platonic.h>
+#include<vcg/complex/trimesh/update/topology.h>
+#include<vcg/complex/trimesh/update/flag.h>
+#include<vcg/complex/trimesh/update/normal.h>
+#include <vcg/complex/trimesh/refine.h>
+
+#include <wrap/io_trimesh/import_ply.h>
+#include <wrap/io_trimesh/export_ply.h>
+#include <wrap/gl/trimesh.h>
 using namespace vcg;
 using namespace std;
 
-struct MyFace;
-struct MyEdge;
-struct MyVertex: public VertexAFVN<float,MyEdge,MyFace>{};
-struct MyFace: public FaceAFAV<MyVertex,MyEdge,MyFace>{};
-struct MyMesh: public tri::TriMesh< vector<MyVertex>, vector<MyFace> >{};
+// struct MyFace;
+// struct MyEdge;
+// struct MyVertex: public VertexAFVN<float,MyEdge,MyFace>{};
+// struct MyFace: public FaceAFAV<MyVertex,MyEdge,MyFace>{};
+// struct MyMesh: public tri::TriMesh< vector<MyVertex>, vector<MyFace> >{};
+class MyEdge;    // dummy prototype never used
+class MyFace;
+class MyVertex;
+
+// Opt stuff
+
+class MyVertex  : public VertexSimp2< MyVertex, MyEdge, MyFace, vert::Coord3f, vert::Normal3f, vert::BitFlags, vert::VFAdj >{};
+class MyFace    : public FaceSimp2< MyVertex, MyEdge, MyFace, face::InfoOcf, face::FFAdj, face::WedgeTexture2f, face::VertexRef, face::BitFlags, face::Normal3f > {};
+class MyMesh    : public vcg::tri::TriMesh< vector<MyVertex>, face::vector_ocf<MyFace> > {};
 
 #define FLAT	0
 #define BUTTERFLY 2
@@ -73,10 +84,11 @@ int  main(int argc, char **argv)
       printf("Error reading file  %s\n",argv[1]);
 			exit(0);
 		}
-  vcg::tri::UpdateTopology<MyMesh>::FaceFace(m);
-	vcg::tri::UpdateFlags<MyMesh>::FaceBorderFromFF(m);
-	vcg::tri::UpdateNormals<MyMesh>::PerVertexNormalized(m);
 	vcg::tri::UpdateTopology<MyMesh>::VertexFace(m);
+	vcg::tri::UpdateTopology<MyMesh>::FaceFace(m);
+  vcg::tri::UpdateFlags<MyMesh>::FaceBorderFromFF(m);
+ 	vcg::tri::UpdateNormals<MyMesh>::PerVertexNormalized(m);
+
   printf("Input mesh  vn:%i fn:%i\n",m.vn,m.fn);
 	
   n_steps=atoi(argv[3]);
