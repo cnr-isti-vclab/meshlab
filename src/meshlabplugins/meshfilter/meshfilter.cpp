34501522a9179e9d17ac660dae38f98b83d252e7
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2005-11-24 15:31:22 +0000

    add Butterfly method to Filter plugin

--- src/meshlabplugins/meshfilter/meshfilter.cpp
+++ src/meshlabplugins/meshfilter/meshfilter.cpp
@@ -23,25 +23,51 @@
 
 #include <math.h>
 #include <stdlib.h>
-
+// TODO : test directory, need to be moved ...
+#include <../../test/loop/new_refine.h>
 #include "meshfilter.h"
-#include <vcg/complex/trimesh/clean.h>
+//#include <vcg/complex/trimesh/clean.h>
 
 using namespace vcg;
+
+//class CVertexO  : public VertexSimp2< CVertexO, CEdge, CFaceO, vert::Coord3f, vert::Normal3f, vert::BitFlags >{};
+//class CFaceO    : public FaceSimp2< CVertexO, CEdge, CFaceO, face::InfoOcf, face::FFAdjOcf, face::WedgeTexture2f, face::VertexRef, face::BitFlags, face::Normal3fOcf > {};
+//class CMeshO    : public vcg::tri::TriMesh< vector<CVertexO>, face::vector_ocf<CFaceO> > {};
 
 
 QStringList ExtraMeshFilterPlugin::filters() const
 { 
-	return QStringList() << tr("Remove Unreferenced Vertexes");
+	QStringList filterList;
+	//filterList << tr("Loop Subdivision Surface");
+	filterList << tr("Butterfly Subdivision Surface");
+	//	filterList << tr("Remove Unreferenced Vertexes");
+	return filterList;
 }
 
 bool ExtraMeshFilterPlugin::applyFilter(const QString &filter, MeshModel &m, QWidget *parent) 
 {
-	if(filter == tr("Remove Unreferenced Vertexes"))
+	//	if(filter == tr("Loop Subdivision Surface") )
+	//{
+		//  int delvert=tri::Clean<CMeshO>::RemoveUnreferencedVertex(m.cm);
+	  //QMessageBox::information(parent, tr("Filter Plugins"), tr("Removed vertices : %1.").arg(delvert));
+	//}
+	if(filter == tr("Butterfly Subdivision Surface") )
 	{
-	  int delvert=tri::Clean<CMeshO>::RemoveUnreferencedVertex(m.cm);
-	  QMessageBox::information(parent, tr("Filter Plugins"), tr("Removed vertices : %1.").arg(delvert));
+		vcg::tri::UpdateTopology<CMeshO>::VertexFace(m.cm);
+		vcg::tri::UpdateTopology<CMeshO>::FaceFace(m.cm);
+		vcg::tri::UpdateFlags<CMeshO>::FaceBorderFromFF(m.cm);
+		vcg::tri::UpdateNormals<CMeshO>::PerVertexNormalized(m.cm);
+		
+		vcg::Refine<CMeshO, MidPointButterfly<CMeshO> >(m.cm,vcg::MidPointButterfly<CMeshO>(),0);
+		
+		//  int delvert=tri::Clean<CMeshO>::RemoveUnreferencedVertex(m.cm);
+	  //QMessageBox::information(parent, tr("Filter Plugins"), tr("Removed vertices : %1.").arg(delvert));
 	}
+	// 	if(filter == tr("Remove Unreferenced Vertexes"))
+// 	{
+		//  int delvert=tri::Clean<CMeshO>::RemoveUnreferencedVertex(m.cm);
+	  //QMessageBox::information(parent, tr("Filter Plugins"), tr("Removed vertices : %1.").arg(delvert));
+// 	}
   
 	return true;
 }
