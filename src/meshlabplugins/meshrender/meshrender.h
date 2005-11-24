290710f0ae97e126695eb9f008dc234288810112
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2005-11-24 01:40:51 +0000

    Initial Relase of Sample rendering plugins that simply shows normals

---
+++ src/meshlabplugins/meshrender/meshrender.h
@@ -0,0 +1,42 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-2005 Trolltech AS. All rights reserved.
+**
+** This file is part of the example classes of the Qt Toolkit.
+**
+** Licensees holding valid Qt Commercial licenses may use this file in
+** accordance with the Qt Commercial License Agreement provided with the
+** Software.
+**
+** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
+** information about Qt Commercial License Agreements.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef EXTRAFILTERSPLUGIN_H
+#define EXTRAFILTERSPLUGIN_H
+
+#include <QObject>
+#include <QStringList>
+
+#include <meshlab/meshmodel.h>
+#include <meshlab/interfaces.h>
+
+class ExtraMeshRenderPlugin : public QObject, public MeshRenderInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(MeshRenderInterface)
+
+public:
+    QStringList modes() const;
+    virtual void Render(  QString &mode, MeshModel &m, RenderMode &rm, QWidget *parent);
+
+};
+
+#endif
---
+++ src/meshlabplugins/meshrender/meshrender.pro
@@ -0,0 +1,21 @@
+TEMPLATE      = lib
+CONFIG       += plugin
+INCLUDEPATH  += ../.. ../../../../sf ../../../../code/lib/glew/include
+HEADERS       = meshrender.h
+SOURCES       = meshrender.cpp
+TARGET        = meshrender
+DESTDIR       = ../../meshlab/plugins
+
+contains(TEMPLATE,lib) {
+   CONFIG(debug, debug|release) {
+      unix:TARGET = $$member(TARGET, 0)_debug
+      else:TARGET = $$member(TARGET, 0)d
+   }
+}
+QT           += opengl
+
+# install
+target.path = $$[QT_INSTALL_EXAMPLES]/tools/plugandpaint/plugins
+sources.files = $$SOURCES $$HEADERS $$RESOURCES $$FORMS extrafilters.pro
+sources.path = $$[QT_INSTALL_EXAMPLES]/tools/plugandpaintplugins/extrafilters
+INSTALLS += target sources
