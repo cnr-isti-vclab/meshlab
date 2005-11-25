6fd51302ef5638f344283e0a485ea01bd1ee1f4f
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2005-11-25 16:28:24 +0000

    First release: calculate gaussian curvature

---
+++ src/meshlabplugins/meshcurvature/meshcurvature.h
@@ -0,0 +1,49 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-2005 Trolltech AS. All rights reserved.
+**
+** This file is part of the example classes of the Qt Toolkit.
+**
+** Licensees holding valid Qt Commercial licenses may use this file in
+** accordance with the Qt Commercial License Agreement provided with the
+** Software.
+**
+** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
+** information about Qt Commercial License Agreements.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef EXTRACURVATUREPLUGIN_H
+#define EXTRACURVATUREPLUGIN_H
+
+#include <QObject>
+#include <QStringList>
+
+#include <meshlab/meshmodel.h>
+#include <meshlab/interfaces.h>
+
+#include <vcg/math/base.h>
+#include <vcg/math/histogram.h>
+#include <vcg/space/triangle3.h>
+#include <vcg/complex/trimesh/update/color.h>
+
+class MeshColorCurvaturePlugin : public QObject, public MeshColorizeInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(MeshColorizeInterface)
+
+public:
+    virtual void Compute( QString &mode, MeshModel &m, QWidget *parent);
+    //virtual void Show(QString &mode, bool show, MeshModel &m, QWidget *parent) {};
+    //virtual void Finalize(QString &mode, MeshModel &m, QWidget *parent){};
+    QStringList colorsFrom() const;
+
+};
+
+#endif
