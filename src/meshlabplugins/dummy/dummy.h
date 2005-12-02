ca5ac3fc667bc6ac3cc674d6801b8eb8a11350f4
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2005-12-02 16:16:23 +0000

    added dummy plugin files

---
+++ src/meshlabplugins/dummy/dummy.h
@@ -0,0 +1,54 @@
+/****************************************************************************
+**
+** Copyright (C) 2005-2005 Trolltech AS. All rights reserved.
+**
+** This file is part of the example classes of the Qt Toolkit.
+**
+** Licensees holding valid Qt Commercial licenses may use this file in
+** accordance with the Qt Commercial License Agreement provided with the
+** Software.
+**
+** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
+** information about Qt Commercial License Agreements.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef DUMMYPLUGIN_H
+#define DUMMYPLUGIN_H
+
+#include <QObject>
+#include <QAction>
+#include <QString>
+
+#include <meshlab/glArea.h>
+#include <meshlab/meshmodel.h>
+#include <meshlab/interfaces.h>
+
+class DummyPlugin : public QObject, public MeshRenderInterface
+{
+  Q_OBJECT
+  Q_INTERFACES(MeshRenderInterface)
+
+	QList<QAction *> actionList;
+  
+public:
+	ExtraMeshIOPlugin()
+	{
+		actionList<<new QAction(QString("action 1"),this);
+		actionList<<new QAction(QString("action 2"),this);
+		actionList<<new QAction(QString("action 3"),this);
+	}
+
+	QList<QAction *> actions () {return actionList;}
+
+	virtual void Render(QAction *, MeshModel &, RenderMode &, GLArea *);
+
+};
+
+#endif
