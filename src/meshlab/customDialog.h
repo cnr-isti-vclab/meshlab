e0f2ba49360884c2abe1d7d884b0f37a2f0d9a90
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2005-12-03 22:50:06 +0000

    Added copyright info

--- src/meshlab/customDialog.h
+++ src/meshlab/customDialog.h
@@ -1,3 +1,39 @@
+/****************************************************************************
+* MeshLab                                                           o o     *
+* A versatile mesh processing toolbox                             o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2005                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+/****************************************************************************
+  History
+$Log$
+Revision 1.3  2005/12/03 22:49:46  cignoni
+Added copyright info
+
+Revision 1.3  2005/11/24 01:38:36  cignoni
+Added new plugins intefaces, tested with shownormal render mode
+
+Revision 1.2  2005/11/21 12:12:54  cignoni
+Added copyright info
+
+****************************************************************************/
+
 #include "ui_custom.h"
 #include <QColor>
 #include <QDialog>
