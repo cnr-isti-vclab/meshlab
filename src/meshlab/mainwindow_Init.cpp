7240bbabc0501b099cdf1636f85d9fb07bb5134b
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2005-12-04 15:25:22 +0000

    added texture action in menu render

--- src/meshlab/mainwindow_Init.cpp
+++ src/meshlab/mainwindow_Init.cpp
@@ -24,6 +24,9 @@
 History
 
 $Log$
+Revision 1.13  2005/12/04 15:25:22  glvertex
+added texture action in menu render
+
 Revision 1.12  2005/12/04 14:45:30  glvertex
 gla now is a local variable used only if needed
 texture button now works properly
@@ -452,6 +455,7 @@ void MainWindow::createMenus()
 	renderModeMenu=renderMenu->addMenu(tr("Render Mode"));
 	renderModeMenu->addAction(backFaceCullAct);
 	renderModeMenu->addActions(renderModeGroupAct->actions());
+	renderModeMenu->addAction(renderModeTextureAct);
 	
 	lightingModeMenu=renderMenu->addMenu(tr("Lighting"));
 	lightingModeMenu->addAction(setLightAct);
