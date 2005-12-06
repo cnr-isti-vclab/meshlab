ddcb730abffcb470ff0904728b224acaf119dba5
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2005-12-06 16:39:41 +0000

    Added a QActionGroup for the menu options

--- src/meshlabplugins/meshcolorize/meshcolorize.h
+++ src/meshlabplugins/meshcolorize/meshcolorize.h
@@ -49,10 +49,28 @@ public:
 
 		MeshColorCurvaturePlugin()
 		{
-			//QActionGroup * ag = new QActionGroup(this);
-			actionList << new QAction(QString("None"),this);
-			actionList << new QAction(QString("Per Face"),this);
-			actionList << new QAction(QString("Per Vertex"),this);
+			QAction *qa; 
+			QActionGroup * ag; 
+
+			ag = new QActionGroup(this); 
+			ag->setExclusive(true); 
+
+			qa = new QAction(QString("None"),this); 
+			qa->setActionGroup(ag); 
+			qa->setCheckable(true);
+			qa->setChecked(true); 
+			actionList << qa; 
+
+			qa = new QAction(QString("Per Face"),this); 
+			qa->setActionGroup(ag); 
+			qa->setCheckable(true);
+			actionList << qa; 
+
+			qa = new QAction(QString("Per Vertex"),this); 
+			qa->setActionGroup(ag); 
+			qa->setCheckable(true);
+			actionList << qa; 
+
 			actionList << new QAction(QString("Gaussian Curvature"),this);
 		}
     
