c0a877c3c7b37a02b9eb5ec7d714d47f6d71c902
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2005-12-08 14:53:36 +0000

    changed implementation of Vett type (from array [] [] [] to 3 std::vector nested)

--- src/test/decimator/decimator.h
+++ src/test/decimator/decimator.h
@@ -24,6 +24,9 @@
   History
 
 $Log$
+Revision 1.4  2005/12/08 14:53:36  mariolatronico
+changed implementation of Vett type (from array [] [] [] to 3 std::vector nested)
+
 Revision 1.3  2005/12/05 18:47:08  mariolatronico
 first try with correct Set and User bit
 
@@ -47,6 +50,7 @@ Test program for clustering decimator.
 #include <iostream>
 #include <math.h>
 #include <QtGlobal>
+#include <limits>
 #define PI 3.14159265
 
 using std::cout;
@@ -58,17 +62,32 @@ namespace vcg{
   /*
     Metodo di clustering
   */
-
   template<class MESH_TYPE>
-  bool Decimator(MESH_TYPE &m,int n)
+  bool Decimator(MESH_TYPE &m, int n)
   {	 
-    int n3 = n*n*n;
+    //int n3 = n*n*n;
     //std::vector <typename MESH_TYPE::CoordType > Vett(n3);
+		const int p = n;
+		std::vector<MESH_TYPE::CoordType> primaDim(n);
+		
+		std::vector< std::vector< std::vector< MESH_TYPE::CoordType> > > Vett(n); 	
+		 	for(int i = 0 ; i < n ; ++i)
+				for(int j = 0 ; j < n ; ++j) {
+						Vett[i].resize(n);	
+						Vett[i][j].resize(n);
+				}
 
-    Point3f Vett[n][n][n];
+		//Point3f Vett[n][n][n];// = new Point3f[n][n][n];
     typename MESH_TYPE::CoordType Cmin,Cmax;
-		Cmin.Zero(); Cmax.Zero();
-    //inizializzo a zero gli elementi
+	//	Cmin.Zero(); Cmax.Zero();
+		for (int i = 0; i < 3; i++) {
+			Cmin[i] = numeric_limits<float>::max();
+			Cmax[i] = numeric_limits<float>::min();
+	
+
+		}
+
+		//inizializzo a zero gli elementi
      	for(int i = 0 ; i < n ; ++i)
 				for(int j = 0 ; j < n ; ++j)
 					for(int k = 0 ; k < n ; ++k)
@@ -136,12 +155,12 @@ namespace vcg{
 				//			std::cout <<  idx << " " << idy <<" "  << idz <<std::endl;
 
 		
-					Vett[idx][idy][idz] = (Vett[idx][idy][idz] + (*vi).P())/2;
+					Vett[idx][idy][idz] = (Vett[idx][idy][idz] + app)/2;
 					(*vi).ClearUserBit(referredBit);
 					//(*vi).SetUserBit(referredBit);
 					//cout << (*vi).Flags() << endl;
       }
-		//		std::cout << "Puppa -------------------------" << endl;
+		
     typename MESH_TYPE::FaceIterator fi;
     //reimposto i vertici con la media di quelli che cadevano nel suo quadrante
     for(fi = m.face.begin(); fi != m.face.end(); ++fi)
@@ -152,7 +171,7 @@ namespace vcg{
 						for(int i = 0; i < 3; ++i)
 							{//per ogni vertice del triangolo
 								if(!(*fi).V(i)->IsUserBit(referredBit))
-								//if ((*fi).V(i)->Flags() != 32)
+							
 									{//se non l'ho gia cambiato
 										
 										Point3f app = (*fi).V(i)->P();
@@ -178,11 +197,11 @@ namespace vcg{
 						float c = (*fi).V(2) - (*fi).V(0);
 						float p = (a + b + c)/2;
 						float area = sqrt( p * (p - a) * (p - b) * (p - c));
-						if(abs(area)  < 0.00000001f ) {
+						if(abs(area)  < numeric_limits<float>::epsilon() ) {
 								(*fi).SetD();
 							}
 					}
-      }
+			}
 
 		// std::vector<typename MESH_TYPE::FaceType> fv;
 // 		for ( fi = m.face.begin(); fi != m.face.end(); ++fi) {
@@ -201,7 +220,7 @@ namespace vcg{
 
 // 		}
 		
-		
+//		delete[] Vett;		
     return true;
 
   }
