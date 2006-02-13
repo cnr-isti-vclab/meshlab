cfa589ecfdc62a398e82f402e927e8de71c51ea9
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2006-02-13 14:18:16 +0000

    cleaned up

--- src/meshlab/LogStream.h
+++ src/meshlab/LogStream.h
@@ -23,6 +23,9 @@
 /****************************************************************************
   History
 $Log$
+Revision 1.5  2006/02/13 14:18:16  cignoni
+cleaned up
+
 Revision 1.4  2006/01/25 15:37:15  glvertex
 Added variable vertical spacing
 
@@ -34,42 +37,11 @@ removed _WINDOWS define and changed <qgl.h> in <QGLWidget> to correctly compile 
 
 Revision 1.1  2005/11/16 23:19:22  cignoni
 Initial Draft release; still to be adapted to our needs.
-
-Revision 1.2  2005/07/22 11:16:25  fiorin
-out-of-core
-volume partition
-
-Revision 1.1  2005/04/01 15:43:42  fiorin
-Initial commit
-
-Revision 1.7  2005/03/31 12:03:04  fiorin
-*** empty log message ***
-
-Revision 1.6  2005/03/18 13:45:32  fiorin
-*** empty log message ***
-
-Revision 1.5  2005/03/16 22:36:45  fiorin
-*** empty log message ***
-
-Revision 1.4  2005/03/16 17:19:24  fiorin
-*** empty log message ***
-
-Revision 1.3  2005/03/15 20:23:02  fiorin
-*** empty log message ***
-
-Revision 1.2  2005/02/22 16:28:48  fiorin
-*** empty log message ***
-
-Revision 1.1  2004/07/12 14:02:14  cignoni
-Initial Commit
-
-
 ****************************************************************************/
 
 #ifndef LOGSTREAM_H
 #define LOGSTREAM_H
 
-#include <GL/glew.h>
 #include <QGLWidget> 
 #include <QFont>
 
@@ -78,10 +50,7 @@ class LogStream
 {
 public:
   typedef enum {Error=0, Warning=1, Info=2, Debug=3, Direct=4, OnlyFileLog=5, OnlyConsole=6} Level ;
-  
 	virtual void Log(int Level, const char * f, ... ) = 0;
-
-
 	virtual void glDraw(QGLWidget *qgl, int Level, int nlines,float vSpacing,QFont font) = 0;
 
 };
