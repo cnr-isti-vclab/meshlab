988dd5940f71bbf9d6459271def717821ad2c107
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2007-01-13 02:00:51 +0000

    Ver 0.9.1b

--- src/meshlab/mainwindow.h
+++ src/meshlab/mainwindow.h
@@ -23,6 +23,9 @@
 /****************************************************************************
   History
 $Log$
+Revision 1.76  2007/01/13 02:00:51  cignoni
+Ver 0.9.1b
+
 Revision 1.75  2006/12/21 21:24:54  cignoni
 version 0.9.1
 
@@ -127,7 +130,7 @@ public:
   MainWindow();
    static bool QCallBack(const int pos, const char * str);
 	 const QString appName() const {return tr("MeshLab v")+appVer(); }
-   const QString appVer() const {return tr("0.9.1"); }
+   const QString appVer() const {return tr("0.9.1b"); }
 
   // MaskObj maskobj;
 
