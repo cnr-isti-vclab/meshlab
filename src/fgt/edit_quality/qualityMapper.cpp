02a5586b425833ace18c21ba5f29a2d3b0b95664
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2008-01-09 22:15:40 +0000

    QualityMapper skeleton

---
+++ src/fgt/edit_quality/qualityMapper.cpp
@@ -0,0 +1,133 @@
+/****************************************************************************
+* MeshLab                                                           o o     *
+* A versatile mesh processing toolbox                             o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2005                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+/****************************************************************************
+History
+Log: qualityMapper.cpp
+Revision 1.0 2008/01/03 MAL
+Beginning
+
+****************************************************************************/
+#include "qualityMapper.h"
+#include <QtGui>
+#include <limits>
+#include <vcg/complex/trimesh/clean.h>
+#include <vcg/complex/trimesh/stat.h>
+#include <vcg/complex/trimesh/update/flag.h>
+// #include "color_manifold.h"
+#include "../../meshlabplugins/meshcolorize/curvature.h"		//<--contains Frange
+#include "../../meshlabplugins/meshcolorize/equalizerDialog.h"	//for EqualizerSettings
+// #include "smoothcolor.h"
+#include <vcg/space/triangle3.h> //for quality
+
+//using namespace std;
+using namespace vcg;
+
+QualityMapperPlugin::QualityMapperPlugin()
+{
+}
+
+
+const PluginInfo &QualityMapperPlugin::pluginInfo() 
+{
+  static PluginInfo ai; 
+  ai.Date=tr("Jan 2008");
+  ai.Version = tr("1.0");
+  ai.Author = ("Alessandro Maione, Federico Bellucci");
+  return ai;
+}
+
+
+// this function is called to fill the parameter list 
+// It is called only for filters that have a not empty list of parameters and 
+// that do not use the auto generated dialog, but want a personalized dialog.
+bool QualityMapperPlugin::getParameters(QAction *action, QWidget * parent, MeshModel &m, FilterParameterSet & par, MainWindowInterface *mw) 
+{
+/************************************************************************/
+/* CONSERVARE QUESTO!!! CREA L'ISTOGRAMMA DELLA QUALITA' DA SOLO!!      */
+/* m@l                                                                  */
+/************************************************************************/
+	Histogramf H;
+	tri::Stat<CMeshO>::ComputePerVertexQualityHistogram(m.cm,H);
+
+	Frange mmmq(tri::Stat<CMeshO>::ComputePerVertexQualityMinMax(m.cm));
+
+	H.FileWrite("risultato.txt");
+
+	//probabilmente questa roba EqualizerSettings non serve...
+	EqualizerSettings eqSettings;
+	eqSettings.meshMinQ = mmmq.minV;
+	eqSettings.meshMaxQ = mmmq.maxV;
+
+	eqSettings.histoMinQ = H.Percentile(eqSettings.percentile/100);
+	eqSettings.histoMaxQ = H.Percentile(1.0f-eqSettings.percentile/100);
+
+//	EqualizerDialog eqdialog(parent);
+//	eqdialog.setValues(eqSettings);
+
+
+ 	Frange FinalRange;
+// 	eqSettings=eqdialog.getValues();
+// 	if (eqSettings.useManual) 
+// 	FinalRange = Frange(eqSettings.manualMinQ,eqSettings.manualMaxQ);
+// 	else
+// 	{
+// 		FinalRange.minV=H.Percentile(eqSettings.percentile/100.0);
+// 		FinalRange.maxV=H.Percentile(1.0f-(eqSettings.percentile/100.0));
+// 	}
+
+	par.addFloat("RangeMin",FinalRange.minV);
+	par.addFloat("RangeMax",FinalRange.maxV);      
+	mw->executeFilter(action,par);
+
+	return true;
+}
+
+
+void QualityMapperPlugin::initParameterSet(QAction *a,MeshModel &m, FilterParameterSet & par)
+{
+// 	switch(ID(a))
+//   {
+// 	case CP_TRIANGLE_QUALITY: {
+// 			QStringList metrics;
+// 			metrics.push_back("area/max side");
+// 			metrics.push_back("inradius/circumradius");
+// 			metrics.push_back("mean ratio");
+// 			par.addEnum("Metric", 0, metrics, tr("Metric:"), tr("Choose a metric to compute triangle quality."));
+// /// transformation matrix into a regular simplex"));
+// 
+// //			par.addEnum("Metric", 0, metrics, tr("Metric:"), tr("Choose a metric to compute triangle quality."));
+// 			break;
+// 		}
+// 	default: assert(0);
+// 	}
+}
+
+bool QualityMapperPlugin::applyFilter(QAction *filter, MeshModel &m, FilterParameterSet & par, vcg::CallBackPos *cb)
+{
+	return true;
+}
+
+
+
+Q_EXPORT_PLUGIN(QualityMapperPlugin)
+  
---
+++ src/fgt/edit_quality/qualityMapper.h
@@ -0,0 +1,148 @@
+/****************************************************************************
+ * MeshLab                                                           o o     *
+ * A versatile mesh processing toolbox                             o     o   *
+ *                                                                _   O  _   *
+ * Copyright(C) 2005                                                \/)\/    *
+ * Visual Computing Lab                                            /\/|      *
+ * ISTI - Italian National Research Council                           |      *
+ *                                                                    \      *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *   
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+ * for more details.                                                         *
+ *                                                                           *
+ ****************************************************************************/
+/****************************************************************************
+  History
+Log: qualityMapper.h
+Revision 1.0 2008/01/03 MAL
+Beginning
+
+****************************************************************************/
+
+#ifndef _QUALITY_MAPPER_PLUGIN_H_
+#define _QUALITY_MAPPER_PLUGIN_H_
+
+#include <QObject>
+//#include <QAction>
+//#include <QActionGroup>
+//#include <QList>
+
+#include <meshlab/meshmodel.h>
+#include <meshlab/interfaces.h>
+#include <meshlab/glarea.h>
+
+//#include <vcg/math/base.h>
+#include <vcg/math/histogram.h>
+//#include <vcg/space/triangle3.h>
+#include <vcg/complex/trimesh/update/color.h> //<-- contains VertexQuality method
+
+
+class QualityMapperPlugin : public QObject, public MeshFilterInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(MeshFilterInterface)
+
+private:
+	Histogramf _histogram;
+
+public:
+    QualityMapperPlugin();
+    ~QualityMapperPlugin(){};
+
+	// The longer string describing each filtering action 
+	// (this string is used in the About plug in dialog)
+	const QString filterInfo(FilterIDType filter)
+	{	return QString("This plug in colorizes mesh vertexes by Quality noting quality distribution and applying a custom color band");	}
+
+	// The very short string describing each filtering action 
+	// (this string is used also to define the menu entry)
+	const QString filterName(FilterIDType filter)
+	{	return QString("Colorize mesh vertexes by Quality following some rules");	}
+
+	// Generic Info about the plug in version and author.
+	const PluginInfo &pluginInfo();
+	//TO BE IMPLEMENTED!
+
+/*
+		// This function is called by the framework, for each action at the loading of the plugins.
+		// it allows to add a list of global persistent parameters that can be changed from the meshlab itself.
+		// If your plug ins/action has no GlobalParameter, do nothing.
+		virtual void initGlobalParameterSet(QAction *, FilterParameterSet & / *globalparam* /) {}
+*/
+	//TO BE IMPLEMENTED IF NECESSARY!
+
+	// The FilterClass describes in which generic class of filters it fits. 
+	// This choice affect the submenu in which each filter will be placed 
+	// For example filters that perform an action only on the selection will be placed in the Selection Class Ê
+	const FilterClass getClass(QAction *) { return MeshFilterInterface::VertexColoring; } //modified by MAL
+
+/*
+		// The filters can have some additional requirements on the mesh capabilities. 
+		// For example if a filters requires Face-Face Adjacency you should re-implement 
+		// this function making it returns MeshModel::MM_FACETOPO. 
+		// The framework will ensure that the mesh has the requirements satisfied before invoking the applyFilter function
+		virtual const int getRequirements(QAction *){return MeshModel::MM_NONE;}*/
+	//TO BE IMPLEMENTED IF NECESSARY!
+	
+
+	// The main function that applies the selected filter with the already stabilished parameters
+	// This function is called by the framework after getting the user params 
+	bool applyFilter(QAction * /*filter*/, MeshModel &/*m*/, FilterParameterSet & /*parent*/, vcg::CallBackPos * /*cb*/);
+	//TO BE IMPLEMENTED!
+
+		//  this function returns true if the filter has parameters that must be filled with an automatically build dialog.
+	bool autoDialog(QAction *) {return false;}
+	//TO BE IMPLEMENTED IF NECESSARY!
+
+	// This function is called to initialized the list of parameters. 
+	// it is called by the auto dialog framework to know the list of parameters.
+	void initParameterSet(QAction *action,MeshModel &meshModel, FilterParameterSet &parent);
+
+
+/*
+		//  this function returns true if the filter has a personally customized dialog..
+		virtual bool customDialog(QAction *) {return false;}*/
+	//TO BE IMPLEMENTED IF NECESSARY!
+
+
+	// This function invokes the custom dialog of a filter and when has the params 
+	// it notifies it to the main window with the collected parameters
+	bool getParameters(QAction *action, QWidget * parent, MeshModel & meshModel, FilterParameterSet & params, MainWindowInterface *mw);
+// 		{
+// 			assert(mw);
+// 			mw->executeFilter(action,params);
+// 			return true;
+// 		}
+//		TO DELETE!?!?
+
+
+/*
+	/// Standard stuff that usually should not be redefined. 
+	void setLog(GLLogStream *log) { this->log = log ; }
+
+	virtual const FilterIDType ID(QAction *a)
+	{
+		foreach( FilterIDType tt, types())
+			if( a->text() == this->filterName(tt) ) return tt;
+		assert(0);
+		return 0;
+	}
+
+	virtual const QString filterInfo(QAction *a){return filterInfo(ID(a));};
+	virtual const QString filterName(QAction *a){return filterName(ID(a));};
+
+	virtual QList<QAction *> actions() const { return actionList;}
+	virtual QList<FilterIDType> &types() { return typeList;}*/
+	//TO BE IMPLEMENTED IF NECESSARY!
+};
+
+#endif
