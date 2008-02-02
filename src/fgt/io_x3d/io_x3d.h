4088fe24f0e79bd947e19a7fee113c63645917e2
Gianpaolo Palma gianpaolopalma <gianpaolo.palma@isti.cnr.it>
2008-02-02 13:36:09 +0000

    Defined X3D plugin interface

---
+++ src/fgt/io_x3d/io_x3d.h
@@ -0,0 +1,56 @@
+/****************************************************************************
+* MeshLab                                                           o o     *
+* A versatile mesh processing toolbox                             o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2005                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+/****************************************************************************
+  History
+
+ $Log$
+ Revision 1.1  2008/02/02 13:36:09  gianpaolopalma
+ Defined X3D plugin interface
+
+ 
+ *****************************************************************************/
+#ifndef IOX3D_H
+#define IOX3D_H
+
+#include <QObject>
+
+#include <meshlab/meshmodel.h>
+#include <meshlab/interfaces.h>
+
+class IoX3DPlugin : public QObject, public MeshIOInterface
+{
+  Q_OBJECT
+  Q_INTERFACES(MeshIOInterface)
+
+  
+public:
+	QList<Format> importFormats() const;
+	QList<Format> exportFormats() const;
+	virtual const PluginInfo &Info();
+	virtual void GetExportMaskCapability(QString &format, int &capability, int &defaultBits) const;
+
+	bool open(const QString &formatName, const QString &fileName, MeshModel &m, int& mask, vcg::CallBackPos *cb=0, QWidget *parent=0);
+	bool save(const QString &formatName, const QString &fileName, MeshModel &m, const int mask, vcg::CallBackPos *cb, QWidget *parent);
+};
+
+#endif
