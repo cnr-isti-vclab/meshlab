cc52f8c808ad65630264487f3a1dbb456f0a18e9
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2008-03-06 08:32:44 +0000

    added meaningful info (q and tex) to the displayed tri

--- src/sampleplugins/sampleedit/sampleedit.cpp
+++ src/sampleplugins/sampleedit/sampleedit.cpp
@@ -118,8 +118,10 @@ void SampleEditPlugin::drawFace(CMeshO::FacePointer fp, MeshModel &m, GLArea * g
 	for(int i=0;i<3;++i)
 		{
 			QString buf=QString("v%1:%2 (%3 %4 %5)").arg(i).arg(fp->V(i) - &m.cm.vert[0]).arg(fp->P(i)[0]).arg(fp->P(i)[1]).arg(fp->P(i)[2]);
+			if( m.ioMask & MeshModel::IOM_VERTQUALITY)
+				buf+=QString(" \nQ(%1)").arg(fp->V(i)->Q());
 			if(vcg::tri::HasPerWedgeTexCoord(m.cm)) 
-					buf+=QString(" \nuv(%3 %4)id:%5").arg(fp->WT(i).U()).arg(fp->WT(i).V()).arg(fp->WT(i).N());
+					buf+=QString(" \nuv(%1 %2) id:%3").arg(fp->WT(i).U()).arg(fp->WT(i).V()).arg(fp->WT(i).N());
 			gla->renderText(fp->P(i)[0], fp->P(i)[1], fp->P(i)[2], buf, qFont);
 		}
 }
