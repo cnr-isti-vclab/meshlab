2f8d05389f192d2bf035625436feb5e2e6bb8e86
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2008-03-22 07:40:31 +0000

    yet another small change to get rid of names with spaces in the middle

--- src/meshlab/plugins/U3D_OSX/IDTFConverter.sh
+++ src/meshlab/plugins/U3D_OSX/IDTFConverter.sh
@@ -2,4 +2,6 @@
 # 
 export LD_LIBRARY_PATH=$1:${LD_LIBRARY_PATH}
 export U3D_LIBDIR=$1
-$1/IDTFConverter.out $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12}
+$1/IDTFConverter.out $2 $3 $4 $5 $6 $7 $8 $9 ${10} "${11}" 
+
+#echo $1/IDTFConverter.out $2 $3 $4 $5 $6 $7 $8 $9 ${10} \"${11}\"> /tmp/dump.txt
