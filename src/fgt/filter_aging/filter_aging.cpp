d69ac0b9831d4d465706cdcacc8f024bac7c6a7a
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2008-04-07 18:36:34 +0000

    Removed method to refine mesh: refineE function will be used instead. Removed method to generate an array of random offset. Added a new method to generate a random value to displace vertexes (using perlin noise).

--- src/fgt/filter_aging/filter_aging.cpp
+++ src/fgt/filter_aging/filter_aging.cpp
@@ -22,23 +22,23 @@
 ****************************************************************************/
 
 
-#include <map>
-#include <vector>
-//#include <iostream>        // DEBUG
-
+//#include <iostream>		// DEBUG
 #include "filter_aging.h"
 
-#include <vcg/complex/trimesh/allocate.h>
-#include <vcg/complex/trimesh/update/normal.h>
+#include <vcg/complex/trimesh/update/position.h>
+#include <vcg/complex/trimesh/update/bounding.h>
+#include <vcg/complex/trimesh/stat.h>
+#include <vcg/math/perlin_noise.h>
+//#include <vcg/space/intersection/triangle_triangle3.h>
 
 
 // Constructor 
 GeometryAgingPlugin::GeometryAgingPlugin() 
 { 
-    typeList << FP_ERODE;
-    
-    foreach(FilterIDType tt, types())
-        actionList << new QAction(filterName(tt), this);
+	typeList << FP_ERODE;
+	
+	foreach(FilterIDType tt, types())
+		actionList << new QAction(filterName(tt), this);
 }
 
 
@@ -51,311 +51,197 @@ GeometryAgingPlugin::~GeometryAgingPlugin()
 // Returns the very short string describing each filtering action
 const QString GeometryAgingPlugin::filterName(FilterIDType filterId) 
 {
-    switch(filterId) {
-        case FP_ERODE:
-            return QString("Aging Simulation");
-        default:
-            assert(0);
-    }
+	switch(filterId) {
+		case FP_ERODE:
+			return QString("Aging Simulation");
+		default:
+			assert(0);
+	}
 }
 
 
 // Returns the longer string describing each filtering action
 const QString GeometryAgingPlugin::filterInfo(FilterIDType filterId) 
 {
-    switch(filterId) {
-        case FP_ERODE: 
-            return QString("Simulate the aging effects due to small collisions or various chipping events");
-        default: 
-            assert(0);
-    }
+	switch(filterId) {
+		case FP_ERODE: 
+			return QString("Simulates the aging effects due to small collisions or various chipping events");
+		default: 
+			assert(0);
+	}
 }
 
 
 // Returns plugin info
 const PluginInfo &GeometryAgingPlugin::pluginInfo() 
 {
-    static PluginInfo ai;
-    ai.Date = tr(__DATE__);
-    ai.Version = tr("0.1");
-    ai.Author = tr("Marco Fais");
-    return ai;
+	static PluginInfo ai;
+	ai.Date = tr(__DATE__);
+	ai.Version = tr("0.1");
+	ai.Author = tr("Marco Fais");
+	return ai;
  }
 
 
 // Initializes the list of parameters (called by the auto dialog framework)
 void GeometryAgingPlugin::initParameterSet(QAction *action, MeshModel &m, FilterParameterSet &params)
 {
-    switch(ID(action)) {
-        case FP_ERODE:
-            params.addAbsPerc("AngleThreshold", 60.0, 10.0, 170.0,  "Angle Threshold", "The minimum angle between two adjacent faces to consider the edge they are sharing.");
-            params.addAbsPerc("EdgeLenThreshold", m.cm.bbox.Diag()*0.01, 0.0, m.cm.bbox.Diag(),"Edge Len Threshold", "The minimum length of an edge. Usefull to avoid the creation of too many small faces.");
-            break;
-        default:
-            assert(0);
-    }
+	std::pair<float,float> qRange;
+	if(m.cm.HasPerVertexQuality())
+		qRange = tri::Stat<CMeshO>::ComputePerVertexQualityMinMax(m.cm);
+	else
+		qRange = std::pair<float,float>(0.0, 255.0);
+	QStringList styles;
+	// TODO: find better strings to list styles 
+	styles.append("Simple     (1 function) ");
+	styles.append("Linear     (8 functions)");
+	styles.append("Sinusoidal (8 functions)");
+	
+	switch(ID(action)) {
+		case FP_ERODE:
+			params.addBool("UseQuality", m.cm.HasPerVertexQuality(), "Use per vertex quality values", 
+					"Use per vertex quality values to find the areas to erode.\n Usefull after applying ambient occlusion filter.");
+			params.addAbsPerc("QualityThreshold", qRange.first+(qRange.second-qRange.first)*2/3, qRange.first, qRange.second,  "Quality threshold", 
+					"When you decide to use per vertex quality values, \n this parameter represents the minimum quality value \n \
+					two vertexes must have to consider the edge they are sharing.");
+			params.addAbsPerc("AngleThreshold", 60.0, 10.0, 170.0,  "Angle threshold (deg)", 
+					"If you decide not to use per vertex quality values, \nthe angle between two adjacent faces will be considered.\n \
+					This parameter represents the minimum angle between two adjacent\n faces to consider the edge they are sharing.");
+			params.addAbsPerc("EdgeLenThreshold", m.cm.bbox.Diag()*0.01, 0.0, m.cm.bbox.Diag(),"Edge len threshold", 
+					"The minimum length of an edge. Usefull to avoid the creation of too many small faces.");
+			params.addAbsPerc("ChipDepth", m.cm.bbox.Diag()*0.005, 0.0, m.cm.bbox.Diag()*0.1,"Max chip depth", 
+					"The maximum depth of a chip.");
+			params.addEnum("ChipStyle", 0, styles, "Chip Style", "Mesh erosion style to use. Different styles are defined \
+					passing different parameters to the noise function \nthat generates displacement values.");
+			params.addBool("Selected", m.cm.sfn>0, "Affect only selected faces", 
+					"The aging procedure will be applied to the selected faces only.");
+			break;
+		default:
+			assert(0);
+	}
 }
 
 
 // The Real Core Function doing the actual mesh processing
 bool GeometryAgingPlugin::applyFilter(QAction *filter, MeshModel &m, FilterParameterSet &params, vcg::CallBackPos *cb)
 {
-    float angleThreshold;
-    float edgeLenTreshold;
-    
-    switch(ID(filter)) {
-        case FP_ERODE:
-            angleThreshold = params.getAbsPerc("AngleThreshold");
-            edgeLenTreshold = params.getAbsPerc("EdgeLenThreshold");
-            return refineNeededEdges(m.cm, FractPoint(), EdgePred(angleThreshold, edgeLenTreshold), hasSelected(m), cb);
-        default:
-            assert(0);
-    }
-}
-
-
-// checks the mesh looking for selected faces
-bool GeometryAgingPlugin::hasSelected(MeshModel &m)
-{
-    CMeshO::FaceIterator fi;
-    for(fi=m.cm.face.begin(); fi!=m.cm.face.end(); ++fi) {
-		if(!(*fi).IsD() && (*fi).IsS()) return true;
+	bool useQuality = params.getBool("UseQuality");
+	if(useQuality && !m.cm.HasPerVertexQuality()) {
+		errorMessage = QString("This mesh doesn't have per vertex quality informations.");
+		return false;
 	}
-	return false;
-}
-
-
-// randomly creates a vector of eroderPoints and returns a pointer to it
-vector<GeometryAgingPlugin::EroderPoint>* GeometryAgingPlugin::generateEdgeEroder(CMeshO::FacePointer f, int z)
-{
-    vector<EroderPoint>* e = new vector<EroderPoint>();
-    
-    srand(time(NULL));
-    
-    //std::cout << "len: " << len << std::endl;           // DEBUG
-    if(f->IsB(z))
-        e->push_back(GeometryAgingPlugin::EroderPoint(0.5, 0.0));
-    /*else
-        e->push_back(GeometryAgingPlugin::EroderPoint(0.5, 0.0, 0.0));*/
-    
-    return e;
-}
-
-
-// refines the edges which will be eroded in the next phase
-bool GeometryAgingPlugin::refineNeededEdges(CMeshO &m, FractPoint frpoi, EdgePred ep, bool hasSelected, CallBackPos *cb)
-{
-    int j, newVertNum = 0, newFaceNum = 0;
-    typedef std::pair<CMeshO::VertexPointer, CMeshO::VertexPointer> vvpair;
-    typedef std::pair<vvpair, int> vvi;
-    map<vvi, CMeshO::VertexPointer> edge2Vert;
-    
-    // First Loop: we analyze the mesh to compute the number of the new faces and new vertices 
-    CMeshO::FaceIterator fi;
-    int step = 0, percStep = m.fn / 33;
-    if(percStep == 0) percStep = 1;
-    for(fi=m.face.begin(),j=0; fi!=m.face.end(); ++fi) 
-        if(!(*fi).IsD())
-        {
-            if(cb && (++step%percStep) == 0)
-                (*cb)(step/percStep, "Eroding...");
-            for(j=0; j<3; j++) {
-                if(ep(&(*fi), j) && (!hasSelected || ((*fi).IsS() && (*fi).FFp(j)->IsS()))) {
-                    int numf = ep.numFacesToAdd(&(*fi), j);
-                    if(numf <= 0) continue;
-                    newFaceNum += numf;
-                    if(((*fi).V(j) < (*fi).V1(j)) || (*fi).IsB(j))
-                        newVertNum += numf;
-                }
-            }
-        } // end face loop
-    
-    if(newVertNum==0) {	// temporary error message
-        errorMessage = QString("Found 0 edges to erode. Change filter parameters and try again.");
-        return false;
-    }
-    
-    CMeshO::VertexIterator lastv = tri::Allocator<CMeshO>::AddVertices(m, newVertNum);
 	
-	// Second Loop: initialization of the edge-vertexes map
-	// and the position of new vertexes
-    for(fi=m.face.begin(); fi!=m.face.end(); ++fi) 
-        if(!(*fi).IsD())
-        {
-            if(cb && (++step%percStep) == 0)
-                (*cb)(step/percStep, "Eroding...");
-            for(j=0; j<3; j++)
-			    if(ep(&(*fi), j)  && (!hasSelected || ((*fi).IsS() && (*fi).FFp(j)->IsS())))
-                    if((*fi).V(j) < (*fi).V1(j) || (*fi).IsB(j)) {
-                        int numf = ep.numFacesToAdd(&(*fi), j) + 1;
-                        for(int p=1; p<numf; p++) {
-                            frpoi((*lastv), face::Pos<CMeshO::FaceType>(&*fi,j), (float)p/numf);
-                            edge2Vert[vvi(vvpair((*fi).V(j), (*fi).V1(j)), p)] = &*lastv;
-                            ++lastv;
-                        }
-                    }
-        }
-	assert(lastv == m.vert.end());
-
-    CMeshO::FaceIterator lastf = tri::Allocator<CMeshO>::AddFaces(m, newFaceNum);
-    CMeshO::FaceIterator oldendf = lastf; 
+	float qualityThreshold = params.getAbsPerc("QualityThreshold");
+	float angleThreshold = params.getAbsPerc("AngleThreshold");
+	float edgeLenTreshold = params.getAbsPerc("EdgeLenThreshold");
+	float chipDepth = params.getAbsPerc("ChipDepth");
+	int chipStyle = params.getEnum("ChipStyle");
+	bool selected = params.getBool("Selected");
 	
-/*
-          v0
-
-   
-          f0
-
-    mp01      mp02
-
-          f3
-      f1       f2
-
- v1      mp12       v2
-
-*/
-
-    vector<CMeshO::VertexPointer> vv;        // total face vertexes
-                                        //   0..2 original face vertexes 
-                                        //   3... newly created vertexes
-    vector<CMeshO::FacePointer> nf;          // total faces (the original one and the newly created)
-    vector<CMeshO::FaceType::TexCoordType> wtt;  // per wedge texture coordinates 
-    
-    for(fi=m.face.begin(); fi!=oldendf; ++fi) 
-        if(!(*fi).IsD())
-        {
-            if(cb && (++step%percStep) == 0)
-                (*cb)(step/percStep, "Eroding...");
-            vv.clear();
-            nf.clear();
-            wtt.clear();
-            vv.push_back((*fi).V(0));
-            vv.push_back((*fi).V(1));
-            vv.push_back((*fi).V(2));
-            int n[3];
-            for(j=0; j<3; j++) {
-                if(ep(&(*fi), j)  && (!hasSelected || ((*fi).IsS() && (*fi).FFp(j)->IsS()))) {
-                	n[j] = ep.numFacesToAdd(&(*fi), j);
-                	if((*fi).V(j) < (*fi).V1(j) || (*fi).IsB(j)) 
-                		for(int p=1; p<n[j]+1; p++)
-                			vv.push_back(edge2Vert[vvi(vvpair((*fi).V(j), (*fi).V1(j)), p)]);
-                	else 
-                		for(int p=1; p<n[j]+1; p++)
-                			vv.push_back(edge2Vert[vvi(vvpair((*fi).V1(j), (*fi).V(j)), p)]);
-                }
-                else
-                	n[j] = 0;
-            }
-            
-            nf.push_back(&*fi);
-            int i;
-            for(i=1; i<=n[0]+n[1]+n[2]; i++) {
-                nf.push_back(&*lastf);
-                lastf++;
-                if(hasSelected || (*fi).IsS()) 
-                    (*nf[i]).SetS();
-            }
-            
-            if(tri::HasPerWedgeTexCoord(m)) {
-                for(i=0; i<3; ++i)
-                    wtt.push_back((*fi).WT(i));
-                for(j=1; j<=n[0]; j++)
-                    wtt.push_back(frpoi.WedgeInterp((*fi).WT(0), (*fi).WT(1), j/(n[0]+1)));
-                for(j=1; j<=n[1]; j++)
-                    wtt.push_back(frpoi.WedgeInterp((*fi).WT(1), (*fi).WT(2), j/(n[1]+1)));
-                for(j=1; j<=n[2]; j++)
-                    wtt.push_back(frpoi.WedgeInterp((*fi).WT(2), (*fi).WT(0), j/(n[2]+1)));
-            }
-            
-            int orgflag = (*fi).UberFlags();
-            for(j=0; j<3; j++)
-            	splitFaceEdge(m, nf, vv, wtt, n, j, orgflag);
-        }
-
-    assert(lastf == m.face.end());	
-    assert(!m.vert.empty());
-    for(fi=m.face.begin(); fi!=m.face.end(); ++fi) 
-        if(!(*fi).IsD()){
-            assert((*fi).V(0) >= &*m.vert.begin() && (*fi).V(0) <= &m.vert.back());
-            assert((*fi).V(1) >= &*m.vert.begin() && (*fi).V(1) <= &m.vert.back());
-            assert((*fi).V(2) >= &*m.vert.begin() && (*fi).V(2) <= &m.vert.back());
-    }
-    vcg::tri::UpdateTopology<CMeshO>::FaceFace(m);
-    vcg::tri::UpdateNormals<CMeshO>::PerVertexNormalizedPerFace(m);
-    
-    Log(0,"Created %i new vertexes and %i new faces.", newVertNum, newFaceNum);       // DEBUG
-    
-    return true;
-}
-
-
-// splits the current edge (z) of the current face creating numf[z] new faces.
-// actually the face edges are already splitted: the new vertexes (contained in vector vv) are 
-// already in their final position but the area need to be retriangulated 
-// (vector f  contains the pointers to all the faces needed to retriangulate the old face)
-void GeometryAgingPlugin::splitFaceEdge(CMeshO &m, vector<CMeshO::FacePointer> &f, vector<CMeshO::VertexPointer> &vv, vector<CMeshO::FaceType::TexCoordType> &wtt, int numf[3], int z, int orgflag) 
-{
-	if(numf[z] <= 0) return;
+	AgingEdgePred ep;					// edge predicate
+	CMeshO::FaceIterator fi, endfi;
+	CMeshO::VertexIterator vi;
+	int fcount = 1;
 	
-	int idx = 3;		// index of the first vertex of the curr face (the second will be idx+1)
-	int idx3;			// index of the third vertex of the curr face
-	int oldf = 0;		// face to split index
-	int newf = 1;		// first new face index
-	int vf0 = (z+1)%3;	// index of the vertex to move in face 0 (original face to split)
-        	        	
-	if(z == 0)
-		idx3 = 2;
-	else if(z == 1) {
-		if(numf[0] > 0) {
-			idx3 = 2 + numf[0];
-			oldf += numf[0];
-			newf += numf[0];
-		}
-		else
-			idx3 = 0;
-	}
-	else {
-		if(numf[0] > 0) {
-			idx3 = 3;
-			newf += numf[0] + numf[1];
-		}
-		else if(numf[1] > 0) {
-			idx3 = 2 + numf[1];
-			oldf += numf[1];
-			newf += numf[1];
-			vf0 = z;
-		}
-		else
-			idx3 = 1;
-	}
-        	    	
-	int i;
-	for(i=0; i<z; i++) idx += numf[i];
+	if(useQuality)
+		ep = AgingEdgePred(AgingEdgePred::QUALITY, edgeLenTreshold, qualityThreshold);
+	else
+		ep = AgingEdgePred(AgingEdgePred::ANGLE, edgeLenTreshold, angleThreshold);
 	
-	f.at(oldf)->V(vf0) = vv.at(idx);
-	if(tri::HasPerWedgeTexCoord(m))
-		f.at(oldf)->WT((z+1)%3) = wtt.at(idx);
-	if(idx > 2)
-		f.at(oldf)->ClearB((z+1)%3);
+	srand(time(NULL));
 	
-	for(i=0; i<numf[z]; i++, idx++, newf++) {
-		f.at(newf)->V(0) = vv.at(idx);
-		f.at(newf)->V(1) = vv.at((i==(numf[z]-1)?(z+1)%3:idx+1));
-		f.at(newf)->V(2) = vv.at(idx3);
-		if(tri::HasPerWedgeTexCoord(m)) {
-			f.at(newf)->WT(0) = wtt.at(idx);
-			f.at(newf)->WT(1) = wtt.at((i==(numf[z]-1)?(z+1)%3:idx+1));
-			f.at(newf)->WT(2) = wtt.at(idx3);
-		}
-		for(int j=0; j<3; j++) assert(f.at(newf)->V(j) != 0);
-		if(orgflag & (CMeshO::FaceType::BORDER0 << z))
-			f.at(newf)->SetB(0);
-		else
-			f.at(newf)->ClearB(0);
-		f.at(newf)->ClearB(1);
-		f.at(newf)->ClearB(2);
+	switch(ID(filter)) {
+		case FP_ERODE:
+			// refine needed edges
+			while(RefineE<CMeshO, MidPoint<CMeshO>, AgingEdgePred>(m.cm, MidPoint<CMeshO>(), ep, selected, cb))
+				vcg::tri::UpdateNormals<CMeshO>::PerVertexNormalizedPerFace(m.cm);
+			
+			// displace vertexes
+			for(fi = m.cm.face.begin(), endfi=m.cm.face.end(); fi != endfi; fi++) {
+				if(cb) (*cb)((fcount++/m.cm.fn)*100, "Aging...");
+				if((*fi).IsD()) continue;
+				for(int j=0; j<3; j++) {
+					if(ep.qaTest(face::Pos<CMeshO::FaceType>(&*fi,j))  && 
+					  (!selected || ((*fi).IsS() && (*fi).FFp(j)->IsS())) ) { 
+						double noise;							// noise value
+						Point3<CVertexO::ScalarType> dispDir;	// displacement direction
+						float angleFactor;
+						
+						noise = generateNoiseValue(chipStyle, (*fi).V(j)->P().X(), (*fi).V(j)->P().Y(), (*fi).V(j)->P().Z());
+						noise *= (noise>=0.0?1.0:-1.0);
+						
+						if((*fi).IsB(j)) {
+							dispDir = Point3<CVertexO::ScalarType>((*fi).N().Normalize());
+							angleFactor = 1.0;
+						}
+						else { 
+							if(useQuality)
+								dispDir = Point3<CVertexO::ScalarType>(((*fi).N() + (*fi).FFp(j)->N()).Normalize());
+							else
+								dispDir = Point3<CVertexO::ScalarType>((*fi).N().Normalize());
+							angleFactor = (sin(vcg::Angle((*fi).N(), (*fi).FFp(j)->N()))/2 + 0.5);
+						}
+						
+						// displacement offset
+						Point3<CVertexO::ScalarType> offset = -(dispDir * chipDepth * noise * angleFactor);
+						
+						// Self intersection test
+						/*bool selfInt=false;
+						for(CMeshO::FaceIterator f=m.cm.face.begin(); f!=m.cm.face.end(); f++) {
+							if((*f).IsD() || &(*f) == &(*fi)) continue;
+							if ((*f).FFp(0)!=&(*fi) && (*f).FFp(1)!=&(*fi) && (*f).FFp(2)!=&(*fi) &&
+									(*f).V(0)!=(*fi).V(0) && (*f).V(0)!=(*fi).V(1) && (*f).V(0)!=(*fi).V(2) &&
+									(*f).V(1)!=(*fi).V(0) && (*f).V(1)!=(*fi).V(1) && (*f).V(1)!=(*fi).V(2) &&
+									(*f).V(2)!=(*fi).V(0) && (*f).V(2)!=(*fi).V(1) && (*f).V(2)!=(*fi).V(2)) {
+								if (NoDivTriTriIsect((*f).V(0)->P(), (*f).V(1)->P(), (*f).V(2)->P(), 
+													 (*fi).V(j)->P()+offset, (*fi).V1(j)->P(), (*fi).V2(j)->P()) )
+									selfInt=true;
+							}
+							if(selfInt)
+								break;
+						}
+						
+						if(!selfInt)*/
+							(*fi).V(j)->P() += offset;
+					}
+				}
+			}
+			vcg::tri::UpdateNormals<CMeshO>::PerVertexNormalizedPerFace(m.cm);
+			
+			return true;
+		default:
+			assert(0);
 	}
 }
 
+
+// returns a noise value
+double GeometryAgingPlugin::generateNoiseValue(int style, const CVertexO::ScalarType &x, 
+											const CVertexO::ScalarType &y, const CVertexO::ScalarType &z)
+{
+	double noise = .0;
+	
+	switch(style) {
+		case SIMPLE:
+			noise = math::Perlin::Noise(x, y, z);
+			break;
+		case LINEAR:
+		case SINUSOIDAL:
+			for(int i=0; i<8; i++) {
+				double p = pow(2, i);
+				noise += math::Perlin::Noise(p*x, p*y, p*z) / p;
+			}
+			noise /= 2.0;
+			noise *= (noise>=0.0?1.0:-1.0);
+			break;
+	}
+
+	if(style == 2) {
+		noise /= 4.0;
+		noise = sin(x + noise);
+	}
+	
+	return noise;
+}
 
 Q_EXPORT_PLUGIN(GeometryAgingPlugin)
--- src/fgt/filter_aging/filter_aging.h
+++ src/fgt/filter_aging/filter_aging.h
@@ -30,121 +30,33 @@
 
 #include <meshlab/meshmodel.h>
 #include <meshlab/interfaces.h>
-//#include <vcg/complex/trimesh/refine.h>
+#include <vcg/complex/trimesh/refine.h>
 
-
-struct FractPoint : public std::unary_function<face::Pos<CMeshO::FaceType>, CMeshO::CoordType>
-{
-    void operator()(CMeshO::VertexType &nv, face::Pos<CMeshO::FaceType> ep, float relpos = 0.5f) {
-        nv.P() = ep.f->V(ep.z)->P() * (1.0f - relpos) + ep.f->V1(ep.z)->P() * relpos;
-        if(CMeshO::HasPerVertexNormal())
-            nv.N() = (ep.f->V(ep.z)->N() * (1.0f - relpos) + ep.f->V1(ep.z)->N() * relpos).Normalize();
-        if(CMeshO::HasPerVertexColor())
-            nv.C().lerp(ep.f->V(ep.z)->C(), ep.f->V1(ep.z)->C(), relpos);
-	}
-    
-	Color4<CMeshO::ScalarType> WedgeInterp(Color4<CMeshO::ScalarType> &c0, Color4<CMeshO::ScalarType> &c1, float relpos = 0.5f)
-    {
-        Color4<CMeshO::ScalarType> cc;
-        cc.lerp(c0, c1, relpos);
-        return cc;
-	}
-
-    template<class FL_TYPE>
-    TexCoord2<FL_TYPE, 1> WedgeInterp(TexCoord2<FL_TYPE, 1> &t0, TexCoord2<FL_TYPE, 1> &t1, float relpos = 0.5f)
-    {
-        TexCoord2<FL_TYPE, 1> tmp;
-        assert(t0.n() == t1.n());
-        tmp.n() = t0.n();
-        tmp.t() = t0.t() * (1.0f - relpos) + t1.t() * relpos;
-        return tmp;
-    }
-};
-
-
-class EdgePred
-{
-    public:
-        EdgePred(float angleTh, float edgeLenTh) {
-            this->angleTh = angleTh;
-            this->edgeLenTh = edgeLenTh;
-        }
-    
-        bool operator()(CMeshO::FacePointer f, int idx) {
-            if(f->IsB(idx)) return true;
-            
-            // the angle between the two face normals in degrees
-            // TODO: check non 2-manifold cases, it's all ok? or there are problems?
-            /*double ffangle = vcg::Angle(f->N(), f->FFp(idx)->N()) * 180 / M_PI;
-            
-            // the 2 points not shared between the 2 faces
-            CVertexO *f1p = f->V2(idx);
-            CVertexO *f2p = f->FFp(idx)->V2(f->FFi(idx));
-            
-            Point3<CVertexO::ScalarType> y, median;
-            
-            y = f->N().Normalize() ^ f->FFp(idx)->N().Normalize();
-            median = y ^ (Point3<CVertexO::ScalarType>(f1p->P() - f2p->P()));
-            */
-            /* There are always 2 cases wich produce the same angle value:
-                         ___|_           ____
-                        |               |  |
-                       -|               |-
-                
-               In the first case the edge lies in a concave area of the mesh
-               while in the second case it lies in a convex area.
-               We need a way to know wich is the current case.
-               This is done comparing ffangle with the angle between the 
-               normal to the current face and the median vector. 
-            */
-            //return (ffangle-angleThreshold >= -0.001  && vcg::Angle(f->N(), median) * 180 / M_PI < ffangle);
-            return false;
-        }
-        
-        int numFacesToAdd(CMeshO::FacePointer f, int idx) {
-            return (int)(Distance(f->V(idx)->P(), f->V1(idx)->P()) / edgeLenTh) - 1;
-        }
-        
-    protected:
-        float angleTh;
-        float edgeLenTh;
-};
+#include "edgepred.h"
 
 
 class GeometryAgingPlugin : public QObject, public MeshFilterInterface
 {
-    Q_OBJECT
-    Q_INTERFACES(MeshFilterInterface)
+	Q_OBJECT
+	Q_INTERFACES(MeshFilterInterface)
 
-    public:
-        enum {FP_ERODE};
-        typedef struct EP{
-            float lx;       // x offset along the surface of the face on the left side of the edge
-            float rx;       // x offset along the surface of the face on the right side of the edge
-            float yrel;     // y relative position between the two edge vertexes
-            EP(float yrel, float lx, float rx=0.0) {
-                this->yrel = yrel;
-                this->lx = lx;
-                this->rx = rx;
-            }
-        } EroderPoint;
-
-        GeometryAgingPlugin();
-        virtual ~GeometryAgingPlugin();
-        
-        virtual const QString filterInfo(FilterIDType filter);
-        virtual const QString filterName(FilterIDType filter);
-        virtual const PluginInfo &pluginInfo();
-        virtual const int getRequirements(QAction *) {return (MeshModel::MM_FACETOPO | MeshModel::MM_BORDERFLAG);}
-        virtual bool autoDialog(QAction *) {return true;}
-        virtual void initParameterSet(QAction *action, MeshModel &m, FilterParameterSet &params);
-        virtual bool applyFilter(QAction *filter, MeshModel &m, FilterParameterSet &params, vcg::CallBackPos *cb);
-        
-    protected:
-        bool hasSelected(MeshModel &m);
-        vector<EroderPoint>* generateEdgeEroder(CMeshO::FacePointer f, int z);
-        bool refineNeededEdges(CMeshO &m, FractPoint mid, EdgePred ep, bool hasSelected, CallBackPos *cb);
-        void splitFaceEdge(CMeshO &m, vector<CMeshO::FacePointer> &f, vector<CMeshO::VertexPointer> &vv, vector<CMeshO::FaceType::TexCoordType> &wtt, int numf[3], int z, int orgflag);
+	public:
+		enum {FP_ERODE};
+	
+		GeometryAgingPlugin();
+		virtual ~GeometryAgingPlugin();
+	
+		virtual const QString filterInfo(FilterIDType filter);
+		virtual const QString filterName(FilterIDType filter);
+		virtual const PluginInfo &pluginInfo();
+		virtual const int getRequirements(QAction *) {return (MeshModel::MM_FACETOPO | MeshModel::MM_BORDERFLAG);}
+		virtual bool autoDialog(QAction *) {return true;}
+		virtual void initParameterSet(QAction *action, MeshModel &m, FilterParameterSet &params);
+		virtual bool applyFilter(QAction *filter, MeshModel &m, FilterParameterSet &params, vcg::CallBackPos *cb);
+		
+	protected:
+		enum {SIMPLE=0, LINEAR=1, SINUSOIDAL=2};
+		double generateNoiseValue(int style, const CVertexO::ScalarType &x, const CVertexO::ScalarType &y, const CVertexO::ScalarType &z);
 };
 
 
