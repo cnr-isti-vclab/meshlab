5a54062a7325a4cc423ffdf8459a5570acdc32b6
Daniele Panozzo danielepanozzo <daniele.panozzo@gmail.com>
2008-04-11 04:19:29 +0000

    added modified butterfly removed selective refinement code cleanup

---
+++ src/meshlabplugins/editrgbtri/subDialog.h
@@ -0,0 +1,41 @@
+#ifndef SUBDIALOG_H_
+#define SUBDIALOG_H_
+
+/****************************************************************************
+* Rgb Triangulations Plugin                                                 *
+*                                                                           *
+* Author: Daniele Panozzo (daniele.panozzo@gmail.com)                       *
+* Copyright(C) 2007                                                         *
+* DISI - Department of Computer Science                                     *
+* University of Genova                                                      *
+*                                                                           *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+****************************************************************************/
+
+#include "ui_subDialog.h"
+#include <QDialog>
+
+namespace rgbt
+{
+
+class SubDialog : public QDialog, public Ui::subDialog
+{
+public:
+	SubDialog();
+	virtual ~SubDialog();
+};
+
+}
+
+#endif /*SUBDIALOG_H_*/
