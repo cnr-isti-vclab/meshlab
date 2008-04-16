60fbb09f1a149174cbaa4980f501e941c82c971e
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2008-04-16 18:40:31 +0000

    Added class field to know if we are working on a selection. Modified operator() method with an additional check when working on a selection. Added a method that returns the predicate type. Added some comments.

--- src/fgt/filter_aging/edgepred.h
+++ src/fgt/filter_aging/edgepred.h
@@ -15,29 +15,39 @@ class AgingEdgePred
 			lenp.thr2 = 1.0;
 			this->thVal = 1.0;
 			this->type = QUALITY;
+			this->selection = false;
 		}
 		
-		AgingEdgePred(int type, float lenTh, float thVal = 0.0) {
+		AgingEdgePred(int type, bool sel, float lenTh, float thVal = 0.0) {
 			lenp = EdgeLen <CMeshO, CMeshO::ScalarType>();
 			lenp.thr2 = lenTh * lenTh;
 			this->thVal = thVal;
 			this->type = type;
+			this->selection = sel;
 		}
-	
+		
+		// Main method to test predicates over edges (used by RefineE function)
 		bool operator()(face::Pos<CMeshO::FaceType> ep) const {
+			if(selection && !ep.f->IsV() && !ep.f->FFp(ep.z)->IsV()) return false;
 			return (lenp(ep) && qaEdgeTest(ep));
 		}
 		
+		// Tests angle predicate over the edge if type is angle; if type is quality 
+		// tests quality predicate but only on the first of the two vertexes of the edge
 		bool qaVertTest(face::Pos<CMeshO::FaceType> ep) const {
 			return (type==ANGLE?testAngle(ep):(type==QUALITY?(ep.f->V(ep.z)->Q()>thVal):false));
 		}
 		
+		// Returns the type of the predicate
+		int getType() { return type; }
+		
 		
 	protected:
-		EdgeLen<CMeshO, CMeshO::ScalarType> lenp;
-		float thVal;
-		int type;
-		
+		EdgeLen<CMeshO, CMeshO::ScalarType> lenp;		// edge len predicate object
+		float thVal;					// edge value threshold (angle or quality)
+		int type;						// predicate type (angle or quality)
+		bool selection;				// working on the selection only
+		 
 		bool qaEdgeTest(face::Pos<CMeshO::FaceType> ep) const {
 			return (type==ANGLE?testAngle(ep):(type==QUALITY?testQuality(ep):false));
 		}
