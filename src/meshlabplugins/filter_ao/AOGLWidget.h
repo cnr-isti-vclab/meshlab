1e439ae291b74628bb68186986bc44602b76db99
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2008-05-29 21:13:58 +0000

    Corrected wrong copyright information in the header

--- src/meshlabplugins/filter_ao/AOGLWidget.h
+++ src/meshlabplugins/filter_ao/AOGLWidget.h
@@ -1,11 +1,26 @@
-/*
- *  editAlignPair.h
- *  editalign_debug
- *
- *  Created by Paolo Cignoni on 11/6/07.
- *  Copyright 2007 __MyCompanyName__. All rights reserved.
- *
- */
+/****************************************************************************
+* MeshLab                                                           o o     *
+* A versatile mesh processing toolbox                             o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2005                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+
 #ifndef __VCG_AOGLWIDGET
 #define __VCG_AOGLWIDGET
 
