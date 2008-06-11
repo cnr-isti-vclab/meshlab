cac115148b96842f98405a77e662ab14e0559e92
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2008-06-11 11:43:36 +0000

    Added copyright information for the files derived from the public domain TSAI calibration library.

--- src/meshlabplugins/edit_phototexturing/src/Tsai/cal_main.c
+++ src/meshlabplugins/edit_phototexturing/src/Tsai/cal_main.c
@@ -1,3 +1,31 @@
+/****************************************************************************
+* MeshLab                                                           o o     *
+* A versatile mesh processing toolbox                             o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2008                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+/* 
+  This file derives from the public domain tsai lib. 
+  The original author of the Tsai lib released it saying that it could be 
+  modified and re-distributed as needed, 
+*/
+
 /*******************************************************************************\
 *                                                                               *
 * This file contains routines for calibrating Tsai's 11 parameter camera model. *
