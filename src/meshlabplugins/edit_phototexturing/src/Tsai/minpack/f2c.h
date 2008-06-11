c98b959af07e61422ad333adedf71a6f6acc2a40
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2008-06-11 11:18:01 +0000

    Added copyright information for the files derived from the public domain library MINPACK.

--- src/meshlabplugins/edit_phototexturing/src/Tsai/minpack/f2c.h
+++ src/meshlabplugins/edit_phototexturing/src/Tsai/minpack/f2c.h
@@ -1,3 +1,30 @@
+/****************************************************************************
+* MeshLab                                                           o o     *
+* A versatile mesh processing toolbox                             o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2008                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+/* This file derives from the public domain MINPACK lib. 
+   MINPACK is a free, portable library for solving nonlinear systems
+   of equations and nonlinear least squares problems. 
+*/
+
 /* f2c.h  --  Standard Fortran to C header file 
 
 	f2c is a Fortran to C converter under development by
--- src/meshlabplugins/edit_phototexturing/src/Tsai/minpack/lmpar.c
+++ src/meshlabplugins/edit_phototexturing/src/Tsai/minpack/lmpar.c
@@ -1,3 +1,30 @@
+/****************************************************************************
+* MeshLab                                                           o o     *
+* A versatile mesh processing toolbox                             o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2008                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+/* This file derives from the public domain MINPACK lib. 
+   MINPACK is a free, portable library for solving nonlinear systems
+   of equations and nonlinear least squares problems. 
+*/
+
 /* lmpar.f -- translated by f2c (version of 17 January 1992  0:17:58).
    You must link the resulting object file with the libraries:
 	-lf77 -li77 -lm -lc   (in that order)
