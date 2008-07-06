277d106c1238ae5ad777f16b2cc825b55276a9f5
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2008-07-06 21:06:18 +0000

    Color Processing filters: Filling - done Invert - done tresholding - done Brightness, contrast and combinations - done Gamma - done (needs some parameters tuning) Levels - done Colourisation - done (needs some parameters tuning) All filters works on the original per vertex color, avoiding summing effects of filters. All the code needs refactoring and completion of comments and filters descriptions.

--- src/fgt/filter_colorproc/filter_colorproc.cpp
+++ src/fgt/filter_colorproc/filter_colorproc.cpp
@@ -27,6 +27,7 @@
 
 #include <vcg/complex/trimesh/update/flag.h>
 #include <vcg/complex/trimesh/update/color.h>
+#include <vcg/space/colorspace.h>
 
 #include <stdlib.h>
 #include <time.h>
@@ -35,31 +36,43 @@ using namespace std;
 using namespace vcg;
 
 FilterColorProc::FilterColorProc()
-
 {
-  typeList << CP_COLOR_FILLING<<CP_TRESHOLDING<<CP_BRIGHTNESS<<CP_CONTRAST<<CP_GAMMA;
+  typeList << CP_FILLING
+           << CP_INVERT
+           << CP_TRESHOLDING
+           << CP_BRIGHTNESS
+           << CP_CONTRAST
+           << CP_CONTR_BRIGHT
+           << CP_GAMMA
+           << CP_LEVELS
+           << CP_COLOURISATION;
 
   FilterIDType tt;
   foreach(tt , types())
-	    actionList << new QAction(filterName(tt), this);
+    actionList << new QAction(filterName(tt), this);
 
 }
 
-FilterColorProc::~FilterColorProc() {
+FilterColorProc::~FilterColorProc()
+{
 	for (int i = 0; i < actionList.count() ; i++ )
 		delete actionList.at(i);
 }
 
 const QString FilterColorProc::filterName(FilterIDType filter)
 {
- switch(filter)
+  switch(filter)
   {
-      case CP_COLOR_FILLING : return "Color Filling";
-      case CP_TRESHOLDING : return "Tresholding";
-      case CP_BRIGHTNESS : return "Brightness";
-      case CP_CONTRAST : return "Contrast";
-      case CP_GAMMA : return "Gamma";
-   default: assert(0);
+    case CP_FILLING : return "Filling";
+    case CP_TRESHOLDING : return "Tresholding";
+    case CP_BRIGHTNESS : return "Brightness";
+    case CP_CONTRAST : return "Contrast";
+    case CP_CONTR_BRIGHT : return "Contrast and Brightness";
+    case CP_GAMMA : return "Gamma";
+    case CP_INVERT : return "Invert";
+    case CP_LEVELS : return "Levels";
+    case CP_COLOURISATION : return "Colourisation";
+    default: assert(0);
   }
   return QString("error!");
 }
@@ -68,12 +81,16 @@ const QString FilterColorProc::filterInfo(FilterIDType filterId)
 {
   switch(filterId)
   {
-    case CP_COLOR_FILLING : return "Color Filling";
-      case CP_TRESHOLDING : return "Tresholding";
-      case CP_BRIGHTNESS : return "Brightness";
-      case CP_CONTRAST : return "Contrast";
-      case CP_GAMMA : return "Gamma";
-	default: assert(0);
+    case CP_FILLING : return "Fill the whole mesh with a color choosed by the user.";
+    case CP_TRESHOLDING : return "All the vertex of the mesh above a certain threshold level becomes white, others black. Treshold = 255 -> all black; Treshold = 0 -> all white;";
+    case CP_BRIGHTNESS : return "Adds or subtracts luminance to the mesh. Brightness = 255 -> all white; Brightness = -255 -> all black;";
+    case CP_CONTRAST : return "Contrast";
+    case CP_CONTR_BRIGHT : return "Contrast and Brightness";
+    case CP_GAMMA : return "Gamma";
+    case CP_INVERT : return "Invert the colors of the mesh.";
+    case CP_LEVELS : return "Levels";
+    case CP_COLOURISATION : return "Colourisation";
+    default: assert(0);
   }
   return QString("error!");
 }
@@ -81,51 +98,238 @@ const QString FilterColorProc::filterInfo(FilterIDType filterId)
 
 const PluginInfo &FilterColorProc::pluginInfo()
 {
-   static PluginInfo ai;
-   ai.Date=tr( __DATE__ );
-	 ai.Version = tr("0.1");
-	 ai.Author = ("Francesco Tonarelli");
-   return ai;
+  static PluginInfo ai;
+  ai.Date=tr( __DATE__ );
+  ai.Version = tr("0.1");
+  ai.Author = ("Francesco Tonarelli");
+  return ai;
 }
 
 const int FilterColorProc::getRequirements(QAction *action)
 {
-    return 0;
+  return 0;
 }
 
 void FilterColorProc::initParameterSet(QAction *a, MeshModel &m, FilterParameterSet & par)
 {
+  //first of all make a copy of color per vertex. This is done to undo
+  //the filters changes, but also to allow that filters always work on
+  //the original colors, avoiding sum of sequential transformation
+  m.storeVertexColor();
+
 	switch(ID(a))
   {
-	case CP_COLOR_FILLING: {
-			float r = 255, g = 255, b = 255;
-			par.addFloat("r", r, "Red:", "Set the <b><i>Red</b></i> component of the color.");
-			par.addFloat("g", g, "Green:", "Set the <b><i>Green</b></i> component of the color.");
-			par.addFloat("b", b, "Blue:", "Set the <b><i>Blue</b></i> component of the color.");
+    case CP_FILLING:
+    {
+			int r = 255, g = 255, b = 255;
+			par.addInt("r", r, "Red:", "Set the <b><i>Red</b></i> component of the color.");
+			par.addInt("g", g, "Green:", "Set the <b><i>Green</b></i> component of the color.");
+			par.addInt("b", b, "Blue:", "Set the <b><i>Blue</b></i> component of the color.");
 			break;
 		}
-	default: assert(0);
+    case CP_TRESHOLDING:
+    {
+      float treshold = 128.0f;
+      par.addFloat("treshold", treshold, "Treshold:", "Set the treshold between shadows (black) and lights (white).");
+      break;
+    }
+    case CP_BRIGHTNESS:
+    {
+      int brightness = 0;
+      par.addInt("brightness", brightness, "Brightness:", "Set the amount of brightness (or darkness) that will be added to the color.");
+      break;
+    }
+    case CP_CONTRAST:
+    {
+      float factor = 1.0f;
+      par.addFloat("factor", factor, "Contrast factor:", "Set the amount of contrast of the mesh.");
+      break;
+    }
+    case CP_CONTR_BRIGHT:
+    {
+      int brightness = 0;
+      float factor = 1.0f;
+      par.addInt("brightness", brightness, "Brightness:", "Set the amount of brightness (or darkness) that will be added to the color.");
+      par.addFloat("factor", factor, "Contrast factor:", "Set the amount of contrast of the mesh.");
+      break;
+    }
+    case CP_GAMMA :
+    {
+      float gamma = 1.0f;
+      par.addFloat("gamma", gamma, "Gamma:", "Provides standard Gamma correction, according to the relation: display_intensity = pixel_value^gamma.");
+      break;
+    }
+    case CP_LEVELS:
+    {
+			int in_min = 0, in_max = 255, out_min = 0, out_max = 255;
+			float gamma = 1;
+			par.addInt("in_min", in_min, "Input min:", "");
+			par.addInt("in_max", in_max, "Input max:", "");
+			par.addInt("out_min", out_min, "Output min:", "");
+			par.addInt("out_max", out_max, "Output max:", "");
+			par.addFloat("gamma", gamma, "Gamma:", "");
+			break;
+		}
+		case CP_COLOURISATION:
+		{
+		  float intensity = 1.0f;
+      double hue, luminance, saturation;
+			ColorSpace<unsigned char>::RGBtoHSL(1.0, 1.0, 1.0, hue, saturation, luminance);
+			par.addFloat("hue", (float)hue, "Hue:", "Change the <b><i>Hue</b></i> of the mesh.");
+			par.addFloat("saturation", (float)saturation, "Saturation:", "Change the <b><i>Saturation</b></i> of the mesh.");
+			par.addFloat("luminance", (float)luminance, "Luminance:", "Change the <b><i>Luminance</b></i> of the mesh.");
+			par.addFloat("intensity", intensity, "Intensity:", "");
+			break;
+    }
+    default: assert(0);
 	}
 }
 
 bool FilterColorProc::applyFilter(QAction *filter, MeshModel &m, FilterParameterSet & par, vcg::CallBackPos * cb)
 {
-    switch(ID(filter))
+  switch(ID(filter))
+  {
+    case CP_FILLING:
+    {
+      int r = math::Clamp<int>(par.getInt("r"), 0, 255);
+      int g = math::Clamp<int>(par.getInt("g"), 0, 255);
+      int b = math::Clamp<int>(par.getInt("b"), 0, 255);
+      Color4b new_col = Color4b(r,g,b,1);
+
+      int  v_num = Filling(m, new_col);
+
+      if(v_num==0)
       {
-        case CP_COLOR_FILLING: {
-                int r = (int)(math::Clamp(par.getFloat("r"), 0.0f, 255.0f));
-                int g = (int)(math::Clamp(par.getFloat("g"), 0.0f, 255.0f));
-                int b = (int)(math::Clamp(par.getFloat("b"), 0.0f, 255.0f));
-                Color4b new_col = Color4b(r,g,b,1);
-			CMeshO::VertexIterator vi;
-                for(vi=m.cm.vert.begin();vi!=m.cm.vert.end();++vi)
-                if(!(*vi).IsD())
-			if((*vi).IsS()) {(*vi).C() = new_col; }
-			else (*vi).C() = new_col;
-                return true;
-            }
-        default: assert(0);
-        }
+        errorMessage = "The mesh doesn't contains any vertex.";
+        return false;
+      }
+
+      return true;
+    }
+    case CP_TRESHOLDING:
+    {
+      float treshold = math::Clamp(par.getFloat("treshold"), 0.0f, 255.0f);
+
+      int  v_num = Tresholding(m, treshold);
+
+      if(v_num==0)
+      {
+        errorMessage = "The mesh doesn't contains any vertex.";
+        return false;
+      }
+
+      return true;
+    }
+    case CP_BRIGHTNESS:
+    {
+      int brightness = math::Clamp<int>(par.getInt("brightness"), -255, 255);
+
+      int  v_num = Brighting(m, brightness);
+
+      if(v_num==0)
+      {
+        errorMessage = "The mesh doesn't contains any vertex.";
+        return false;
+      }
+
+      return true;
+    }
+    case CP_CONTRAST:
+    {
+      float factor = math::Clamp<float>(par.getFloat("factor"), 1.0f/6.0f, 6.0f);
+
+      int  v_num = Contrast(m, factor);
+
+      if(v_num==0)
+      {
+        errorMessage = "The mesh doesn't contains any vertex.";
+        return false;
+      }
+
+      return true;
+    }
+    case CP_CONTR_BRIGHT:
+    {
+      int brightness = math::Clamp<int>(par.getInt("brightness"), -255, 255);
+      float factor = math::Clamp<float>(par.getFloat("factor"), 1.0f/6.0f, 6.0f);
+
+      int  v_num = ContrastBrightness(m, factor, brightness);
+
+      if(v_num==0)
+      {
+        errorMessage = "The mesh doesn't contains any vertex.";
+        return false;
+      }
+
+      return true;
+    }
+    case CP_GAMMA :
+    {
+      float gamma = math::Clamp<float>(par.getFloat("gamma"), 0.5f, 1.5f);  //valori sensati???
+
+      int  v_num = Gamma(m, gamma);
+
+      if(v_num==0)
+      {
+        errorMessage = "The mesh doesn't contains any vertex.";
+        return false;
+      }
+
+      return true;
+    }
+    case CP_INVERT :
+    {
+      int  v_num = Invert(m);
+
+      if(v_num==0)
+      {
+        errorMessage = "The mesh doesn't contains any vertex.";
+        return false;
+      }
+
+      return true;
+    }
+    case CP_LEVELS:
+    {
+			float gamma = math::Clamp<float>(par.getFloat("gamma"), 0.5f, 1.5f);  //valori sensati???
+      int  in_min = par.getInt("in_min");
+      int  in_max = par.getInt("in_max");
+      int  out_min = par.getInt("out_min");
+      int  out_max = par.getInt("out_max");
+
+      int  v_num = Levels(m, gamma, in_min, in_max, out_min, out_max);
+
+      if(v_num==0)
+      {
+        errorMessage = "The mesh doesn't contains any vertex.";
+        return false;
+      }
+
+      return true;
+		}
+		case CP_COLOURISATION:
+		{
+			float luminance = math::Clamp<float>(par.getFloat("luminance"), 0.0f, 1.0f);
+			float saturation = math::Clamp<float>(par.getFloat("saturation"), 0.0f, 1.0f);
+      float hue = math::Clamp<float>(par.getFloat("hue"), 0.0f, 1.0f);
+      float intensity = math::Clamp<float>(par.getFloat("intensity"), 0.0f, 1.0f);
+
+      double r, g, b;
+
+      ColorSpace<unsigned char>::HSLtoRGB( (double)hue, (double)saturation, (double)luminance, r, g, b);
+      Color4b color = Color4b((int)(r*255), (int)(g*255), (int)(b*255), 1);
+      int  v_num = Colourisation(m, color, intensity);
+
+      if(v_num==0)
+      {
+        errorMessage = "The mesh doesn't contains any vertex.";
+        return false;
+      }
+
+      return true;
+    }
+    default: assert(0);
+  }
 	return false;
 }
 
@@ -133,12 +337,27 @@ const MeshFilterInterface::FilterClass FilterColorProc::getClass(QAction *a)
 {
   switch(ID(a))
   {
-    case CP_COLOR_FILLING :
+    case CP_FILLING :
     case CP_TRESHOLDING :
     case CP_BRIGHTNESS :
     case CP_CONTRAST :
-    case CP_GAMMA : return MeshFilterInterface::VertexColoring;
-	default: assert(0);
+    case CP_CONTR_BRIGHT :
+    case CP_GAMMA :
+    case CP_INVERT :
+    case CP_COLOURISATION :
+    case CP_LEVELS : return MeshFilterInterface::VertexColoring;
+    default: assert(0);
   }
 }
+
+bool FilterColorProc::autoDialog(QAction *a)
+{
+  switch(ID(a))
+  {
+    case CP_INVERT : return false;
+    default : return true;
+  }
+  assert(0);
+}
+
 Q_EXPORT_PLUGIN(FilterColorProc)
--- src/fgt/filter_colorproc/filter_colorproc.h
+++ src/fgt/filter_colorproc/filter_colorproc.h
@@ -31,6 +31,7 @@
 #include <meshlab/meshmodel.h>
 #include <meshlab/interfaces.h>
 
+#include <math.h>
 
 class FilterColorProc : public QObject, public MeshFilterInterface
 {
@@ -38,22 +39,397 @@ class FilterColorProc : public QObject, public MeshFilterInterface
 	Q_INTERFACES(MeshFilterInterface)
 
 	public:
-		enum { CP_COLOR_FILLING,CP_TRESHOLDING,CP_BRIGHTNESS,CP_CONTRAST,CP_GAMMA };
+		enum { CP_FILLING,
+           CP_TRESHOLDING,
+           CP_BRIGHTNESS,
+           CP_CONTRAST,
+           CP_CONTR_BRIGHT,
+           CP_GAMMA,
+           CP_LEVELS,
+           CP_INVERT,
+           CP_COLOURISATION };
 
 		FilterColorProc();
 		~FilterColorProc();
 
-        virtual const FilterClass getClass(QAction *);
+    virtual const FilterClass getClass(QAction *);
 		virtual const QString filterName(FilterIDType filter);
 		virtual const QString filterInfo(FilterIDType filter);
 		virtual const PluginInfo &pluginInfo();
 
 		virtual const int getRequirements(QAction *);
 
-		virtual bool autoDialog(QAction *) {return true;}
+		virtual bool autoDialog(QAction *);
 		virtual void initParameterSet(QAction *,MeshModel &/*m*/, FilterParameterSet & /*parent*/);
-		virtual bool applyFilter(QAction *filter, MeshModel &m, FilterParameterSet & /*parent*/, vcg::CallBackPos * cb) ;
+		virtual bool applyFilter(QAction *filter, MeshModel &m, FilterParameterSet & /*parent*/, vcg::CallBackPos * cb);
+
+    //Apply the color filling filter, with the given color, to the mesh.
+    //This don't need to work on the original per vertex color.
+		static int Filling(MeshModel &m, vcg::Color4b c)
+    {
+      int counter=0;
+
+      CMeshO::VertexIterator vi;
+      for(vi=m.cm.vert.begin();vi!=m.cm.vert.end();++vi) //scan all the vertex...
+      {
+        if(!(*vi).IsD()) //if it has not been deleted...
+        {
+          if(m.cm.sfn!=0) //mesh has a selected region, work just on it
+          {
+            if((*vi).IsS()) //if this vertex has been selected, do transormation
+            {
+              (*vi).C() = c;
+              ++counter;
+            }
+          }
+          else //mesh has not a selected region, transorm all vertex
+          {
+            (*vi).C() = c;
+            ++counter;
+          }
+        }
+      }
+      return counter;
+    }
+
+    //Apply the tresholding filter, with the given treshold, to the mesh.
+    static int Tresholding(MeshModel &m, float treshold)
+    {
+      int counter=0;
+
+      //security check: arrays of differents sizes means that something wrong copying
+      //original colors. we can't work out the filter...
+      assert(m.cm.vert.size()==m.originalVertexColor.size());
+
+      std::vector<vcg::Color4b>::iterator ci;
+      CMeshO::VertexIterator vi;
+      for(vi=m.cm.vert.begin(),ci=m.originalVertexColor.begin();vi!=m.cm.vert.end();++vi,++ci) //scan all the vertex...
+      {
+        if(!(*vi).IsD()) //if it has not been deleted...
+        {
+          if(m.cm.sfn!=0) //mesh has a selected region, work just on it
+          {
+            if((*vi).IsS()) //if this vertex has been selected, do transormation
+            {
+              float value = compute_lightness((*ci));
+
+              if(value<=treshold) (*vi).C() = vcg::Color4b(0,0,0,1);
+              else (*vi).C() = vcg::Color4b(255,255,255,1);
+              ++counter;
+            }
+          }
+          else //mesh has not a selected region, transorm all vertex
+          {
+            float value = compute_lightness((*ci));
+
+            if(value<=treshold) (*vi).C() = vcg::Color4b(0,0,0,1);
+            else (*vi).C() = vcg::Color4b(255,255,255,1);
+            ++counter;
+          }
+        }
+      }
+      return counter;
+    }
+
+    //Apply the brightness filter, with the given amount, to the mesh.
+    static int Brighting(MeshModel &m, int amount)
+    {
+      int counter=0;
+
+      //security check: arrays of differents sizes means that something wrong copying
+      //original colors. we can't work out the filter...
+      assert(m.cm.vert.size()==m.originalVertexColor.size());
+
+      std::vector<vcg::Color4b>::iterator ci;
+      CMeshO::VertexIterator vi;
+      for(vi=m.cm.vert.begin(),ci=m.originalVertexColor.begin();vi!=m.cm.vert.end();++vi,++ci) //scan all the vertex...
+      {
+        if(!(*vi).IsD()) //if it has not been deleted...
+        {
+          if(m.cm.sfn!=0) //mesh has a selected region, work just on it
+          {
+            if((*vi).IsS()) //if this vertex has been selected, do transormation
+            {
+              (*vi).C() = color_add((*ci),amount);
+              ++counter;
+            }
+          }
+          else //mesh has not a selected region, transorm all vertex
+          {
+            (*vi).C() = color_add((*ci),amount);
+            ++counter;
+          }
+        }
+      }
+      return counter;
+    }
+
+    static int Contrast(MeshModel &m, float factor)
+    {
+      int counter=0;
+
+      //security check: arrays of differents sizes means that something wrong copying
+      //original colors. we can't work out the filter...
+      assert(m.cm.vert.size()==m.originalVertexColor.size());
+
+      std::vector<vcg::Color4b>::iterator ci;
+      CMeshO::VertexIterator vi;
+      for(vi=m.cm.vert.begin(),ci=m.originalVertexColor.begin();vi!=m.cm.vert.end();++vi,++ci) //scan all the vertex...
+      {
+        if(!(*vi).IsD()) //if it has not been deleted...
+        {
+          if(m.cm.sfn!=0) //mesh has a selected region, work just on it
+          {
+            if((*vi).IsS()) //if this vertex has been selected, do transormation
+            {
+              (*vi).C() = color_mul((*ci),factor);
+              ++counter;
+            }
+          }
+          else //mesh has not a selected region, transorm all vertex
+          {
+            (*vi).C() = color_mul((*ci),factor);
+            ++counter;
+          }
+        }
+      }
+      return counter;
+    }
+
+    static int ContrastBrightness(MeshModel &m, float factor, int amount)
+    {
+      int counter=0;
+
+      //security check: arrays of differents sizes means that something wrong copying
+      //original colors. we can't work out the filter...
+      assert(m.cm.vert.size()==m.originalVertexColor.size());
+
+      std::vector<vcg::Color4b>::iterator ci;
+      CMeshO::VertexIterator vi;
+      for(vi=m.cm.vert.begin(),ci=m.originalVertexColor.begin();vi!=m.cm.vert.end();++vi,++ci) //scan all the vertex...
+      {
+        if(!(*vi).IsD()) //if it has not been deleted...
+        {
+          if(m.cm.sfn!=0) //mesh has a selected region, work just on it
+          {
+            if((*vi).IsS()) //if this vertex has been selected, do transormation
+            {
+              (*vi).C() = color_mul_add((*ci),factor,amount);
+              ++counter;
+            }
+          }
+          else //mesh has not a selected region, transorm all vertex
+          {
+            (*vi).C() = color_mul_add((*ci),factor,amount);
+            ++counter;
+          }
+        }
+      }
+      return counter;
+    }
+
+    //Apply the gamma correction filter, with the given gamma exponet, to the mesh.
+    static int Gamma(MeshModel &m, float gamma)
+    {
+      int counter=0;
+
+      //security check: arrays of differents sizes means that something wrong copying
+      //original colors. we can't work out the filter...
+      assert(m.cm.vert.size()==m.originalVertexColor.size());
+
+      std::vector<vcg::Color4b>::iterator ci;
+      CMeshO::VertexIterator vi;
+      for(vi=m.cm.vert.begin(),ci=m.originalVertexColor.begin();vi!=m.cm.vert.end();++vi,++ci) //scan all the vertex...
+      {
+        if(!(*vi).IsD()) //if it has not been deleted...
+        {
+          if(m.cm.sfn!=0) //mesh has a selected region, work just on it
+          {
+            if((*vi).IsS()) //if this vertex has been selected, do transormation
+            {
+              (*vi).C() = color_gamma((*ci),gamma);
+              ++counter;
+            }
+          }
+          else //mesh has not a selected region, transorm all vertex
+          {
+            (*vi).C() = color_gamma((*ci),gamma);
+            ++counter;
+          }
+        }
+      }
+      return counter;
+    }
+
+    //Invert the rgb components of the color.
+    //This don't need to work on the original per vertex color.
+    static int Invert(MeshModel &m)
+    {
+      int counter=0;
+
+      CMeshO::VertexIterator vi;
+      for(vi=m.cm.vert.begin();vi!=m.cm.vert.end();++vi) //scan all the vertex...
+      {
+        if(!(*vi).IsD()) //if it has not been deleted...
+        {
+          if(m.cm.sfn!=0) //mesh has a selected region, work just on it
+          {
+            if((*vi).IsS()) //if this vertex has been selected, do transormation
+            {
+              (*vi).C() = color_invert((*vi).C());
+              ++counter;
+            }
+          }
+          else //mesh has not a selected region, transorm all vertex
+          {
+            (*vi).C() = color_invert((*vi).C());
+            ++counter;
+          }
+        }
+      }
+      return counter;
+    }
+
+
+    static int Levels(MeshModel &m, float gamma, int in_min, int in_max, int out_min, int out_max)
+    {
+      int counter=0;
+
+      //security check: arrays of differents sizes means that something wrong copying
+      //original colors. we can't work out the filter...
+      assert(m.cm.vert.size()==m.originalVertexColor.size());
+
+      std::vector<vcg::Color4b>::iterator ci;
+      CMeshO::VertexIterator vi;
+      for(vi=m.cm.vert.begin(),ci=m.originalVertexColor.begin();vi!=m.cm.vert.end();++vi,++ci) //scan all the vertex...
+      {
+        if(!(*vi).IsD()) //if it has not been deleted...
+        {
+          if(m.cm.sfn!=0) //mesh has a selected region, work just on it
+          {
+            if((*vi).IsS()) //if this vertex has been selected, do transormation
+            {
+              (*vi).C() = color_levels((*ci),gamma, in_min, in_max, out_min, out_max);
+              ++counter;
+            }
+          }
+          else //mesh has not a selected region, transorm all vertex
+          {
+            (*vi).C() = color_levels((*ci),gamma, in_min, in_max, out_min, out_max);
+            ++counter;
+          }
+        }
+      }
+      return counter;
+    }
+
+    static int Colourisation(MeshModel &m, vcg::Color4b c,float intensity)
+    {
+      int counter=0;
+
+      //security check: arrays of differents sizes means that something wrong copying
+      //original colors. we can't work out the filter...
+      assert(m.cm.vert.size()==m.originalVertexColor.size());
+
+      std::vector<vcg::Color4b>::iterator ci;
+      CMeshO::VertexIterator vi;
+      for(vi=m.cm.vert.begin(),ci=m.originalVertexColor.begin();vi!=m.cm.vert.end();++vi,++ci) //scan all the vertex...
+      {
+        if(!(*vi).IsD()) //if it has not been deleted...
+        {
+          if(m.cm.sfn!=0) //mesh has a selected region, work just on it
+          {
+            if((*vi).IsS()) //if this vertex has been selected, do transormation
+            {
+              (*vi).C() = color_apply_diff((*ci), c, intensity);
+              ++counter;
+            }
+          }
+          else //mesh has not a selected region, transorm all vertex
+          {
+            (*vi).C() = color_apply_diff((*ci), c,intensity);
+            ++counter;
+          }
+        }
+      }
+      return counter;
+    }
+    //Computes the luminance value for a specified color.
+    static float compute_lightness(vcg::Color4b c)
+    {
+      float min_rgb = vcg::math::Min((float)c[0],(float)c[1]);
+      min_rgb = vcg::math::Min(min_rgb,(float)c[2]);
+      float max_rgb = vcg::math::Max((float)c[0],(float)c[1]);
+      max_rgb = vcg::math::Max(max_rgb,(float)c[2]);
+      return (max_rgb + min_rgb)/2;
+    }
+
+    //Adds an integer amount to rgb components of the color.
+    static vcg::Color4b color_add(vcg::Color4b c, int amount)
+    {
+      return vcg::Color4b( vcg::math::Clamp<int>(c[0]+amount,0,255),
+                           vcg::math::Clamp<int>(c[1]+amount,0,255),
+                           vcg::math::Clamp<int>(c[2]+amount,0,255), 1 );
+    }
+
+    //Subtracts a middle value, multiplies the rgb components of the color for a factor,
+    //and adds the middle value back.This is used for Contrast operation.
+    static vcg::Color4b color_mul(vcg::Color4b c, float factor)
+    {
+      return vcg::Color4b( (int)vcg::math::Clamp<float>((c[0] - 128)*factor + 128,0.0f,255.0f),
+                           (int)vcg::math::Clamp<float>((c[1] - 128)*factor + 128,0.0f,255.0f),
+                           (int)vcg::math::Clamp<float>((c[2] - 128)*factor + 128,0.0f,255.0f), 1 );
+    }
+
+    //This is a composition of color_mul() and color_add(), for Contrast&Brightness filter.
+    //The result is clamped just one time after all computations; this get a more accurate
+    //result respect to composition of color_mul() and color_add().
+    static vcg::Color4b color_mul_add(vcg::Color4b c, float factor, int amount)
+    {
+      return vcg::Color4b( (int)vcg::math::Clamp<float>((c[0] - 128)*factor + 128 + amount,0.0f,255.0f),
+                           (int)vcg::math::Clamp<float>((c[1] - 128)*factor + 128 + amount,0.0f,255.0f),
+                           (int)vcg::math::Clamp<float>((c[2] - 128)*factor + 128 + amount,0.0f,255.0f), 1 );
+    }
+
+    //computes the gamma transformation on a given color, according to new_val = old_val^gamma
+    static vcg::Color4b color_gamma(vcg::Color4b c, float exponent)
+    {
+      return vcg::Color4b( (int)vcg::math::Clamp<float>(pow((double)c[0], exponent),0.0f,255.0f),
+                           (int)vcg::math::Clamp<float>(pow((double)c[1], exponent),0.0f,255.0f),
+                           (int)vcg::math::Clamp<float>(pow((double)c[2], exponent),0.0f,255.0f), 1 );
+    }
+
+    //invert the given color
+    static vcg::Color4b color_invert(vcg::Color4b c)
+    {
+      return vcg::Color4b( 255 - c[0], 255 - c[1], 255 - c[2], 1);
+    }
+
+    static vcg::Color4b color_levels(vcg::Color4b c, float gamma, int in_min, int in_max, int out_min, int out_max)
+    {
+      return vcg::Color4b( levels_value(c[0], gamma, in_min, in_max, out_min, out_max),
+                      levels_value(c[1], gamma, in_min, in_max, out_min, out_max),
+                      levels_value(c[2], gamma, in_min, in_max, out_min, out_max), 1);
+    }
+
+    static int levels_value(int value, float gamma, int in_min, int in_max, int out_min, int out_max)
+    {
+      // normalize
+      value = (value-in_min) / (in_max - in_min);
+      // transform gamma
+      value = (int)(pow(value,gamma));
+      // rescale range and clamp
+      return vcg::math::Clamp<int>(value * (out_max - out_min) + out_min, 0, 255);
+    }
+
+    static vcg::Color4b color_apply_diff(vcg::Color4b old_color, vcg::Color4b new_color, float intensity)
+    {
+      return vcg::Color4b( value_apply_diff(old_color[0],new_color[0],intensity), value_apply_diff(old_color[1],new_color[1],intensity), value_apply_diff(old_color[2], new_color[2],intensity), 1);
+    }
+
+    static int value_apply_diff(int old_value, int new_value, float intensity)
+    {
+      return  vcg::math::Clamp<int>((int)(old_value + intensity * (new_value - old_value)), 0, 255);
+    }
 };
-
-
 #endif
