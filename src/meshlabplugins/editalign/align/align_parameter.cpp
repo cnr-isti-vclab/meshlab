52b18ece2cfe2aca3a2058168f530319ff00b769
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2008-08-12 16:08:49 +0000

    add a class that allows one to translate between FilterParameterSets and AlignPair::Param used for alignment

---
+++ src/meshlabplugins/editalign/align/align_parameter.cpp
@@ -0,0 +1,56 @@
+/****************************************************************************
+* VCGLib                                                            o o     *
+* Visual and Computer Graphics Library                            o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2004                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+
+#include "align_parameter.h"
+
+using namespace vcg;
+
+AlignParameter::AlignParameter(){}
+
+// given a FilterParameterSet get back the alignment parameter  (dual of the buildParemeterSet)
+void AlignParameter::buildAlignParameters(FilterParameterSet &fps , AlignPair::Param &app)
+{
+	app.SampleNum=fps.getInt("SampleNum");
+  app.MinDistAbs=fps.getFloat("MinDistAbs");
+	app.TrgDistAbs=fps.getFloat("TrgDistAbs");
+	app.MaxIterNum=fps.getInt("MaxIterNum");
+	app.SampleMode= fps.getBool("SampleMode")?AlignPair::Param::SMNormalEqualized  : AlignPair::Param::SMRandom;
+	app.ReduceFactor=fps.getFloat("ReduceFactor");
+	app.MatchMode=fps.getBool("MatchMode")? AlignPair::Param::MMRigid : AlignPair::Param::MMClassic;
+}
+
+// given an alignment parameter builds the corresponding FilterParameterSet (dual of the retrieveParemeterSet)
+void AlignParameter::buildFilterParameterSet(AlignPair::Param &app, FilterParameterSet &fps)
+{
+	fps.clear();
+	fps.addInt("SampleNum",app.SampleNum,"Sample Number","Number of samples that we try to choose at each ICP iteration");
+	fps.addFloat("MinDistAbs",app.MinDistAbs,"Minimal Starting Distance","For all the chosen sample on one mesh we consider for ICP only the samples nearer than this value."
+							                             "If MSD is too large outliers could be included, if it is too small convergence will be very slow. "
+							                             "A good guess is needed here, suggested values are in the range of 10-100 times of the device scanning error."
+							                             "This value is also dynamically changed by the 'Reduce Distance Factor'");
+	fps.addFloat("TrgDistAbs",app.TrgDistAbs,"Target Distance","When 50% of the chosen samples are below this distance we consider the two mesh aligned. Usually it should be a value lower than the error of the scanning device. ");
+	fps.addInt("MaxIterNum",app.MaxIterNum,"Max Iteration Num","The maximum number of iteration that the ICP is allowed to perform.");
+	fps.addBool("SampleMode",app.SampleMode == AlignPair::Param::SMNormalEqualized,"Normal Equalized Sampling","if true (default) the sample points of icp are choosen with a  distribution uniform with respect to the normals of the surface. Otherwise they are distributed in a spatially uniform way.");
+	fps.addFloat("ReduceFactor",app.ReduceFactor,"MSD Reduce Factor","At each ICP iteration the Minimal Starting Distance is reduced to be 5 times the <Reduce Factor> percentile of the sample distances (e.g. if RF is 0.9 the new Minimal Starting Distance is 5 times the value <X> such that 90% of the sample lies at a distance lower than <X>.");
+	fps.addBool("MatchMode",app.MatchMode == AlignPair::Param::MMRigid,"Rigid matching","If true the ICP is cosntrained to perform matching only throug roto-translations (no scaling allowed). If false a more relaxed transformation matrix is allowed (scaling and shearing can appear).");
+}
---
+++ src/meshlabplugins/editalign/align/align_parameter.h
@@ -0,0 +1,41 @@
+/****************************************************************************
+* VCGLib                                                            o o     *
+* Visual and Computer Graphics Library                            o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2004                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+
+#include <meshlab/filterparameter.h>
+#include <meshlabplugins/editalign/align/AlignPair.h>
+#include <vcg/simplex/faceplus/base.h>
+
+class AlignParameter {
+
+public:
+	//translates the filter parameters into align parameters
+	static void buildAlignParameters(FilterParameterSet &fps, vcg::AlignPair::Param &app);
+
+	//translates the align parameters into filter parameters
+	static void buildFilterParameterSet(vcg::AlignPair::Param &app, FilterParameterSet &fps);
+
+private:
+	//no need to have an instance of this class
+	AlignParameter();
+
+};
