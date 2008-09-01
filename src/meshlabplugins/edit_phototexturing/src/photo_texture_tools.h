bd24d9b330ccdd5f5eb4908e60201c73f025ead3
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2008-09-01 07:35:48 +0000

    a simple class that right now just has a key for perMeshData that this plugin will need to use in the future

---
+++ src/meshlabplugins/edit_phototexturing/src/photo_texture_tools.h
@@ -0,0 +1,32 @@
+/****************************************************************************
+* MeshLab                                                           o o     *
+* A versatile mesh processing toolbox                             o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2005                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+
+
+class PhotoTextureTools {
+public:
+	//perMeshdata key for matrix needed to transform this a mesh back to where
+	//the texture can be projected.  note: permesh data will be set by some other plugin
+	static const std::string TransformForPhoto;
+};
+
+const std::string PhotoTextureTools::TransformForPhoto = "TransformForPhoto";
