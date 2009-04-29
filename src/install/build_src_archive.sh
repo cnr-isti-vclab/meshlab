401db98a0dfe08d40a516f694161b9ebdb5fa114
Guido Ranzuglia granzuglia <guido.ranzuglia@isti.cnr.it>
2009-04-29 19:08:43 +0000

    the most important file in all meshlab directory

--- src/install/build_src_archive.sh
+++ src/install/build_src_archive.sh
@@ -5,7 +5,8 @@
 # when the script ends you should have a tgz with all the needed sources
 #
 cp meshlab/src/install/building_meshlab.txt ./how_to_compile.txt
-find meshlab code/lib/glew sf ./how_to_compile.txt | egrep -v  -e CVS -e /ui_ -e build -e /moc_ -e /php -e meshlabv12.pro -e /qrc_ -e /test/ -e MeshLabBundle -e docs/ -e /web/ -e /apps/ -e\# | egrep -e  [.]cpp$ -e [.]c$ -e  [.]h$ -e  [.]pro$ -e [.]ui$ -e [.]png$ -e [.]rc$ -e [.]qrc$ -e [.]vert$ -e [.]frag$ -e [.]gdp$ -e [.]txt$ -e [.]icns$ -e images/100mesh.html -e images/eye.ico -e [.]inl$ > list.txt
+find meshlab vcglib ./how_to_compile.txt | egrep -v  -e CVS -e /ui_ -e build -e /moc_ -e /php -e meshlabv11.pro -e /qrc_ -e /test/ -e MeshLabBundle -e docs/ -e /web/ -e\# | egrep -e  [.]cpp$ -e [.]c$ -e  [.]h$ -e  [.]tpp$ -e  [.]pro$ -e [.]pri$ -e [.]ui$ -e [.]png$ -e [.]rc$ -e [.]qrc$ -e [.]vert$ -e [.]frag$ -e [.]rfx$ -e [.]glsl$ -e [.]gdp$ -e [.]txt$ -e [.]icns$ -e images/100mesh.html -e images/eye.ico -e [.]inl$ > list.txt
+echo "meshlab/src/fgt/edit_topo/edit_topomeshbuilder.h" >> list.txt
 #
 gnutar -c -v -z  -T ./list.txt -f meshlab_`date +"%Y%m%d"`.tgz 
 
