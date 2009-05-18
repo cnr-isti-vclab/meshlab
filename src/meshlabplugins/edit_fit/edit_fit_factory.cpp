94415ea9bc4071dab5d02c352fd3d1867c5d42e3
Guido Ranzuglia granzuglia <guido.ranzuglia@isti.cnr.it>
2009-05-18 14:38:40 +0000

    added GPL incipit

--- src/meshlabplugins/edit_fit/edit_fit_factory.cpp
+++ src/meshlabplugins/edit_fit/edit_fit_factory.cpp
@@ -1,14 +1,14 @@
 /****************************************************************************
 * MeshLab                                                           o o     *
-* A versatile mesh processing toolbox                             o     o   *
+* An extendible mesh processor                                    o     o   *
 *                                                                _   O  _   *
-* Copyright(C) 2005-2008                                           \/)\/    *
+* Copyright(C) 2005, 2009                                          \/)\/    *
 * Visual Computing Lab                                            /\/|      *
 * ISTI - Italian National Research Council                           |      *
 *                                                                    \      *
 * All rights reserved.                                                      *
 *                                                                           *
-* This program is free software; you can redistribute it and/or modify      *   
+* This program is free software; you can redistribute it and/or modify      *
 * it under the terms of the GNU General Public License as published by      *
 * the Free Software Foundation; either version 2 of the License, or         *
 * (at your option) any later version.                                       *
