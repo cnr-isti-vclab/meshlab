94daee8b22efeedf01059a9db790ba25832d71b3
Guido Ranzuglia granzuglia <guido.ranzuglia@isti.cnr.it>
2009-05-25 13:09:40 +0000

    1.2.1b -> 1.2.1

--- src/meshlab/mainwindow.h
+++ src/meshlab/mainwindow.h
@@ -212,7 +212,7 @@ public:
   MainWindow();
    static bool QCallBack(const int pos, const char * str);
 	 const QString appName() const {return tr("MeshLab v")+appVer(); }
-   const QString appVer() const {return tr("1.2.1b"); }
+   const QString appVer() const {return tr("1.2.1"); }
 
 public slots:
 
