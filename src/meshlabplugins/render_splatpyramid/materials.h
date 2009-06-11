610c7782d61d663463c7ebd14a72f1fcc48fc15d
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2009-06-11 23:28:43 +0000

    added copyright info

--- src/meshlabplugins/render_splatpyramid/materials.h
+++ src/meshlabplugins/render_splatpyramid/materials.h
@@ -1,23 +1,25 @@
-/**
-*
-* Disciplina: Introducao a Computacao Grafica
-* Professor: Claudio Esperanca
-* Periodo: 2004/01
-*
-* Aluno: Andre de Almeida Maximo
-* DRE: 104004976
-*
-*/
-
-/**
-*
-* Materials Properties Constants (Header)
-*** Copied from:
-*** Nate Robins, 1997
-*** Tool for teaching about OpenGL
-* File: materials.h
-*
-*/
+/****************************************************************************
+* MeshLab                                                           o o     *
+* An extendible mesh processor                                    o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2005, 2006                                          \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
 
 #ifndef __MATERIALS_H__
 #define __MATERIALS_H__
