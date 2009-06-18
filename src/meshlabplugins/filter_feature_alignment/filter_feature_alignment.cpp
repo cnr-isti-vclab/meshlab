3bb43034fedac8ea839fbcbdd8914cb3913ee653
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2009-06-18 17:40:08 +0000

    add comments to GUI

--- src/meshlabplugins/filter_feature_alignment/filter_feature_alignment.cpp
+++ src/meshlabplugins/filter_feature_alignment/filter_feature_alignment.cpp
@@ -103,15 +103,15 @@ const QString FilterFeatureAlignment::filterName(FilterIDType filter) const
 {
     switch(filter)
     {
-        case AF_COMPUTE_FEATURE : return "Compute feature";
+        case AF_COMPUTE_FEATURE : return "Features computation";
         case AF_EXTRACTION : return "Features extraction";
-        case AF_MATCHING : return "Features matching";
+        case AF_MATCHING : return "Matching";
         case AF_RIGID_TRANSFORMATION : return "Rigid transformation";
         case AF_CONSENSUS : return "Consensus";
-        case AF_RANSAC : return "Feature Alignment";
-        case AF_RANSAC_DIAGRAM : return "Alignment Diagram";
+        case AF_RANSAC : return "Feature based alignment";
+        case AF_RANSAC_DIAGRAM : return "Alignment diagram";
         case AF_PERLIN_COLOR : return "Perlin color";
-        case AF_COLOR_NOISE : return "Color Noise";
+        case AF_COLOR_NOISE : return "Color noise";
         default: assert(0);
     }
     return QString("error!");
@@ -121,15 +121,15 @@ const QString FilterFeatureAlignment::filterInfo(FilterIDType filterId) const
 {
     switch(filterId)
     {
-        case AF_COMPUTE_FEATURE : return "Compute feature values";
-        case AF_EXTRACTION : return "Given a mesh and a valid feature type, extract the best k features from the mesh.";
-        case AF_MATCHING : return "";
-        case AF_RIGID_TRANSFORMATION : return "";
-        case AF_CONSENSUS : return "Performs a randomized consensus, followed by a full consensus only if randomized consensus exceeds a given percentage.";
-        case AF_RANSAC : return "Perform ransac algorithm";
-        case AF_RANSAC_DIAGRAM : return "Ransac Diagram";        
-        case AF_PERLIN_COLOR : return "Colors randomly the vertexes of the mesh";
-        case AF_COLOR_NOISE : return "";
+        case AF_COMPUTE_FEATURE : return "Computes features for the current mesh. Selected feature is computed per-vertex and feature values are stored in a proper per-vertex attribute of the mesh.";
+        case AF_EXTRACTION : return "Extracts the requested number of feature points. Feature points are chosen according to the specified sampling strategy. Optionally, extracted feature points can be stored into PickedPoints attribute to be displayed leter on. NOTICE: this plugin requires that features have been computed previously.";
+        case AF_MATCHING : return "Extracts feature points and performs matching. The number of matched bases is returned. A base of points is randomly picked on Move Mesh and matches are efficiently retrieved on Fix Mesh through kNN search and Branch & Bound algorithm. NOTICE: this plugin requires that features have been computed previously.";
+        case AF_RIGID_TRANSFORMATION : return "Extracts feature points, performs matching and computes the rigid transformation for all matched bases found. NOTICE: this plugin requires that features have been computed previously.";
+        case AF_CONSENSUS : return "Performs consensus procedure and returns the percentage of consensus. In addiction, Move Mesh can be painted accordingly to consensus. NOTICE: this plugin requires that features have been computed previously.";
+        case AF_RANSAC : return "Performs the complete alignment process. Automatic alignment procedure is feature based and uses the iterative non deterministic RANSAC algorithm to look for a good coarse alignment. It is very important to provide a precise esteem of overlap percentage between the meshes in order to get a good success probability and avoid too rough results. Pre-semplification of meshes speed up the process. NOTICE: this plugin requires that features have been computed previously.";
+        case AF_RANSAC_DIAGRAM : return "Useful to perform tests and to evaluate performances. It repeats the alignment process several times with different settings and outputs stats in the file .\\Diagram.txt. The file is in a format suitable for OpenOffice, to easily create diagrams. NOTICE: this plugin requires that features have been computed previously.";
+        case AF_PERLIN_COLOR : return "Paints the mesh using PerlinColor function. The color assigned to verteces depends on their position in the space; it means that near verteces will be painted with similar colors.";
+        case AF_COLOR_NOISE : return "Adds to the color the requested amount of bits of noise. Bits of noise are added independently for each RGB channel. Example: 4 bits => R+-8, G+-8, B+-8.";
         default: assert(0);
     }
     return QString("error!");
@@ -147,68 +147,68 @@ void FilterFeatureAlignment::initParameterSet(QAction *a, MeshDocument& md, Filt
         case AF_COMPUTE_FEATURE:
         {
             QStringList l;
-            l << "GMSmooth Curvature"
-              << "FeatureRGB";
-            par.addEnum("featureType", 0, l,"Feature type", "");
+            l << "GMSmooth curvature"
+              << "RGB";
+            par.addEnum("featureType", 0, l,"Feature type:", "The feature that you want to compute for the current mesh.");
             break;
         }
         case AF_EXTRACTION:
         {
             QStringList l, st;
-            l << "GMSmooth Curvature"
-              << "FeatureRGB";
-            st << "Uniform" << "Poisson";
-            par.addEnum("featureType", 0, l,"Feature type", "");
-            par.addInt("numMovFeatureSelected", 250, "Number of feature to select", "");
-            par.addEnum("samplingStrategy", 0, st,"Sampling", "");
-            par.addBool("pickPoints", false, "Store features as picked points", "");
+            l << "GMSmooth curvature"
+              << "RGB";
+            st << "Uniform" << "Poisson disk";
+            par.addEnum("featureType", 0, l,"Feature type:", "The feature that you want to compute for the current mesh.");
+            par.addInt("numMovFeatureSelected", 250, "Number of features:", "The number of feature points you want to extract from the current mesh.");
+            par.addEnum("samplingStrategy", 0, st,"Sampling strategy:", "The sampling strategy used to select feature points:<br>Uniform -> random selection<br>Poisson disk -> Poisson disk sampling.");
+            par.addBool("pickPoints", false, "Store feature points:", "Turns on and off storing of feature points into PickedPoints attribute. Check this if you plan of showing feature points. NOTICE: rendering more of two hundred feature points can be very slow.");
             break;
         }
         case AF_MATCHING:
         {
             QStringList l;
-            l << "GMSmooth Curvature"
-              << "FeatureRGB";
-            par.addEnum("featureType", 0, l,"Feature type: ", "");
-            par.addMesh("mFix", 0, "Fix mesh");
-            par.addMesh("mMov", 1, "Move mesh");
-            par.addInt("numMovFeatureSelected", 250, "Number of feature extracted from Mov mesh: ", "");
-            par.addInt("nBase", 4, "Number of base features: ", "");            
-            par.addInt("k", 150, "Number of neighboor picked: ", "");            
+            l << "GMSmooth curvature"
+              << "RGB feature";
+            par.addEnum("featureType", 0, l,"Feature type:", "The feature that you want to compute for the current mesh.");
+            par.addMesh("mFix", 0, "Fix mesh:", "The mesh that stays still and grow large after alignment.");
+            par.addMesh("mMov", 1, "Move mesh:", "The mesh that moves to fit Fix Mesh.");
+            par.addInt("numMovFeatureSelected", 250, "Number of features:", "The number of feature points you want to extract from Move Mesh. The base of feature points that has to be matched is randomly picked in this subset.");
+            par.addInt("nBase", 4, "Number of base features:", "Number of feature points that make a base. At least 3 are required, 4 get a better result.");
+            par.addInt("k", 75, "Number of neighboors:", "Number of neighboor feature points picked by kNN search during matching. Greater values produce a greater success probability but make the alignment process slower.");
             break;
         }
         case AF_RIGID_TRANSFORMATION :
         {
             QStringList l;
-            l << "GMSmooth Curvature"
-              << "FeatureRGB";
-            par.addEnum("featureType", 0, l,"Feature type: ", "");
-            par.addMesh("mFix", 0, "Fix mesh");
-            par.addMesh("mMov", 1, "Move mesh");
-            par.addInt("numMovFeatureSelected", 250, "Number of feature extracted from Mov mesh: ", "");
-            par.addInt("nBase", 4, "Number of base features: ", "");           
-            par.addInt("k", 150, "Number of neighboor picked: ", "");            
+            l << "GMSmooth curvature"
+              << "RGB";
+            par.addEnum("featureType", 0, l,"Feature type:", "The feature that you want to compute for the current mesh.");
+            par.addMesh("mFix", 0, "Fix mesh:", "The mesh that stays still and grow large after alignment.");
+            par.addMesh("mMov", 1, "Move mesh:", "The mesh that moves to fit Fix Mesh.");
+            par.addInt("numMovFeatureSelected", 250, "Number of features:", "The number of feature points you want to extract from Move Mesh. The base of feature points that has to be matched is randomly picked in this subset.");
+            par.addInt("nBase", 4, "Number of base features:", "Number of feature points that make a base. At least 3 are required, 4 get a better result.");
+            par.addInt("k", 75, "Number of neighboors:", "Number of neighboor feature points picked by kNN search during matching. Greater values produce a greater success probability but make the alignment process slower.");
             break;
         }        
         case AF_CONSENSUS :
         {
             QStringList l;
-            l << "GMSmooth Curvature"
-              << "FeatureRGB";
-            par.addEnum("featureType", 0, l,"Feature type: ", "");
-            par.addMesh("mFix", 0, "Fix mesh");
-            par.addMesh("mMov", 1, "Move mesh");
-            par.addFloat("consensusDist", 2, "Consensus distance in \% of BBox diagonal of move mesh", "");
-            par.addInt("fullConsensusSamples", 2500, "Number of samples", "");
-            par.addBool("normEq", true, "Use normal equalization in consensus","");
-            par.addBool("paint", true, "Paint move mesh according to consensus","");
+            l << "GMSmooth curvature"
+              << "RGB";
+            par.addEnum("featureType", 0, l,"Feature type:", "The feature that you want to compute for the current mesh.");
+            par.addMesh("mFix", 0, "Fix mesh:", "The mesh that stays still and grow large after alignment.");
+            par.addMesh("mMov", 1, "Move mesh:", "The mesh that moves to fit Fix Mesh.");
+            par.addFloat("consensusDist", 2, "Consensus distance:","Consensus distance expressed in percentage of Move Mesh bounding box diagonal. It states how close two verteces must be to be in consensus.");
+            par.addInt("fullConsensusSamples", 2500, "Number of samples:", "Number of samples used to perform consensus procedure. Greater values get a more actual esteem but takes more time to be computed.");
+            par.addBool("normEq", true, "Normal equalization:","If checked samples are selected according a normal equilezed strategy, elsewhere a random selection is performed. Normal equalization is computationally more expensive but provides a better esteem, specially with few samples.");
+            par.addBool("paint", true, "Paint Move Mesh:","If checked Move Mesh is painted according to consensus as follows:<br>white -> not tested<br>blue -> too far<br>yellow -> close but badly oriented<br>red -> in consensus.");
             break;
         }
         case AF_RANSAC:
         {
             QStringList l;
-            l << "GMSmooth Curvature"
-              << "FeatureRGB";
+            l << "GMSmooth curvature"
+              << "RGB";
             par.addEnum("featureType", 0, l,"Feature type", "");
             par.addMesh("mFix", 0, "Fix mesh");
             par.addMesh("mMov", 1, "Move mesh");
@@ -528,7 +528,7 @@ bool FilterFeatureAlignment::ExtractionOperation(MeshModel& m, typename ALIGNER_
     //enables needed attributes
     m.updateDataMask(FeatureType::getRequirements());
     //extract features
-    vector<FeatureType*>* vecF = FeatureAlignment<MeshType,FeatureType>::extractFeatures(param.numFixFeatureSelected, m.cm, param.samplingStrategy, cb);
+    vector<FeatureType*>* vecF = FeatureAlignment<MeshType,FeatureType>::extractFeatures(param.numMovFeatureSelected, m.cm, param.samplingStrategy, cb);
     if(!vecF) return false;  //something wrong!
 
     //clear old picked points, then, if requested, add all new points
