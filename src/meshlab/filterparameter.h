51cd39d30234aa3cc5667db865f10635c45533f3
Guido Ranzuglia granzuglia <guido.ranzuglia@isti.cnr.it>
2009-09-03 10:14:31 +0000

    removed unneeded comment

--- src/meshlab/filterparameter.h
+++ src/meshlab/filterparameter.h
@@ -633,6 +633,35 @@ public:
 	RichParameter* lastCreated;
 };
 
+class RichWidgetXML : public Visitor
+{
+public:
+	RichWidgetXML(QDomDocument& doc){}
+
+	void visit(RichBool& pd);
+	void visit(RichInt& pd);
+	void visit(RichFloat& pd);
+	void visit(RichString& pd);
+	void visit(RichMatrix44f& pd);
+	void visit(RichPoint3f& pd);
+	void visit(RichColor& pd);
+	void visit(RichColor4b& pd);
+	void visit(RichAbsPerc& pd);
+
+	void visit(RichEnum& pd);
+	void visit(RichFloatList& pd);
+
+	void visit(RichDynamicFloat& pd);
+
+	void visit(RichOpenFile& pd);
+	void visit(RichSaveFile& pd);
+	void visit(RichMesh& pd);
+
+	~RichWidgetXML() {}
+
+	QDomDocument docdom;
+};
+
 class RichParameterSet
 {
 
