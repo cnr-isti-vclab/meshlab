105de2ad2b936984e6d9bcee29ed9e6fa8e3a846
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2009-11-23 11:27:17 +0000

    Refactoring meshlab common files. Created a 'common' directory with the shared pieces of code. Most of the pro/pri files have been changed. A rebuild all is strongly suggested... * Added skeleton for the common plugin manager

---
+++ src/common/pluginmanager.cpp
@@ -0,0 +1,5 @@
+#include "pluginmanager.h"
+
+PluginManager::PluginManager()
+{
+}
---
+++ src/common/pluginmanager.h
@@ -0,0 +1,10 @@
+#ifndef PLUGINMANAGER_H
+#define PLUGINMANAGER_H
+
+class PluginManager
+{
+public:
+    PluginManager();
+};
+
+#endif // PLUGINMANAGER_H
