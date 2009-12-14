2bc871c153d8d450cc2898fb96cf5a4e7b388f66
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2009-12-14 14:19:23 +0000

    small notes to the extrude functions

--- src/fgt/filter_shape/extrude.h
+++ src/fgt/filter_shape/extrude.h
@@ -5,9 +5,13 @@ class Extrude{
 //Core functions:
    void SelectedFaces(MeshType & in, ScalarType offset);
 
-   void FromProfileAndPath(vector<Point3f> &profileVec, vector<Point3f> &pathPointVec, vector<Point3f> &pathFrameVec);
+
+// Generate an extruded profile starting from a profile, a path and a set of rotations.
+// We assume that the profile is centered around the origin and lie on the XY plane.
+// the pathFrameVec encodes the rotations that have to be applied to the profile at each step encoded as eulerian angles.
+void FromProfileAndPath(vector<Point3f> &profileVec, vector<Point3f> &pathPointVec, vector<Point3f> &pathFrameVec, vector<float> &scaleVec);
 
 // High level funcs
-   void TubeFromPath(vector<Point3f> &profileVec, int profileSize);
+ void TubeFromPath(vector<Point3f> &pathPointVec, int profileSize);
 
 } 
