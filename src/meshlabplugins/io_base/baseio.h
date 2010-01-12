2ff4c6750bec48a7859027f157135bb437fa981c
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2010-01-12 09:27:13 +0000

    cleaned unused functions

--- src/meshlabplugins/io_base/baseio.h
+++ src/meshlabplugins/io_base/baseio.h
@@ -20,24 +20,8 @@
 * for more details.                                                         *
 *                                                                           *
 ****************************************************************************/
-/****************************************************************************
-  History
-
- $Log$
- Revision 1.3  2007/11/26 07:35:25  cignoni
- Yet another small cosmetic change to the interface of the io filters.
-
- Revision 1.2  2007/11/25 09:48:38  cignoni
- Changed the interface of the io filters. Now also a default bit set for the capabilities has to specified
-
- Revision 1.1  2006/11/30 22:55:06  cignoni
- Separated very basic io filters to the more advanced one into two different plugins baseio and meshio
-
- *****************************************************************************/
 #ifndef BASEIOPLUGIN_H
 #define BASEIOPLUGIN_H
-
-#include <QString>
 
 #include <common/interfaces.h>
 
@@ -49,22 +33,19 @@ class BaseMeshIOPlugin : public QObject, public MeshIOInterface
   
 public:
 	
-	BaseMeshIOPlugin() : MeshIOInterface() {}
-	
-	QList<Format> importFormats() const;
-	QList<Format> exportFormats() const;
+  BaseMeshIOPlugin() : MeshIOInterface() {}
 
-	void GetExportMaskCapability(QString &format, int &capability, int &defaultBits) const;
+  QList<Format> importFormats() const;
+  QList<Format> exportFormats() const;
 
-	bool open(const QString &formatName, const QString &fileName, MeshModel &m, int& mask,const RichParameterSet & par, vcg::CallBackPos *cb=0, QWidget *parent=0);
-	bool save(const QString &formatName, const QString &fileName, MeshModel &m, const int mask, const RichParameterSet & par, vcg::CallBackPos *cb=0, QWidget *parent= 0);
+  void GetExportMaskCapability(QString &format, int &capability, int &defaultBits) const;
+
+  bool open(const QString &formatName, const QString &fileName, MeshModel &m, int& mask,const RichParameterSet & par, vcg::CallBackPos *cb=0, QWidget *parent=0);
+  bool save(const QString &formatName, const QString &fileName, MeshModel &m, const int mask, const RichParameterSet & par, vcg::CallBackPos *cb=0, QWidget *parent= 0);
   virtual void initOpenParameter(const QString &format, MeshModel &/*m*/, RichParameterSet & par);
-	virtual void applyOpenParameter(const QString &format, MeshModel &m, const RichParameterSet &par);
-	void initPreOpenParameter(const QString &formatName, const QString &filename, RichParameterSet &parlst);
-
+  virtual void applyOpenParameter(const QString &format, MeshModel &m, const RichParameterSet &par);
+  void initPreOpenParameter(const QString &formatName, const QString &filename, RichParameterSet &parlst);
   virtual void initSaveParameter(const QString &format, MeshModel &/*m*/, RichParameterSet & par);
-	//const QString filterInfo(FilterIDType filter);
-	//const QString filterName(FilterIDType filter);
 };
 
 #endif
