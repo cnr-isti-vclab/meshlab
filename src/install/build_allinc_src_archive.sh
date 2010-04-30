6e53dcdfd010ff6d52af49c54aec106c48e323f4
Guido Ranzuglia granzuglia <guido.ranzuglia@isti.cnr.it>
2010-04-30 13:12:19 +0000

    included Eigen files without extension

--- src/install/build_allinc_src_archive.sh
+++ src/install/build_allinc_src_archive.sh
@@ -5,7 +5,7 @@
 # when the script ends you should have a tgz with all the needed sources
 #
 cp meshlab/src/install/building_meshlab.txt ./how_to_compile.txt
-find meshlab vcglib ./how_to_compile.txt | egrep -v  -e SVN -e /ui_ -e build -e /moc_ -e /php -e meshlabv11.pro -e /qrc_ -e /test/ -e MeshLabBundle -e docs/ -e /web/ -e\# | egrep -e  [.]cpp$ -e [.]c$ -e  [.]h$ -e  [.]tpp$ -e  [.]pro$ -e [.]pri$ -e [.]ui$ -e [.]png$ -e [.]rc$ -e [.]qrc$ -e [.]vert$ -e [.]frag$ -e [.]rfx$ -e [.]glsl$ -e [.]gdp$ -e [.]txt$ -e [.]icns$ -e images/100mesh.html -e images/eye.ico -e [.]inl$ > list.txt
+find meshlab vcglib ./how_to_compile.txt | egrep -v  -e SVN -e /ui_ -e build -e /moc_ -e /php -e meshlabv11.pro -e /qrc_ -e /test/ -e MeshLabBundle -e docs/ -e /web/ -e\# | egrep -e  [.]cpp$ -e [.]c$ -e  [.]h$ -e  [.]tpp$ -e  [.]pro$ -e [.]pri$ -e [.]ui$ -e [.]png$ -e [.]rc$ -e [.]qrc$ -e [.]vert$ -e [.]frag$ -e [.]rfx$ -e [.]glsl$ -e [.]gdp$ -e [.]txt$ -e [.]icns$ -e images/100mesh.html -e images/eye.ico -e [.]inl$ -e vcg/Eigen/* > list.txt
 echo "meshlab/src/fgt/edit_topo/edit_topomeshbuilder.h" >> list.txt
 #
 gnutar -c -v -z  -T ./list.txt -f meshlab_`date +"%Y%m%d"`.tgz 
