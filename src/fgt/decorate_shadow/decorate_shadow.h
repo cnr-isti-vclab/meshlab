b58650479a9147d4836e81885998ee2b7d73901c
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2010-08-03 12:46:22 +0000

    removed collision between old and new parameter names

--- src/fgt/decorate_shadow/decorate_shadow.h
+++ src/fgt/decorate_shadow/decorate_shadow.h
@@ -84,7 +84,7 @@ private:
     DecorateShader* _decoratorSH, *_decoratorSSAO;
     inline const QString DecorateShadowSSAORadius() { return  "MeshLab::Decoration::SSAORadius" ; }
     inline const QString DecorateShadowMethod() { return  "MeshLab::Decoration::ShadowMethod" ; }
-    inline const QString DecorateShadowIntensity() { return  "MeshLab::Decoration::ShadowIntensity" ; }
+    inline const QString DecorateShadowIntensity() { return  "MeshLab::Decoration::ShadowIntensityVal" ; }
 
 };
 
