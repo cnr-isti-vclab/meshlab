6409e9e64db50e7fb7c192cd622b13be41428076
Fabio Ganovelli ganovelli <fabio.ganovelli@isti.cnr.it>
2010-12-22 15:06:34 +0000

    ongoing berkeleyDB removal

--- src/meshlabplugins/edit_ocme/src/ocme/berkeleydb/ocme_simpledb.cpp
+++ src/meshlabplugins/edit_ocme/src/ocme/berkeleydb/ocme_simpledb.cpp
@@ -1,111 +1,6 @@
 #include "../ocme_disk_loader.h"
 #include "../utils/memory_debug.h"
 
-#ifndef NO_BERKELEY
-// File: excxx_example_database_read.cpp
-
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-
-#include "../ooc_vector/berkeleydb/berkeleydb.hpp"
-#include "../ocme_definition.h"
-#include "../impostor_definition.h"
-#include "../../utils/timing.h"
-
-
-void
-OCME::Save( ){
-	TIM::Begin(11);
-	int siz = this->SizeOf();			// size of the Multigrid Table 
-	char * buf = new char[siz];			// allocate memory
-
-	char * res = this->Serialize(buf);	// serialize
- 	RAssert(res-buf == siz);
-	char title[65];
-	sprintf(&title[0],"%s","MULTIGRID_TABLE");
-	Dbt key(&title[0], (u_int32_t)strlen("MULTIGRID_TABLE") + 1);
-   	Dbt data(buf, siz);
- 
-    int result =  ((BerkeleyDb*)extMemHnd)->getDb().put(NULL, &key, &data, 0) ;
-	RAssert(result==0);
-
-	delete [] buf;
-	sprintf(lgn->Buf(),"ocme table:%d",siz);
-	lgn->Push();
-
-	stat.size_ocme_table = siz;
-
-	sprintf(lgn->Buf(),"saving the impostors data..");
-	lgn->Push();
-	unsigned int start =clock();
-	CellsIterator ci;
-	for(ci = cells.begin(); ci != cells.end();++ci)
-	{
-		(*ci).second->impostor->SetCentroids();
-		SaveImpostor((*ci).second);
-	}
-	sprintf(lgn->Buf(),"..done in %f",(clock()-start)/float(CLOCKS_PER_SEC));
-	lgn->Push();
-
-	TIM::End(11);
-
-}
-
-
-void OCME::LoadImpostor(Cell * c){
-
-}
-
-void OCME::SaveImpostor(Cell * c){
-		static int n=0;
-		char title[255];
-		RAssert(MemDbg::CheckHeap(0));
-		int siz =c->impostor->SizeOf();
-		if(siz == 32 ) return; // it means that only vn and fn have been written->no impostor
-		char * buf = new char[siz];
-		c->impostor->Serialize(buf);
-
-		sprintf(&title[0],"%s_impostor",ToString(c->key).c_str());
-		Dbt keyimp(&title[0], (u_int32_t)strlen(&title[0]) + 1);
-		Dbt dataimp(buf, siz);
-	 
-		int result =  ((BerkeleyDb*)extMemHnd)->getDb().put(NULL, &keyimp, &dataimp, 0) ;
-		RAssert(result==0);
-		delete [] buf;
-		stat.size_impostors+=siz;
-}
-
-
-
-void OCME::Load( ){
-	Dbt data;
-	char title[65];
-	sprintf(&title[0],"%s","MULTIGRID_TABLE");
-	Dbt key(&title[0], (u_int32_t)strlen("MULTIGRID_TABLE") + 1);
-	data.set_flags(DB_DBT_MALLOC);
-    int res = ((BerkeleyDb*)extMemHnd)->getDb().get(NULL, &key, &data, 0);
-	if(res == DB_NOTFOUND)
-		printf("MULTIGRID_TABLE not found");
-
-	TIM::Begin(10);
-	this->DeSerialize((char*)data.get_data());
-
-	// load the impostors
-	for(CellsIterator ci = this->cells.begin(); ci != this->cells.end(); ++ci){
-		sprintf(&title[0],"%s_impostor",ToString((*ci).second->key).c_str());
-		Dbt keyimp(&title[0], (u_int32_t)strlen(&title[0]) + 1);
-		res = ((BerkeleyDb*)extMemHnd)->getDb().get(NULL, &keyimp, &data, 0);
-		if(res != DB_NOTFOUND)
-		{
-			(*ci).second->impostor = new Impostor();
-			(*ci).second->impostor->DeSerialize((char*)data.get_data());
-		}
-	}
-	TIM::End(10);
-
-}
-#else
 // File: excxx_example_database_read.cpp
 
 #include <iostream>
@@ -219,5 +114,5 @@ void OCME::RemoveImpostor(const CellKey & key){
 		sprintf(&title[0],"%s_impostor",ToString(key).c_str());
 		((SimpleDb*)extMemHnd)->Del(title);
 }
-#endif
+
 
