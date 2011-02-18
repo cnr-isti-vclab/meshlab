cd4ae66017cc7a67e498eec3ee47631ca735e6ed
Marco Callieri mcallieri <marco.callieri@isti.cnr.it>
2011-02-18 15:58:04 +0000

    filter which exports all layers as a raw binary data file (points+normals) BNPTS to be used with the streaming out-of-core poisson reconstructor - first implementation

---
+++ src/meshlabplugins/filter_bnpts/filter_bnpts.h
@@ -0,0 +1,48 @@
+/****************************************************************************
+* MeshLab                                                           o o     *
+* A versatile mesh processing toolbox                             o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2005                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+
+#ifndef FILTER_BNPTS_H
+#define FILTER_BNPTS_H
+
+#include <QObject>
+
+#include <common/interfaces.h>
+
+class FilterBnptsPlugin : public QObject, public MeshFilterInterface
+{
+	Q_OBJECT
+	Q_INTERFACES(MeshFilterInterface)
+
+public:
+	enum { FP_BNPTSGEN };
+
+	FilterBnptsPlugin();
+	
+	virtual QString filterName(FilterIDType filter) const;
+	virtual QString filterInfo(FilterIDType filter) const;
+	virtual FilterClass getClass(QAction *);
+	virtual void initParameterSet(QAction *,MeshDocument &/*m*/, RichParameterSet & /*parent*/);
+	virtual bool applyFilter(QAction *filter, MeshDocument &md, RichParameterSet & /*parent*/, vcg::CallBackPos * cb) ;
+};
+
+#endif
