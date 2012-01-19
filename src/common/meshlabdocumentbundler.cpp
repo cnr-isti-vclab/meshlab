df5dec971b3a8ba03d677c18b0bfb0f8a83d5594
Fabio Ganovelli ganovelli <fabio.ganovelli@isti.cnr.it>
2012-01-19 13:51:46 +0000

    fixed path to  the images

--- src/common/meshlabdocumentbundler.cpp
+++ src/common/meshlabdocumentbundler.cpp
@@ -19,21 +19,27 @@ bool MeshDocumentFromBundler(MeshDocument &md, QString filename_out,QString imag
 	md.addNewMesh(model_filename,QString("model"));
 	std::vector<vcg::Shotf> shots;
 	const QString path = QFileInfo(filename_out).absolutePath();
+        const QString path_im = QFileInfo(image_list_filename).absolutePath()+QString("/");
 
 	std::vector<std::string>   image_filenames;
-	vcg::tri::io::ImporterOUT<CMeshO>::Open(md.mm()->cm,shots,image_filenames,qPrintable(filename_out), qPrintable(image_list_filename));
+        vcg::tri::io::ImporterOUT<CMeshO>::Open(md.mm()->cm,shots,image_filenames,qPrintable(filename_out), qPrintable(image_list_filename),qPrintable(path_im));
 
+        QString curr_path = QDir::currentPath();
+        QFileInfo imi(image_list_filename);
+
+        QDir::setCurrent(imi.absoluteDir().absolutePath());
 	QStringList image_filenames_q;
 	for(unsigned int i  = 0; i < image_filenames.size(); ++i)
-		image_filenames_q.push_back(QString::fromStdString(image_filenames[i]));
+                image_filenames_q.push_back(QString::fromStdString(image_filenames[i]));
 
 	for(int i=0 ; i<shots.size() ; i++){
 			md.addNewRaster();
-			const QString fullpath_image_filename = path + image_filenames_q[i].section('.',1,2);
+                        const QString fullpath_image_filename = path_im + image_filenames_q[i];
 			md.rm()->addPlane(new Plane(md.rm(),fullpath_image_filename,QString("")));
 			md.rm()->setLabel(image_filenames_q[i].section('/',1,2));
 			md.rm()->shot = shots[i];
 	}
+        QDir::setCurrent(curr_path);
 
     return true;
 }
