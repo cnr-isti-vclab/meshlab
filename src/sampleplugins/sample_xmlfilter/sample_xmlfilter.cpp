f9f3f393bcf5d7d103954e2664283bc335471fa8
Guido Ranzuglia granzuglia <guido.ranzuglia@isti.cnr.it>
2012-03-16 08:37:59 +0000

    updated

--- src/sampleplugins/sample_xmlfilter/sample_xmlfilter.cpp
+++ src/sampleplugins/sample_xmlfilter/sample_xmlfilter.cpp
@@ -44,19 +44,16 @@ bool SampleXMLFilterPlugin::applyFilter( const QString& filterName,MeshDocument&
 		{
 			// Typical usage of the callback for showing a nice progress bar in the bottom. 
 		//	// First parameter is a 0..100 number indicating percentage of completion, the second is an info string.
-			cb(100*i/m.cm.vert.size(), "Randomly Displacing...");
+			//cb(100*i/m.cm.vert.size(), "Randomly Displacing...");
 
 			float rndax = (float(2.0f*rand())/RAND_MAX - 1.0f ) *max_displacement;
 			float rnday = (float(2.0f*rand())/RAND_MAX - 1.0f ) *max_displacement;
 			float rndaz = (float(2.0f*rand())/RAND_MAX - 1.0f ) *max_displacement;
-			m.cm.vert[i].P() += vcg::Point3f(rndax,rnday,rndaz);	
-			bool red = false;
+			m.cm.vert[i].P() += vcg::Point3f(rndax,rnday,rndaz);
 			if (i % 500 == 0)
-				red = true;
-			else
-				red = false;
-			if (hasInterruptRequested(red))
-				return true;
+				md.updateRenderMesh(m);
+			/*if1 (hasInterruptRequested(red))
+				return true;*/
 		}
 
 		//// Log function dump textual info in the lower part of the MeshLab screen. 
