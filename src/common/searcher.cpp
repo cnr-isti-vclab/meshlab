6a5c3cf72a0329d12043ae70e3e96d0b59ae6959
Guido Ranzuglia granzuglia <guido.ranzuglia@isti.cnr.it>
2012-07-30 17:36:46 +0000

    - removed syntactical  error in sepexp regular expression (introduced with previous commit)

--- src/common/searcher.cpp
+++ src/common/searcher.cpp
@@ -33,8 +33,7 @@ bool WordActionsMap::getActionsPerWord( const QString& word,QList<QAction*>& res
 WordActionsMapAccessor::WordActionsMapAccessor()
 :map(),sepexp(),ignexp()
 {
-	sepexp.setPattern("\\b(W+)\\b|\\s+");
-	sepexp.setMinimal(true);
+	sepexp.setPattern("\\W+");
 	ignexp.setPattern("\\b(an|the|of|it|as|in|by|and|or|for)\\b|\\b[a-z]\\b|'s\\b|\\.|<[^>]*>");
 }
 
