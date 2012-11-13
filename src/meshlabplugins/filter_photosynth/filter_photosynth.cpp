080d16b1616fdb5401de331108b83a7f3f3d7fdb
Matteo Dellepiane matteodelle <matteo.dellepiane@isti.cnr.it>
2012-11-13 16:36:18 +0000

    dellepiane: modified to account for change in Plane creation + color-per-vertex attribute saving

--- src/meshlabplugins/filter_photosynth/filter_photosynth.cpp
+++ src/meshlabplugins/filter_photosynth/filter_photosynth.cpp
@@ -168,6 +168,7 @@ bool FilterPhotosynthPlugin::applyFilter(QAction */*filter*/, MeshDocument &md, 
 	        mm->cm.vert.back().P() = Point3f(p._x,p._z,-p._y);
 	        mm->cm.vert.back().C() = Color4b(p._r,p._g,p._b,255);
 	      }
+		  mm->updateDataMask(MeshModel::MM_VERTCOLOR);
 	      //we consider cameras only if the user chooses to download images
 	      if(par.getBool("saveImages"))
 	      {
@@ -203,7 +204,7 @@ bool FilterPhotosynthPlugin::applyFilter(QAction */*filter*/, MeshDocument &md, 
 	          //this way the user can choose which point cloud the raster has to align with
 	          RasterModel *rm = md.addNewRaster();
 	          QString imgName = imageDir.filePath(QString("IMG_%1.jpg").arg(img._ID) );
-	          rm->addPlane(new Plane(imgName,QString("")));
+	          rm->addPlane(new Plane(imgName,Plane::RGBA));
 	          rm->shot = s;
 	          rm->setLabel(QString("IMG_%1_%2.jpg").arg(int(img._ID),3,10,QLatin1Char('0')).arg(sys->_id));
 	        }
