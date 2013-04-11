6ed5fc5857bb6d5d4decf6b16bf5a9953622133f
Guido Ranzuglia granzuglia <guido.ranzuglia@isti.cnr.it>
2013-04-11 15:00:18 +0000

    - added new catch branch

--- src/common/mlapplication.cpp
+++ src/common/mlapplication.cpp
@@ -16,11 +16,17 @@ bool MeshLabApplication::notify( QObject * rec, QEvent * ev )
 {
 	try
 	{
-		return QApplication::notify(rec,ev);
+		bool not = QApplication::notify(rec,ev);
+		return not;
 	}
 	catch (MeshLabException& e)
 	{    
 		qCritical("%s",e.what());
+	}
+	catch (...)
+	{
+		qCritical("Something really bad happened!!!!!!!!!!!!!");
+		abort();
 	}
   return false;
 }
