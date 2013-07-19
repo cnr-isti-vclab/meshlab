0f527b8bb177f4b26310e4eecc07e493d100e049
Matteo Dellepiane matteodelle <matteo.dellepiane@isti.cnr.it>
2013-07-19 13:04:34 +0000

    dellepiane: higher value for Max of Far plane set.

--- src/meshlab/glarea.cpp
+++ src/meshlab/glarea.cpp
@@ -1016,8 +1016,8 @@ void GLArea::wheelEvent(QWheelEvent*e)
 		float notch = e->delta()/ float(WHEEL_STEP);
 		switch(e->modifiers())
 		{
-		case Qt::ControlModifier+Qt::ShiftModifier     : clipRatioFar  = math::Clamp( clipRatioFar*powf(1.2f, notch),0.01f,5000.0f); break;
-		case Qt::ControlModifier                       : clipRatioNear = math::Clamp(clipRatioNear*powf(1.2f, notch),0.01f,50.0f); break;
+		case Qt::ControlModifier+Qt::ShiftModifier     : clipRatioFar  = math::Clamp( clipRatioFar*powf(1.2f, notch),0.01f,50000.0f); break;
+		case Qt::ControlModifier                       : clipRatioNear = math::Clamp(clipRatioNear*powf(1.2f, notch),0.01f,500.0f); break;
 		case Qt::ShiftModifier                         : fov = math::Clamp(fov+1.2f*notch,5.0f,90.0f); break;
 		case Qt::AltModifier                           : glas.pointSize = math::Clamp(glas.pointSize*powf(1.2f, notch),0.01f,150.0f);
 			foreach(MeshModel * mp, this->md()->meshList)
