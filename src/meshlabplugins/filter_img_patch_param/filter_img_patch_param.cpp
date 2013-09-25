0115b882359967aac6d0f67dbd43fd8f8605723c
Matteo Dellepiane matteodelle <matteo.dellepiane@isti.cnr.it>
2013-09-25 14:10:16 +0000

    dellepiane: set use of distance and border quality as default in image_patch_param filter

--- src/meshlabplugins/filter_img_patch_param/filter_img_patch_param.cpp
+++ src/meshlabplugins/filter_img_patch_param/filter_img_patch_param.cpp
@@ -147,11 +147,11 @@ void FilterImgPatchParamPlugin::initParameterSet( QAction *act,
         case FP_PATCH_PARAM_ONLY:
         {
             par.addParam( new RichBool( "useDistanceWeight",
-                                        false,
+                                        true,
                                         "Use distance weight",
                                         "Includes a weight accounting for the distance to the camera during the computation of reference images" ) );
             par.addParam( new RichBool( "useImgBorderWeight",
-                                        false,
+                                        true,
                                         "Use image border weight",
                                         "Includes a weight accounting for the distance to the image border during the computation of reference images" ) );
             par.addParam( new RichBool( "useAlphaWeight",
