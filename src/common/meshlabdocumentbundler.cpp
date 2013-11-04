c6f5341d58cf004f86ac9636b9f5aa15d35d0c0b
Matteo Dellepiane matteodelle <matteo.dellepiane@isti.cnr.it>
2013-11-04 18:09:23 +0000

    dellepiane: fixed image path issues with importing .out

--- src/common/meshlabdocumentbundler.cpp
+++ src/common/meshlabdocumentbundler.cpp
@@ -27,22 +27,32 @@ bool MeshDocumentFromBundler(MeshDocument &md, QString filename_out,QString imag
   QString curr_path = QDir::currentPath();
   QFileInfo imi(image_list_filename);
 
-  QDir::setCurrent(imi.absoluteDir().absolutePath());
+  //
   QStringList image_filenames_q;
+  QSize size;
   for(unsigned int i  = 0; i < image_filenames.size(); ++i)
-    image_filenames_q.push_back(QString::fromStdString(image_filenames[i]));
+  {
+	QImageReader sizeImg(QString::fromStdString(image_filenames[i]));
+	if(sizeImg.size()==QSize(-1,-1))
+		image_filenames_q.push_back(path_im+QString::fromStdString(image_filenames[i]));		
+	else
+		image_filenames_q.push_back(QString::fromStdString(image_filenames[i]));
+  }
+  QDir::setCurrent(imi.absoluteDir().absolutePath());
 
 	for(size_t i=0 ; i<shots.size() ; i++){
 			md.addNewRaster();
-                        const QString fullpath_image_filename = path_im + image_filenames_q[i];
+                        const QString fullpath_image_filename = image_filenames_q[i];
             md.rm()->addPlane(new Plane(fullpath_image_filename,Plane::RGBA));
-			md.rm()->setLabel(image_filenames_q[i].section('/',1,2));
+			int count=fullpath_image_filename.count('\\');
+			if (count==0)
+			{
+				count=fullpath_image_filename.count('/');
+				md.rm()->setLabel(fullpath_image_filename.section('/',count,1));
+			}
+			else
+				md.rm()->setLabel(fullpath_image_filename.section('\\',count,1));
 			md.rm()->shot = shots[i];
-			/*md.rm()->shot.Intrinsics.ViewportPx[0]=md.rm()->currentPlane->image.width();
-			md.rm()->shot.Intrinsics.ViewportPx[1]=md.rm()->currentPlane->image.height();
-			md.rm()->shot.Intrinsics.CenterPx[0]=(int)((double)md.rm()->shot.Intrinsics.ViewportPx[0]/2.0f);
-			md.rm()->shot.Intrinsics.CenterPx[1]=(int)((double)md.rm()->shot.Intrinsics.ViewportPx[1]/2.0f);*/
-
 	}
         QDir::setCurrent(curr_path);
 
