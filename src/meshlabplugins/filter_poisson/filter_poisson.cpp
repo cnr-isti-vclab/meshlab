04ea7341597a975beb3e0c15b4b24a57e1a45dfa
Guido Ranzuglia granzuglia <guido.ranzuglia@isti.cnr.it>
2014-03-31 15:45:24 +0000

    - useless control on the normals. It can just avoid crashes derived from an importer setting up to [0.0f,0.0f,0.0f] the normal vectors of a mesh without per-vertex normal attribute.

--- src/meshlabplugins/filter_poisson/filter_poisson.cpp
+++ src/meshlabplugins/filter_poisson/filter_poisson.cpp
@@ -152,10 +152,26 @@ bool PoissonPlugin::applyFilter(QAction *filter, MeshDocument &md, RichParameter
 	if (m.hasDataMask(MeshModel::MM_VERTTEXCOORD)){
 		m.clearDataMask(MeshModel::MM_VERTTEXCOORD);
 	}
-	
-	int cnt=0;
-	CMeshO::VertexIterator vi;
-	for(vi=m.cm.vert.begin(); vi!=m.cm.vert.end(); ++vi)
+
+    //Useless control on the normals. It can just avoid crashes derived from an importer setting up to [0.0f,0.0f,0.0f] the normal vectors of a mesh without per-vertex normal attribute.
+    int zeronrm = 0;
+    for(CMeshO::VertexIterator vi=m.cm.vert.begin(); vi!=m.cm.vert.end(); ++vi)
+    {
+        if(!(*vi).IsD())
+        {
+            if ((*vi).N() == vcg::Point3f(0.0f,0.0f,0.0f))
+                ++zeronrm;
+        }
+    }
+
+    if (zeronrm == m.cm.vn)
+    {
+        Log(GLLogStream::SYSTEM,"All the normal vectors are set to [0.0,0.0,0.0]. Poisson reconstruction filter requires a set of valid per-vertex normal. Filter will be aborted.");
+        return false;
+    }
+
+    int cnt=0;
+	for(CMeshO::VertexIterator vi=m.cm.vert.begin(); vi!=m.cm.vert.end(); ++vi)
 	if(!(*vi).IsD()){
 			(*vi).N().Normalize();
 			for(int ii=0;ii<3;++ii){
