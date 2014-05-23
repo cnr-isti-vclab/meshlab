ba526763790d821198497c1a9ca3cc2cd0bfcd58
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2014-05-23 15:07:00 +0000

    Refactored and improved the old code for finding rotation matrix that matches meshes

--- src/meshlabplugins/edit_align/align/Guess.h
+++ src/meshlabplugins/edit_align/align/Guess.h
@@ -8,7 +8,7 @@
 *                                                                    \      *
 * All rights reserved.                                                      *
 *                                                                           *
-* This program is free software; you can redistribute it and/or modify      *   
+* This program is free software; you can redistribute it and/or modify      *
 * it under the terms of the GNU General Public License as published by      *
 * the Free Software Foundation; either version 2 of the License, or         *
 * (at your option) any later version.                                       *
@@ -27,171 +27,213 @@
 #include <vcg/space/point3.h>
 #include <vcg/math/matrix44.h>
 #include <vcg/space/index/grid_static_obj.h>
+#include <vcg/space/index/kdtree/kdtree.h>
+
 #include <vcg/complex/algorithms/point_sampling.h>
 namespace vcg
 {
 namespace tri
 {
 
-// Class for Brute force alignment. 
-// given two meshes, Fix and Mov, it build a set of coarser and coarser occupancy grids. 
-// Put Fix into the grids, and then try to rotate a subset of the Mov points to see how much 
+// Class for Brute force alignment.
+// given two meshes, Fix and Mov, it build a set of coarser and coarser occupancy grids.
+// Put Fix into the grids, and then try to rotate a subset of the Mov points to see how much
 // they cover the same space of the other one.
 
-class Guess 
+class Guess
 {
 public:
-	// Parameter of the automatic alighment approach
-	class Param 
-		{
-		public:
-		  Param()
-			{
-				GridSize=200000;
-				MatrixNum=1000;
-				Range=8;
-				Step=1;
-				SampleNum=10000;
-				UGLevels=3;
-			}
-			
-			int MatrixNum; // Number of Direction to be tested. 
+  // Parameter of the automatic alighment approach
+  class Param
+  {
+  public:
+    Param()
+    {
 
-      int Range;     // numero
-			int Step;
-			int GridSize;
-			int SampleNum; // Number of points of the Movable mesh that has to be used in the SearchTranslate
-			int UGLevels;
-		};
+      GridSize=200000;
+      MatrixNum=1000;
+      Range=8;
+      SampleNum=10000;
+      UGLevels=3;
+    }
 
-	class Result
-		{
-		public:
-			
-			Result(Matrix44d &_m, double _err,int _id, const Point3d &btv)
-			{
-				m=_m;
-				id=_id;
-				err=_err;
-				BestTr=btv;
-			}
-			Matrix44d m;
-			int id; // identificativo della rotazione da cui si era partiti
-			double err;
-			Point3d BestTr; //Best Translation vector found. 
-			bool operator <  (const Result & rr) const {return (err< rr.err);}
-			};
+    int MatrixNum; // Number of Direction to be tested.
+
+    int Range;     // numero
+    int GridSize;
+    int SampleNum; // Number of points of the Movable mesh that has to be used in the SearchTranslate
+    int UGLevels;
+  };
+
+    class Result
+        {
+        public:
+
+            Result(const Matrix44f &_m, int _score,int _id, const Point3f &BestTransvec)
+            {
+                m=_m;
+                id=_id;
+                score=_score;
+                BestTr=BestTransvec;
+            }
+            Matrix44f m;
+            int id; // the id of the in the RotMVec;
+            int score;
+            Point3f BestTr; //Best Translation vector found.
+            bool operator <  (const Result & rr) const {return (score< rr.score);}
+            };
 
 /***********************************/
-// The internal state:
+    // The internal state:
 
-  std::vector<Matrix44f> MV;
-	Param pp;
+    std::vector<Matrix44f> RotMVec;
+    Param pp;
 
-	//Matrix44f BaseRot;
-	
-	// the array of finer and finer grids
-	GridStaticObj< bool,float> u[4];
-	
-	std::vector< Point3f > movVertBase;
-	std::vector< Point3f > movNormBase;
-	Point3f movBarycenterBase;
-		
-	
+    // the array of finer and finer grids
+    GridStaticObj< bool,float> u[4];
+
+    // the mesh are represented by just two (well sampled) point clouds.
+    std::vector< Point3f > movVertBase;
+    std::vector< Point3f > movNormBase;
+
+    Point3f movBarycenterBase;
+    Point3f fixBarycenterBase;
+
 /***********************************/
 
-	Guess(){	
-	};
+    Guess(){}
 
-	template <class MSH> 
-	void Init(MSH &fixm, MSH &movm)
-	{
-		std::vector<MSH *> FixV;
-		std::vector<Matrix44f> MV;
-		Matrix44f tm; tm.SetIdentity();
-		MV.push_back(tm);
-		FixV.push_back(&fixm);
-		Init(FixV,MV,movm,tm);
-	}
+    template <class MeshType>
+    void Init(MeshType &fixm, MeshType &movm)
+    {
+        std::vector<MeshType *> FixV;
+        std::vector<Matrix44f> MV;
+        Matrix44f tm; tm.SetIdentity();
+        MV.push_back(tm);
+        FixV.push_back(&fixm);
+        Init(FixV,MV,movm,tm);
+    }
 
+    template <class MeshType>
+    void GenerateOccupancyMesh(MeshType &m, int lev, const Matrix44f &res)
+    {
+      VectorConstDataWrapper<std::vector<Point3f> > ww(this->movVertBase);
+      Matrix44f invRes = Inverse(res);
+      KdTree<float> surfTree(ww);
+      surfTree.setMaxNofNeighbors(1);
+      float scaleFact=0;
+      float squaredVoxDiag = u[lev].voxel.SquaredNorm();
+      m.Clear();
+      int cnt =0,crossCnt=0;
+      for(int i=0;i<u[lev].siz[0];++i) {
+        for(int j=0;j<u[lev].siz[1];++j) {
+          for(int k=0;k<u[lev].siz[2];++k) {
+            if(u[lev].Grid(i,j,k))
+            {
+              cnt++;
+              Point3f p;
+              u[lev].IPfToPf(Point3f(i+0.5f,j+0.5f,k+0.5f),p);
+              surfTree.doQueryK(invRes*p);
+              if(surfTree.getNeighborSquaredDistance(0) < squaredVoxDiag)
+              {
+                scaleFact = 0.5;
+                crossCnt++;
+              }
+              else scaleFact = 0.2;
+              MeshType t;
+              tri::Hexahedron(t);
+              tri::UpdatePosition<MeshType>::Scale(t,scaleFact);
+              tri::UpdatePosition<MeshType>::Translate(t,Point3f(0.5f,0.5f,0.5f));
+              tri::UpdatePosition<MeshType>::Translate(t,Point3f(float(i),float(j),float(k)));
+              tri::Append<MeshType,MeshType>::Mesh(m,t);
+            }
+          }
+        }
+      }
+      tri::UpdatePosition<MeshType>::Matrix(m,u[lev].IPtoPfMatrix());
+      qDebug("Found %i (%i) filled on %i",cnt, crossCnt, u[lev].siz[0]*u[lev].siz[1]*u[lev].siz[2]);
+    }
+    template <class MeshType>
+    void Init( std::vector<MeshType*> &FixMV, std::vector<Matrix44f> &FixTrV, MeshType &MovM, const Matrix44f & MovTr)
+    {
+          Box3f tbb;
+            // Step 1; Compute the Global BBox and init all the ug's
+            for(int i=0;i<FixMV.size();++i)
+                tbb.Add(FixTrV[i],FixMV[i]->bbox);
 
-	template <class MSH>
-	void Init( std::vector<MSH*> &FixMV, std::vector<Matrix44f> &FixTV, MSH &MovM, const Matrix44f & MovTr)
-	{
-		  Box3f tbb;
-			// Step 1; Compute the Global BBox and init all the ug's
-			for(int i=0;i<FixMV.size();++i)
-				tbb.Add(FixTV[i],FixMV[i]->bbox);
-			
-			// the UG are coaser and coarser;
-			// u[0] is the finest
-			// u[1] has 1/8 of the cells of u[0] etc etc...
-			for(int kk=0;kk<pp.UGLevels;++kk)
-			{
+            // the UG are coaser and coarser;
+            // u[0] is the finest
+            // u[1] has 1/8 of the cells of u[0] etc etc...
+            for(int kk=0;kk<pp.UGLevels;++kk)
+            {
                 u[kk].Create(tbb,pp.GridSize/pow(8.0f,float(kk)),false);
-				printf("Created grid of %i (%i %i %i)\n",u[kk].size(),u[kk].siz[0],u[kk].siz[1],u[kk].siz[2]);
-			}
-			
-			// Step 2: add all the Vertexes of Fix Meshes to the UG;
-			typename MSH::VertexIterator vi;
-			Point3f tp;
-			for(int i=0;i<FixMV.size();++i)
-				{
-					for(vi=FixMV[i]->vert.begin();vi!=FixMV[i]->vert.end();++vi)
-						{
-							tp=FixTV[i]*(*vi).P();
-							for(int kk=0;kk<pp.UGLevels;++kk)
-										u[kk].Grid(tp)=true;
-						}
-						
-				}
-		
+                qDebug("Created grid of %i (%i %i %i)\n",u[kk].size(),u[kk].siz[0],u[kk].siz[1],u[kk].siz[2]);
+            }
+
+            // Step 2: add all the Vertexes of Fix Meshes to the UG;
+            typename MeshType::VertexIterator vi;
+            Point3f tp;
+            for(int i=0;i<FixMV.size();++i)
+                {
+                    for(vi=FixMV[i]->vert.begin();vi!=FixMV[i]->vert.end();++vi)
+                        {
+                            tp=FixTrV[i]*(*vi).P();
+                            for(int kk=0;kk<pp.UGLevels;++kk)
+                                        u[kk].Grid(tp)=true;
+                        }
+
+                }
+
       // step 3; subsample the vertex of mov mesh
-			TrivialSampler<MSH> ts(movVertBase);
-			SurfaceSampling<MSH, TrivialSampler<MSH> >::Montecarlo(MovM, ts, pp.SampleNum);
-								
-			movBarycenterBase=Point3f(0,0,0);
-			for(size_t i=0;i<movVertBase.size();++i)
-					movBarycenterBase += movVertBase[i];
-			movBarycenterBase = movBarycenterBase/float(movVertBase.size());
-			
-		  // step 4; prepare the basic rotation matrixes.
-			GenRotMatrix( );
-			
-	}
+            TrivialSampler<MeshType> ts(movVertBase);
+            SurfaceSampling<MeshType, TrivialSampler<MeshType> >::Montecarlo(MovM, ts, pp.SampleNum);
 
-	void ComputeTrans(std::vector<Result> &rv);
-	void ComputeAlign(std::vector<Result> &rv);
-	void AddVert(std::vector<Point3f> &vv, Matrix44d &Tr);
-	void ApplyTransformation(const Point3f &baseTranslation, const Matrix44f &BaseRot, std::vector< Point3f > &movVert, std::vector< Point3f > &movNorm, Box3f &movBox);
+            movBarycenterBase=Point3f(0,0,0);
+            for(size_t i=0;i<movVertBase.size();++i)
+                    movBarycenterBase += movVertBase[i];
+            movBarycenterBase = movBarycenterBase/float(movVertBase.size());
+            qDebug("mov barycenter %f %f %f\n",movBarycenterBase[0],movBarycenterBase[1],movBarycenterBase[2]);
 
-	int SearchBestTranslation(	GridStaticObj<bool,float> &U,
-										const Matrix44f &BaseRot,
-										const int range, 
-										const int step, 
-										Point3f &StartTrans,
-										Point3f &BestTrans  // miglior vettore di spostamento
-										);
-	
-	
-	int SearchTranslate(const int UGLev,
-										const Matrix44d &BaseRot,
-										const int range, 
-										const int step, 
-										Point3d &BestTransV,  // miglior vettore di spostamento
-										bool Verbose);
+            fixBarycenterBase=tbb.Center();
+            qDebug("fix barycenter %f %f %f\n",fixBarycenterBase[0],fixBarycenterBase[1],fixBarycenterBase[2]);
 
-	int SearchTranslate2(const int UGLev,
-										const Matrix44d &BaseRot,
-										const int range, 
-										const int step, 
-										Point3d &BestTransV,  // miglior vettore di spostamento
-										bool Verbose);
+          // step 4; prepare the vector of the rotation matrixes.
+            GenRotMatrix( );
 
-	void GetM(Result &rr, Matrix44d &Res);
-  void GenRotMatrix();
-	Point3f InitBaseTranslation(Matrix44f &baseRot);
-	Matrix44f BuildTransformation(Matrix44f &baseRot, Point3f &BaseTrans);
+    }
+
+    void ComputeTrans(std::vector<Result> &rv);
+    void ComputeAlign(std::vector<Result> &rv);
+    void AddVert(std::vector<Point3f> &vv, Matrix44d &Tr);
+    void ApplyTransformation(const Point3f &baseTranslation, const Matrix44f &BaseRot,
+                             std::vector< Point3f > &movVert, std::vector< Point3f > &movNorm, Box3f &movBox) const;
+
+    int SearchBestTranslation(GridStaticObj<bool,float> &U,
+                                        const Matrix44f &BaseRot,
+                                        const int range,
+                                        Point3f &StartTrans,
+                                        Point3f &BestTrans  // miglior vettore di spostamento
+                                        );
+
+
+    int SearchTranslate(const int UGLev,
+                                        const Matrix44d &BaseRot,
+                                        const int range,
+                                        const int step,
+                                        Point3d &BestTransV,  // miglior vettore di spostamento
+                                        bool Verbose);
+
+    int SearchTranslate2(const int UGLev,
+                                        const Matrix44d &BaseRot,
+                                        const int range,
+                                        const int step,
+                                        Point3d &BestTransV,  // miglior vettore di spostamento
+                                        bool Verbose);
+
+    void GenRotMatrix();
+    Point3f ComputeBaseTranslation(Matrix44f &baseRot) const;
+    Matrix44f BuildTransformation(const Matrix44f &baseRot, const Point3f &BaseTrans) const ;
+    Matrix44f BuildResult(const Matrix44f &baseRot, const Point3f &BaseTrans, const Point3f &BestTrans) const;
 
 private :
 
@@ -201,9 +243,9 @@ private :
 
 /*
 First Step: Precompute a set of well Distributed Normals
-- Numero di orientamenti da testare 
+- Numero di orientamenti da testare
   funziona sempre
-	con 66 (GenNormal(1,2) ottaedro suddiviso 2 volte ) per un totale di 858 orientamenti
+    con 66 (GenNormal(1,2) ottaedro suddiviso 2 volte ) per un totale di 858 orientamenti
   con 42 (GenNormal(2,1) icosaedro suddiviso una volta) per un totale di 462 orientamenti
 */
 } // end namespace tri;
