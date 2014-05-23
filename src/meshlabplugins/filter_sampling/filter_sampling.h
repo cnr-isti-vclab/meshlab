98617534546a7d97ab1ad312a5b12df478f363d9
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2014-05-23 14:54:53 +0000

    Forgot to commit the .h in the previous commit of sampling

--- src/meshlabplugins/filter_sampling/filter_sampling.h
+++ src/meshlabplugins/filter_sampling/filter_sampling.h
@@ -8,7 +8,7 @@
 *                                                                    \      *
 * All rights reserved.                                                      *
 *                                                                           *
-* This program is free software; you can redistribute it and/or modify      *   
+* This program is free software; you can redistribute it and/or modify      *
 * it under the terms of the GNU General Public License as published by      *
 * the Free Software Foundation; either version 2 of the License, or         *
 * (at your option) any later version.                                       *
@@ -20,49 +20,37 @@
 * for more details.                                                         *
 *                                                                           *
 ****************************************************************************/
-/****************************************************************************
-  History
-$Log: sampleplugins.h,v $
-Revision 1.2  2006/11/29 00:59:21  cignoni
-Cleaned plugins interface; changed useless help class into a plain string
-
-Revision 1.1  2006/09/25 09:24:39  e_cerisoli
-add sampleplugins
-
-****************************************************************************/
-
 #ifndef FILTERDOCSAMPLINGPLUGIN_H
 #define FILTERDOCSAMPLINGPLUGIN_H
 
-#include <QObject>
 #include <common/interfaces.h>
 
 class FilterDocSampling : public QObject, public MeshFilterInterface
 {
-	Q_OBJECT
-	MESHLAB_PLUGIN_IID_EXPORTER(MESH_FILTER_INTERFACE_IID)
-	Q_INTERFACES(MeshFilterInterface)
+  Q_OBJECT
+  MESHLAB_PLUGIN_IID_EXPORTER(MESH_FILTER_INTERFACE_IID)
+  Q_INTERFACES(MeshFilterInterface)
 
-public:
-	enum {
-				FP_ELEMENT_SUBSAMPLING,
-				FP_MONTECARLO_SAMPLING,
-				FP_REGULAR_RECURSIVE_SAMPLING,
-				FP_CLUSTERED_SAMPLING,
-				FP_STRATIFIED_SAMPLING,
-				FP_HAUSDORFF_DISTANCE,
-				FP_TEXEL_SAMPLING,
-				FP_VERTEX_RESAMPLING,
-				FP_UNIFORM_MESH_RESAMPLING,
-				FP_VORONOI_CLUSTERING,
-				FP_VORONOI_COLORING,
-				FP_DISK_COLORING,
-				FP_POISSONDISK_SAMPLING,
-				FP_VARIABLEDISK_SAMPLING
-			} ;
+  public:
+    enum {
+    FP_ELEMENT_SUBSAMPLING,
+        FP_MONTECARLO_SAMPLING,
+        FP_REGULAR_RECURSIVE_SAMPLING,
+        FP_CLUSTERED_SAMPLING,
+        FP_STRATIFIED_SAMPLING,
+        FP_HAUSDORFF_DISTANCE,
+        FP_TEXEL_SAMPLING,
+        FP_VERTEX_RESAMPLING,
+        FP_UNIFORM_MESH_RESAMPLING,
+        FP_VORONOI_CLUSTERING,
+        FP_VORONOI_COLORING,
+        FP_DISK_COLORING,
+        FP_POISSONDISK_SAMPLING,
+        FP_POINTCLOUD_SIMPLIFICATION
+  } ;
 
-	FilterDocSampling();
-	
+  FilterDocSampling();
+
   QString filterName(FilterIDType filter) const;
   QString filterInfo(FilterIDType filter) const;
   void initParameterSet(QAction *,MeshDocument &/*m*/, RichParameterSet & /*parent*/);
