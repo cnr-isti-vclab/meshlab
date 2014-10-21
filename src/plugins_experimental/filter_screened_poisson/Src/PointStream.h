cdba9417ed706e75c1c666aee11c3c8869a9bd0a
Paolo Cignoni cignoni <paolo.cignoni@isti.cnr.it>
2014-10-21 17:05:20 +0000

    Poisson Changes: - Added a Class MemoryPointStream for using setTree methods without relying on the filesystem

--- src/plugins_experimental/filter_screened_poisson/Src/PointStream.h
+++ src/plugins_experimental/filter_screened_poisson/Src/PointStream.h
@@ -8,14 +8,14 @@ are permitted provided that the following conditions are met:
 Redistributions of source code must retain the above copyright notice, this list of
 conditions and the following disclaimer. Redistributions in binary form must reproduce
 the above copyright notice, this list of conditions and the following disclaimer
-in the documentation and/or other materials provided with the distribution. 
+in the documentation and/or other materials provided with the distribution.
 
 Neither the name of the Johns Hopkins University nor the names of its contributors
 may be used to endorse or promote products derived from this software without specific
-prior writften permission. 
+prior writften permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
-EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES
 OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
 SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
 INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
@@ -33,51 +33,66 @@ template< class Real >
 class PointStream
 {
 public:
-	virtual ~PointStream( void ){}
-	virtual void reset( void ) = 0;
-	virtual bool nextPoint( Point3D< Real >& p , Point3D< Real >& n ) = 0;
+    virtual ~PointStream( void ){}
+    virtual void reset( void ) = 0;
+    virtual bool nextPoint( Point3D< Real >& p , Point3D< Real >& n ) = 0;
 };
 
 template< class Real >
 class ASCIIPointStream : public PointStream< Real >
 {
-	FILE* _fp;
+    FILE* _fp;
 public:
-	ASCIIPointStream( const char* fileName );
-	~ASCIIPointStream( void );
-	void reset( void );
-	bool nextPoint( Point3D< Real >& p , Point3D< Real >& n );
+    ASCIIPointStream( const char* fileName );
+    ~ASCIIPointStream( void );
+    void reset( void );
+    bool nextPoint( Point3D< Real >& p , Point3D< Real >& n );
 };
 
 template< class Real >
 class BinaryPointStream : public PointStream< Real >
 {
-	FILE* _fp;
-	static const int POINT_BUFFER_SIZE=1024;
-	Real _pointBuffer[ POINT_BUFFER_SIZE * 2 * 3 ];
-	int _pointsInBuffer , _currentPointIndex;
+    FILE* _fp;
+    static const int POINT_BUFFER_SIZE=1024;
+    Real _pointBuffer[ POINT_BUFFER_SIZE * 2 * 3 ];
+    int _pointsInBuffer , _currentPointIndex;
 public:
-	BinaryPointStream( const char* filename );
-	~BinaryPointStream( void );
-	void reset( void );
-	bool nextPoint( Point3D< Real >& p , Point3D< Real >& n );
+    BinaryPointStream( const char* filename );
+    ~BinaryPointStream( void );
+    void reset( void );
+    bool nextPoint( Point3D< Real >& p , Point3D< Real >& n );
 };
+
+template< class Real >
+class MemoryPointStream : public PointStream< Real >
+{
+     char* _pVec, *_nVec;
+    size_t _pStride, _nStride;
+    size_t _curPos;
+    size_t  _pointNum;
+public:
+    MemoryPointStream(  void*pVec, size_t stride_p,  void *nVec, size_t stride_n, size_t pointNum);
+    ~MemoryPointStream( void );
+    void reset( void );
+    bool nextPoint( Point3D< Real >& p , Point3D< Real >& n );
+};
+
 
 template< class Real >
 class PLYPointStream : public PointStream< Real >
 {
-	char* _fileName;
-	PlyFile* _ply;
-	int _nr_elems;
-	char **_elist;
+    char* _fileName;
+    PlyFile* _ply;
+    int _nr_elems;
+    char **_elist;
 
-	int _pCount , _pIdx;
-	void _free( void );
+    int _pCount , _pIdx;
+    void _free( void );
 public:
-	PLYPointStream( const char* fileName );
-	~PLYPointStream( void );
-	void reset( void );
-	bool nextPoint( Point3D< Real >& p , Point3D< Real >& n );
+    PLYPointStream( const char* fileName );
+    ~PLYPointStream( void );
+    void reset( void );
+    bool nextPoint( Point3D< Real >& p , Point3D< Real >& n );
 };
 #include "PointStream.inl"
 #endif // POINT_STREAM_INCLUDED
