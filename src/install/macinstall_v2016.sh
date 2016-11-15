6a538663a69b14d3389207623bd525723b62d646
Paolo Cignoni <paolo.cignoni@isti.cnr.it>
2016-11-16 00:01:31 +0100

    New probably working mac installer.
    uses the qt deploy tool

--- src/install/macinstall_v2016.sh
+++ src/install/macinstall_v2016.sh
@@ -19,16 +19,6 @@ APPNAME="meshlab.app"
 APPFOLDER=$BUILDPATH/distrib/$APPNAME
 BUNDLE="MeshLabBundle"
 
-QTCOMPONENTS="QtCore QtGui QtOpenGL QtNetwork QtXml QtXmlPatterns QtScript QtWidgets"
-
-QTCORE=QtCore.framework/Versions/5/QtCore
-QTGUI=QtGui.framework/Versions/5/QtGui
-QTNETWORK=QtNetwork.framework/Versions/5/QtNetwork
-QTSCRIPT=QtScript.framework/Versions/5/QtScript
-QTXMLPATTERNS=QtXmlPatterns.framework/Versions/5/QtXmlPatterns
-QTXML=QtXml.framework/Versions/5/QtXml
-QTOPENGL=QtOpenGL.framework/Versions/5/QtOpenGL
-QTWIDGETS=QtWidgets.framework/Versions/5/QtWidgets
 
 if [ -e $APPFOLDER -a -d $APPFOLDER ]
 then
@@ -38,31 +28,27 @@ else
   exit 0
 fi
 
-echo "Copying the built app into the bundle"
-
+# Start by erasing everything
 rm -r -f $BUNDLE
 
+echo "Copying the built app into the bundle"
 mkdir $BUNDLE
 cp -r $APPFOLDER $BUNDLE
-
+mkdir $BUNDLE/$APPNAME/Contents/PlugIns
 # copy the files icons into the app.
 cp ../meshlab/images/meshlab_obj.icns $BUNDLE/$APPNAME/Contents/Resources
-# copy the qt.conf file in the resources to avoid problems with qt's jpg dll
-cp ../install/qt.conf $BUNDLE/$APPNAME/Contents/Resources
 
-read -rsp $'Copying samples and other files. Press enter to continue...\n'
+for x in $BUILDPATH/distrib/plugins/*.dylib
+do
+cp ./$x $BUNDLE/meshlab.app/Contents/PlugIns/
+done
 
-mkdir $BUNDLE/$APPNAME/Contents/Frameworks   
-mkdir $BUNDLE/$APPNAME/Contents/plugins   
-mkdir $BUNDLE/$APPNAME/Contents/plugins/imageformats 
-mkdir $BUNDLE/$APPNAME/Contents/plugins/platforms 
-  
-mkdir $BUNDLE/$APPNAME/Contents/plugins/U3D_OSX  
-cp plugins/U3D_OSX/IDTFConverter.out  $BUNDLE/$APPNAME/Contents/plugins/U3D_OSX
-cp plugins/U3D_OSX/IDTFConverter.sh  $BUNDLE/$APPNAME/Contents/plugins/U3D_OSX
-cp plugins/U3D_OSX/libIFXCore.so  $BUNDLE/$APPNAME/Contents/plugins/U3D_OSX
-mkdir $BUNDLE/$APPNAME/Contents/plugins/U3D_OSX/Plugins
-cp plugins/U3D_OSX/Plugins/libIFXExporting.so  $BUNDLE/$APPNAME/Contents/plugins/U3D_OSX/Plugins
+for x in $BUNDLE/meshlab.app/Contents/plugins/*.dylib
+do
+ install_name_tool -change libcommon.1.dylib @executable_path/libcommon.1.dylib $x
+done
+
+echo 'Copying samples and other files'
 
 cp ../../docs/gpl.txt $BUNDLE
 cp ../../docs/readme.txt $BUNDLE
@@ -83,6 +69,14 @@ cp sample/chameleon4k.pts $BUNDLE/sample
 cp sample/normalmap/laurana500.* $BUNDLE/sample/normalmap
 cp sample/normalmap/matteonormb.* $BUNDLE/sample/normalmap
 
+
+mkdir $BUNDLE/$APPNAME/Contents/plugins/U3D_OSX  
+cp plugins/U3D_OSX/IDTFConverter.out  $BUNDLE/$APPNAME/Contents/plugins/U3D_OSX
+cp plugins/U3D_OSX/IDTFConverter.sh  $BUNDLE/$APPNAME/Contents/plugins/U3D_OSX
+cp plugins/U3D_OSX/libIFXCore.so  $BUNDLE/$APPNAME/Contents/plugins/U3D_OSX
+mkdir $BUNDLE/$APPNAME/Contents/plugins/U3D_OSX/Plugins
+cp plugins/U3D_OSX/Plugins/libIFXExporting.so  $BUNDLE/$APPNAME/Contents/plugins/U3D_OSX/Plugins
+
 mkdir $BUNDLE/$APPNAME/Contents/textures   
 cp textures/*.png $BUNDLE/$APPNAME/Contents/textures/
 mkdir $BUNDLE/$APPNAME/Contents/textures/cubemaps   
@@ -98,90 +92,7 @@ mkdir $BUNDLE/$APPNAME/Contents/shaders/shadersrm
 cp shaders/shadersrm/*.rfx $BUNDLE/$APPNAME/Contents/shaders/shadersrm
 #added shadowmapping shaders
 cp -r shaders/decorate_shadow $BUNDLE/$APPNAME/Contents/shaders
-echo 'Copying Qt Components into the app'
-#read -rsp $'Press enter to continue...\n'
 
-for x in $QTCOMPONENTS
-do
-#    cp -R $QTPATH/lib/$x.framework $BUNDLE/$APPNAME/Contents/Frameworks 
-rsync -quiet -avu --exclude='*debug*' $QTPATH/lib/$x.framework $BUNDLE/$APPNAME/Contents/Frameworks
-done
-
-echo "Changing the paths of the qt component frameworks"
-#read -rsp $'Press enter to continue...\n'
-
-  for x in $QTCOMPONENTS
-  do
-   install_name_tool -id  @executable_path/../Frameworks/$x.framework/Versions/Current/$x $BUNDLE/meshlab.app/Contents/Frameworks/$x.framework/Versions/Current/$x
-  done
-
-install_name_tool -change $QTPATH/lib/$QTCORE    @executable_path/../Frameworks/$QTCORE    $BUNDLE/meshlab.app/Contents/Frameworks/$QTGUI
-install_name_tool -change $QTPATH/lib/$QTCORE    @executable_path/../Frameworks/$QTCORE    $BUNDLE/meshlab.app/Contents/Frameworks/$QTXML
-install_name_tool -change $QTPATH/lib/$QTCORE    @executable_path/../Frameworks/$QTCORE    $BUNDLE/meshlab.app/Contents/Frameworks/$QTXMLPATTERNS
-install_name_tool -change $QTPATH/lib/$QTNETWORK @executable_path/../Frameworks/$QTNETWORK $BUNDLE/meshlab.app/Contents/Frameworks/$QTXMLPATTERNS
-install_name_tool -change $QTPATH/lib/$QTCORE    @executable_path/../Frameworks/$QTCORE    $BUNDLE/meshlab.app/Contents/Frameworks/$QTNETWORK
-install_name_tool -change $QTPATH/lib/$QTCORE    @executable_path/../Frameworks/$QTCORE    $BUNDLE/meshlab.app/Contents/Frameworks/$QTSCRIPT
-install_name_tool -change $QTPATH/lib/$QTCORE    @executable_path/../Frameworks/$QTCORE    $BUNDLE/meshlab.app/Contents/Frameworks/$QTOPENGL
-install_name_tool -change $QTPATH/lib/$QTGUI     @executable_path/../Frameworks/$QTGUI     $BUNDLE/meshlab.app/Contents/Frameworks/$QTOPENGL
-
-echo "Copying the meshlab plugins and xml's in the meshlab package" #--------------------------
-#read -rsp $'Press enter to continue...\n'
-
-
-for x in $BUILDPATH/distrib/plugins/*.dylib
-do
-cp ./$x $BUNDLE/meshlab.app/Contents/plugins/
-done
-pwd
-for x in $BUILDPATH/distrib/plugins/*.xml
-do
-cp ./$x $BUNDLE/meshlab.app/Contents/plugins/
-done
-
-echo "Copying the qt plugins" 
-#read -rsp $'Press enter to continue...\n'
-
-for x in $QTPATH/plugins/imageformats/*.dylib
-do
-  PLUGINNAME=$(basename $x)
-  cp $x $BUNDLE/meshlab.app/Contents/plugins/imageformats
-  install_name_tool -change $QTPATH/lib/$QTCORE  @executable_path/../Frameworks/$QTCORE  $BUNDLE/meshlab.app/Contents/plugins/imageformats/$PLUGINNAME
-  install_name_tool -change $QTPATH/lib/$QTGUI   @executable_path/../Frameworks/$QTGUI   $BUNDLE/meshlab.app/Contents/plugins/imageformats/$PLUGINNAME
-done
-for x in $QTPATH/plugins/platforms/*.dylib
-do
-  PLUGINNAME=$(basename $x)
-  cp $x $BUNDLE/meshlab.app/Contents/plugins/platforms
-  install_name_tool -change $QTPATH/lib/$QTCORE  @executable_path/../Frameworks/$QTCORE  $BUNDLE/meshlab.app/Contents/plugins/platforms/$PLUGINNAME
-  install_name_tool -change $QTPATH/lib/$QTGUI   @executable_path/../Frameworks/$QTGUI   $BUNDLE/meshlab.app/Contents/plugins/platforms/$PLUGINNAME
-done
-
-echo "Now Changing how each exec (meshlab and all the dll refers) " #--------------------------
-#read -rsp $'Press enter to continue...\n'
-
-#EXECNAMES="MacOS/libcommon.1.dylib MacOS/meshlab $PLUGINSNAMESLIB" 
-
-for x in $BUNDLE/meshlab.app/Contents/plugins/*.dylib
-do
-PLUGINNAME=$(basename $x)
-EXECNAMES="$EXECNAMES plugins/$PLUGINNAME"
-done
-#EXECNAMES="MacOS/libcommon.1.dylib MacOS/meshlab $EXECNAMES" 
-EXECNAMES="MacOS/meshlab" 
-echo "iii $EXECNAMES"
-#read -rsp $'Press enter to continue...\n'
-
-for x in $EXECNAMES 
-do
-  install_name_tool -change @rpath/$QTCORE @executable_path/../Frameworks/$QTCORE       $BUNDLE/meshlab.app/Contents/$x
-  install_name_tool -change @rpath/$QTGUI @executable_path/../Frameworks/$QTGUI         $BUNDLE/meshlab.app/Contents/$x
-  install_name_tool -change @rpath/$QTNETWORK @executable_path/../Frameworks/$QTNETWORK $BUNDLE/meshlab.app/Contents/$x
-  install_name_tool -change @rpath/$QTOPENGL @executable_path/../Frameworks/$QTOPENGL   $BUNDLE/meshlab.app/Contents/$x
-  install_name_tool -change @rpath/$QTXML @executable_path/../Frameworks/$QTXML         $BUNDLE/meshlab.app/Contents/$x
-  install_name_tool -change @rpath/$QTXMLPATTERNS @executable_path/../Frameworks/$QTXMLPATTERNS $BUNDLE/meshlab.app/Contents/$x
-  install_name_tool -change @rpath/$QTSCRIPT @executable_path/../Frameworks/$QTSCRIPT   $BUNDLE/meshlab.app/Contents/$x
-  install_name_tool -change @rpath/$QTWIDGETS @executable_path/../Frameworks/$QTWIDGETS $BUNDLE/meshlab.app/Contents/$x
-  install_name_tool -change libcommon.1.dylib @executable_path/libcommon.1.dylib        $BUNDLE/meshlab.app/Contents/$x
-done
-
+echo "Changing the paths of the qt component frameworks using the qt tool macdeployqt"
+$QTPATH/bin/macdeployqt $BUNDLE/$APPNAME -verbose=2
 cd ../install
