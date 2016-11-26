eba742dff67b199547df95ac4c8da5eadb955931
Paolo Cignoni <paolo.cignoni@isti.cnr.it>
2016-11-26 16:31:49 +0100

    Small changes to make compile the old structure synth lib on OS X

--- src/external/structuresynth-1.5/ssynth/SyntopiaCore/GLEngine/Raytracer/VoxelStepper.cpp
+++ src/external/structuresynth-1.5/ssynth/SyntopiaCore/GLEngine/Raytracer/VoxelStepper.cpp
@@ -122,7 +122,7 @@ namespace SyntopiaCore {
 						currentT = p;
 
 						// We do not intersect grid.
-						if (!found) return false;
+						if (!found) return nullptr;
 				}
 
 				stepX = (dir.x() > 0) ? 1 : -1;
