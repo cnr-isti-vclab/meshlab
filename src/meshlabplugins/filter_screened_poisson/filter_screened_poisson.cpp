ec6129c9fd9611c8276253f2e8f5566bbbe8d376
Paolo Cignoni <paolo.cignoni@isti.cnr.it>
2017-02-28 16:39:41 +0100

    Corrected Cleaning for poisson (removal of vertexes with bad normals)

--- src/meshlabplugins/filter_screened_poisson/filter_screened_poisson.cpp
+++ src/meshlabplugins/filter_screened_poisson/filter_screened_poisson.cpp
@@ -555,11 +555,15 @@ void PoissonClean(MeshType &m, bool scaleNormal, bool cleanFlag)
   vcg::tri::UpdateNormal<MeshType>::NormalizePerVertex(m);
 
   if(cleanFlag) {
-	for (auto vi = m.vert.begin(); vi != m.vert.end(); ++vi)
-		if (vcg::SquaredNorm(vi->N()) < std::numeric_limits<float>::min()*10.0)
-			vi->setD();
+    for (auto vi = m.vert.begin(); vi != m.vert.end(); ++vi)
+      if (vcg::SquaredNorm(vi->N()) < std::numeric_limits<float>::min()*10.0)
+        vcg::tri::Allocator<MeshType>::DeleteVertex(m,*vi);
+ 
+    for (auto fi = m.face.begin(); fi != m.face.end(); ++fi)
+        if( fi->V(0)->IsD() || fi->V(1)->IsD() || fi->V(2)->IsD() ) 
+          vcg::tri::Allocator<MeshType>::DeleteFace(m,*fi);          
   }
-
+  
   vcg::tri::Allocator<MeshType>::CompactEveryVector(m);
   if(scaleNormal)
   {
