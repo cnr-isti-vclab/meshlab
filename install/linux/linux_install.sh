eb69e83dccefd99983dbe580dc6435f1cfa01a00
alemuntoni <muntoni.alessandro@gmail.com>
2020-01-24 11:02:12 +0100

    fixing deploying paths in ubuntu

--- install/linux/linux_install.sh
+++ install/linux/linux_install.sh
@@ -15,21 +15,15 @@ else
   exit 1
 fi
 
-rm -r sample/
-mkdir lib
-cp libcommon.so.1.0.0 lib/libcommon.so.1.0.0
-cp lib/libcommon.so.1.0.0 lib/libcommon.so.1.0
-cp lib/libcommon.so.1.0.0 lib/libcommon.so.1
-cp lib/libcommon.so.1.0.0 lib/libcommon.so
 cp $INSTALL_PATH/../meshlab.png .
 cp $INSTALL_PATH/default.desktop .
-cp ../../LICENSE.txt .
-cp ../../docs/readme.txt .
+cp ../LICENSE.txt .
+cp ../docs/readme.txt .
 
 rm -r plugins/U3D_OSX/
 rm -r plugins/U3D_W32/
 
 $INSTALL_PATH/linuxdeployqt meshlab -appimage
 
-#at this moment, the distrib folder contains all the files necessary to execute meshlab
+#at this point, distrib folder contains all the files necessary to execute meshlab
 echo "distrib folder is now a self contained meshlab application"
