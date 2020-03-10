c1cc9e675a3528104bf803d788b95eff59dfab39
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-03-10 14:05:59 -0500

    Fix ambiguous reference to glClearColor

--- src/meshlab/glarea.cpp
+++ src/meshlab/glarea.cpp
@@ -441,9 +441,9 @@ void GLArea::paintEvent(QPaintEvent* /*event*/)
         hasToUpdateTexture=false;
     }*/
 
-    glClearColor(1.0,1.0,1.0,0.0);
+    ::glClearColor(1.0,1.0,1.0,0.0);
     if (takeSnapTile && (ss.background == 3))
-        glClearColor(0.0, 0.0, 0.0, 0.0);
+        ::glClearColor(0.0, 0.0, 0.0, 0.0);
     glEnable(GL_DEPTH_TEST);
     glDepthMask(GL_TRUE);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
