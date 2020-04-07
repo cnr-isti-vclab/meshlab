c0ce2b3ea8514a184cd20f63739039cb63719bf7
alemuntoni <muntoni.alessandro@gmail.com>
2020-04-07 14:40:19 +0200

    CMake build for use_cpu_opengl subproject

--- src/use_cpu_opengl/main.cpp
+++ src/use_cpu_opengl/main.cpp
@@ -4,44 +4,38 @@
  * @author Alessandro Muntoni (muntoni.alessandro@gmail.com)
  */
 
+#ifdef _WIN32
 #include <QFile>
 #include <QApplication>
 #include <QMessageBox>
-
-#ifdef _WIN32
 #include <windows.h>
-#endif //_WIN32
 
 bool renameOrCopy();
+
+#ifdef UNICODE
+#define MESHLAB_EXE L"meshlab.exe"
+#else
+#define MESHLAB_EXE "meshlab.exe"
+#endif //UNICODE
+
+#endif //_WIN32
 
 int main(int argc, char* argv[])
 {
     bool res = false;
-    if (argc > 1){
+    #ifdef _WIN32
+    if (argc > 1) {
         res = renameOrCopy();
-        #ifdef _WIN32
         if (res){
             //need to start a new meshlab process, using the beautiful call
             //CreateProcess of windows...
-            PROCESS_INFORMATION pif;  //Gives info on the thread and..
-                                       //..process for the new process
-            STARTUPINFO si;          //Defines how to start the program
+            PROCESS_INFORMATION pif;
+            STARTUPINFO si;
 
             ZeroMemory(&si,sizeof(si)); //Zero the STARTUPINFO struct
             si.cb = sizeof(si);         //Must set size of structure
-            CreateProcess(
-                                L"meshlab.exe", //Path to executable file
-                                NULL,   //Command string - not needed here
-                                NULL,   //Process handle not inherited
-                                NULL,   //Thread handle not inherited
-                                false,  //No inheritance of handles
-                                0,      //No special flags
-                                NULL,   //Same environment block as this prog
-                                NULL,   //Current directory - no separate path
-                                &si,    //Pointer to STARTUPINFO
-                                &pif);   //Pointer to PROCESS_INFORMATION
+            CreateProcess(MESHLAB_EXE, NULL, NULL, NULL, false, 0, NULL, NULL, &si, &pif);
         }
-        #endif //_WIN32
     }
     else {
         QApplication app(argc, argv);
@@ -62,12 +56,12 @@ int main(int argc, char* argv[])
             res = renameOrCopy();
 
             if (!res){
-                QMessageBox messageBox(QMessageBox::Critical, "Error", "Error while copying opengl32.dll");
+                QMessageBox messageBox(QMessageBox::Critical, "Error", "Error while renaming/copying opengl32.dll");
                 messageBox.exec();
                 return -1;
             }
             else {
-                QMessageBox messageBox(QMessageBox::Information, "Ok", "opengl32.dll copied correctly");
+                QMessageBox messageBox(QMessageBox::Information, "Ok", "opengl32.dll renamed/copied correctly");
                 messageBox.exec();
                 return 0;
             }
@@ -78,14 +72,18 @@ int main(int argc, char* argv[])
 
         return app.exec();
     }
+    #else
+    std::cerr << "This program is just for windows systems.\n";
+    #endif //_WIN32
     return res ? 0 : -1;
 }
 
+#ifdef _WIN32
 bool renameOrCopy() {
     bool res;
 
     if (QFile::exists("opengl32sw.dll")){
-        QFile::rename("opengl32sw.dll", "opengl32.dll");
+        res = QFile::rename("opengl32sw.dll", "opengl32.dll");
     }
     else {
         res = QFile::copy(":/dll/opengl32.dll", "opengl32.dll");
@@ -93,3 +91,4 @@ bool renameOrCopy() {
 
     return res;
 }
+#endif //_WIN32
