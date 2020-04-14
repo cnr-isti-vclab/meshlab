36f7fd1b90d86654ae4af40e42870b4d5a4d61bf
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-04-14 12:31:41 -0500

    cmake: Make templates match again

--- src/templates/common.cmake
+++ src/templates/common.cmake
@@ -1,4 +1,11 @@
 {% extends "CMakeLists.template.cmake" %}
+
+{% block before_vars %}
+if (EXISTS ../../ML_VERSION)
+    file(READ ../../ML_VERSION MESHLAB_VERSION)
+    add_definitions(-DMESHLAB_VERSION=${MESHLAB_VERSION})
+endif()
+{% endblock %}
 {% block outputdir %}{%endblock%}
 {% block add_target %}
 set(TARGET_TYPE SHARED)
