6052a444da20df3402180b1d7e648e2e374d92a4
alemuntoni <muntoni.alessandro@gmail.com>
2020-06-13 21:02:24 +0200

    tmp solution appimage and snap: moving libIFX to bin dir

--- install/linux/linux_appimages.sh
+++ install/linux/linux_appimages.sh
@@ -12,6 +12,7 @@
 # You can give as argument the DISTRIB_PATH.
 
 cd "${0%/*}" #move to script directory
+INSTALL_PATH=$(pwd)
 
 #checking for parameters
 if [ "$#" -eq 0 ]
@@ -21,68 +22,32 @@ else
     DISTRIB_PATH=$1
 fi
 
-SOURCE_PATH=$PWD/../../src
+sh linux_make_boundle.sh $@
 
-INSTALL_PATH=$(pwd)
 cd $DISTRIB_PATH
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
-
-#check if we have an exec in distrib
-if ! [ -f meshlab ]
-then
-  echo "ERROR: meshlab bin not found inside distrib"
-  exit 1
-fi
-
-rm -r lib/macx64/
-rm -r lib/win32-msvc/
-rm -r lib/win32-msvc2008/
-rm -r lib/win32-msvc2015/
-rm -r lib/readme.txt
-rm -r plugins/plugins.txt
-rm -r README.md
-
-mkdir -p usr/bin
-mkdir -p usr/lib/meshlab
-mkdir -p usr/share/applications
-mkdir -p usr/share/meshlab
-mkdir -p usr/share/doc/meshlab
-mkdir -p usr/share/icons/hicolor/512x512/apps/
-mkdir -p usr/share/icons/Yaru/512x512/apps/
-
-cp $INSTALL_PATH/resources/meshlab_server.desktop usr/share/applications/
-cp meshlab.png usr/share/icons/hicolor/512x512/apps/meshlab.png
-mv meshlab.png usr/share/icons/Yaru/512x512/apps/meshlab.png
-mv meshlabserver usr/bin
-mv LICENSE.txt usr/share/doc/meshlab/
-mv privacy.txt usr/share/doc/meshlab/
-mv readme.txt usr/share/doc/meshlab/
-mv lib/libmeshlab-common* usr/lib/
-mv plugins/ usr/lib/meshlab/
-mv lib/linux/* usr/lib/meshlab/
-#chrpath -r ../lib/ usr/lib/meshlab/plugins/*.so
-mv shaders/ usr/share/meshlab/
 
 export VERSION=$(cat $INSTALL_PATH/../../ML_VERSION)
+
+mv usr/bin/meshlabserver .
+$INSTALL_PATH/resources/linuxdeployqt usr/share/applications/meshlab.desktop -appimage
+mv *.AppImage ../MeshLab$VERSION-linux.AppImage
+chmod +x ../MeshLab$VERSION-linux.AppImage
+
+rm AppRun
+rm *.desktop
+rm *.png
+rm usr/share/applications/meshlab.desktop
+cp $INSTALL_PATH/resources/meshlab_server.desktop usr/share/applications/
+mv usr/bin/meshlab .
+mv meshlabserver usr/bin
 
 $INSTALL_PATH/resources/linuxdeployqt usr/share/applications/meshlab_server.desktop -appimage
 mv *.AppImage ../MeshLabServer$VERSION-linux.AppImage
 chmod +x ../MeshLabServer$VERSION-linux.AppImage
 
-rm AppRun
-rm *.desktop
-rm *.png
-rm usr/share/applications/meshlab_server.desktop
+
 cp $INSTALL_PATH/resources/default.desktop usr/share/applications/meshlab.desktop
-mv usr/bin/meshlabserver .
-mv meshlab usr/bin
-
-$INSTALL_PATH/resources/linuxdeployqt usr/share/applications/meshlab.desktop -appimage
-mv *.AppImage ../MeshLab$VERSION-linux.AppImage
-chmod +x ../MeshLab$VERSION-linux.AppImage
-
-cp $INSTALL_PATH/resources/meshlab_server.desktop usr/share/applications/
-mv meshlabserver usr/bin/
+mv meshlab usr/bin/
 
 rm -r lib
 
--- install/linux/linux_deploy.sh
+++ install/linux/linux_deploy.sh
@@ -10,6 +10,7 @@
 # You can give as argument the DISTRIB_PATH.
 
 cd "${0%/*}" #move to script directory
+INSTALL_PATH=$(pwd)
 
 #checking for parameters
 if [ "$#" -eq 0 ]
@@ -19,46 +20,9 @@ else
     DISTRIB_PATH=$1
 fi
 
-SOURCE_PATH=$PWD/../../src
+sh linux_make_boundle.sh $@
 
-INSTALL_PATH=$(pwd)
 cd $DISTRIB_PATH
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
-
-#check if we have an exec in distrib
-if ! [ -f meshlab ]
-then
-  echo "ERROR: meshlab bin not found inside distrib"
-  exit 1
-fi
-
-rm -r lib/macx64/
-rm -r lib/win32-msvc/
-rm -r lib/win32-msvc2008/
-rm -r lib/win32-msvc2015/
-rm -r lib/readme.txt
-rm -r plugins/plugins.txt
-rm -r README.md
-
-mkdir -p usr/bin
-mkdir -p usr/lib/meshlab
-mkdir -p usr/share/applications
-mkdir -p usr/share/meshlab
-mkdir -p usr/share/doc/meshlab
-mkdir -p usr/share/icons/hicolor/512x512/apps/
-
-cp $INSTALL_PATH/resources/default.desktop usr/share/applications/meshlab.desktop
-mv meshlab.png usr/share/icons/hicolor/512x512/apps/meshlab.png
-mv meshlab usr/bin
-mv meshlabserver usr/bin
-mv LICENSE.txt usr/share/doc/meshlab/
-mv privacy.txt usr/share/doc/meshlab/
-mv readme.txt usr/share/doc/meshlab/
-mv lib/libmeshlab-common* usr/lib/
-mv plugins/ usr/lib/meshlab/
-mv lib/linux/* usr/lib/meshlab/
-#chrpath -r ../lib/ usr/lib/meshlab/plugins/*.so
-mv shaders/ usr/share/meshlab/
 
 $INSTALL_PATH/resources/linuxdeployqt usr/share/applications/meshlab.desktop -bundle-non-qt-libs -executable=usr/bin/meshlabserver
 
