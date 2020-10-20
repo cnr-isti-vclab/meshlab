af67dc48b84f725b822c13c8f03dc3537780dfad
alemuntoni <muntoni.alessandro@gmail.com>
2020-10-20 14:35:07 +0200

    copy data to windows portable folder

--- scripts/windows/windows_deploy.sh
+++ scripts/windows/windows_deploy.sh
@@ -7,6 +7,7 @@
 # You can give as argument the path where meshlab is installed.
 
 SCRIPTS_PATH="$(dirname "$(realpath "$0")")"
+DISTRIB_PATH=$SCRIPTS_PATH/../../distrib
 
 #checking for parameters
 if [ "$#" -eq 0 ]
@@ -20,6 +21,8 @@ windeployqt $BUNDLE_PATH/meshlab.exe
 
 mv $BUNDLE_PATH/lib/meshlab/IFX* $BUNDLE_PATH
 cp $BUNDLE_PATH/IFXCoreStatic.lib $BUNDLE_PATH/lib/meshlab/
+cp $DISTRIB_PATH/LICENSE.txt $BUNDLE_PATH/
+cp $DISTRIB_PATH/privacy.txt $BUNDLE_PATH/
 
 #at this point, distrib folder contains all the files necessary to execute meshlab
 echo "$BUNDLE_PATH is now a self contained meshlab application"
