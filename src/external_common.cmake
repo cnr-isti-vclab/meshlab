34e5d4fcffd8cfd5204f5255e60807a19d5f831a
alemuntoni <muntoni.alessandro@gmail.com>
2020-10-28 16:20:31 +0100

    moved vcglib to external_common.cmake

--- src/external_common.cmake
+++ src/external_common.cmake
@@ -2,12 +2,30 @@
 set(GLEW_DIR ${EXTERNAL_DIR}/glew-2.1.0)
 add_subdirectory(${GLEW_DIR})
 
+
+# VCGLIb -- required
+if(NOT VCGDIR)
+	get_filename_component(VCGDIR "${CMAKE_CURRENT_SOURCE_DIR}/vcglib" ABSOLUTE)
+	if(NOT EXISTS ${VCGDIR})
+		set(VCGDIR NOTFOUND)
+	endif()
+endif()
+set(VCGDIR
+	"${VCGDIR}")
+
+if(NOT VCGDIR)
+	message(FATAL_ERROR "VCGLib not found. Please clone recursively the MeshLab repo.")
+endif()
+include_directories(${VCGDIR} ${CMAKE_CURRENT_SOURCE_DIR})
+
+
 # Eigen3 - required
 set(EIGEN_DIR ${VCGDIR}/eigenlib)
 if(EIGEN3_INCLUDE_DIR)
-    message(STATUS "- Eigen - using system-provided library")
-    set(EIGEN_INCLUDE_DIRS ${EIGEN3_INCLUDE_DIR})
+	message(STATUS "- Eigen - using system-provided library")
+	set(EIGEN_INCLUDE_DIRS ${EIGEN3_INCLUDE_DIR})
 else()
-    message(STATUS "- Eigen - using bundled source")
-    set(EIGEN_INCLUDE_DIRS ${EIGEN_DIR})
+	message(STATUS "- Eigen - using bundled source")
+	set(EIGEN_INCLUDE_DIRS ${EIGEN_DIR})
 endif()
+include_directories(${EIGEN_INCLUDE_DIRS})
