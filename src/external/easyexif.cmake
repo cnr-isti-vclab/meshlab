65302ecc2d2d876588c6ef8f0f17ecb0b81b89cf
alemuntoni <muntoni.alessandro@gmail.com>
2021-04-22 15:05:11 +0200

    external-exif lib

---
+++ src/external/easyexif.cmake
@@ -0,0 +1,17 @@
+# Copyright 2019, 2020, Collabora, Ltd.
+# Copyright 2019, 2020, Visual Computing Lab, ISTI - Italian National Research Council
+# SPDX-License-Identifier: BSL-1.0
+
+option(ALLOW_BUNDLED_EXIF "Allow use of bundled EasyExif source" ON)
+
+set(EXIF_DIR ${EXTERNAL_DIR}/easyexif)
+
+if(ALLOW_BUNDLED_EXIF AND EXISTS "${EXIF_DIR}/exif.h")
+	message(STATUS "- exif - using bundled source")
+	add_library(external-exif STATIC ${EXIF_DIR}/exif.h ${EXIF_DIR}/exif.cpp)
+	target_include_directories(external-exif PUBLIC ${EXIF_DIR})
+else()
+	message(
+		FATAL_ERROR
+			"Exif is required - ALLOW_BUNDLED_EXIF must be enabled and found.")
+endif()
