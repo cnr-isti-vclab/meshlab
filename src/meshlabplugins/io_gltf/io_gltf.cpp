6510cc854adb15be674476bc611bbdefe41d997e
alemuntoni <muntoni.alessandro@gmail.com>
2021-06-29 10:52:47 +0200

    load vnormals, code reorganization

--- src/meshlabplugins/io_gltf/io_gltf.cpp
+++ src/meshlabplugins/io_gltf/io_gltf.cpp
@@ -34,8 +34,8 @@
 //function declaration
 void loadMesh(
 		MeshModel& m,
-		const tinygltf::Model& model,
-		const tinygltf::Primitive& p);
+		const tinygltf::Mesh& tm,
+		const tinygltf::Model& model);
 
 QString IOglTFPlugin::pluginName() const
 {
@@ -89,10 +89,8 @@ void IOglTFPlugin::open(
 		if (!warn.empty())
 			reportWarning(QString::fromStdString(warn));
 
-		const tinygltf::Mesh& tm = model.meshes[0];
-		for (const tinygltf::Primitive& p : tm.primitives){
-			loadMesh(m, model, p);
-		}
+		for (const tinygltf::Mesh& tm : model.meshes)
+			loadMesh(m, tm, model);
 
 		//todo remove this
 		//throw MLException("gltf still not supported");
@@ -132,86 +130,147 @@ void populateVertices(
 }
 
 template <typename Scalar>
+void populateVNormals(
+		const std::vector<CMeshO::VertexPointer>& ivp,
+		const Scalar* normArray,
+		unsigned int vertNumber)
+{
+	for (unsigned int i = 0; i < vertNumber*3; i+= 3){
+		ivp[i/3]->N() = CMeshO::CoordType(normArray[i], normArray[i+1], normArray[i+2]);
+	}
+}
+
+template <typename Scalar>
 void populateTriangles(
 		MeshModel&m,
 		const std::vector<CMeshO::VertexPointer>& ivp,
 		const Scalar* triArray,
 		unsigned int triNumber)
 {
-	CMeshO::FaceIterator fi =
-			vcg::tri::Allocator<CMeshO>::AddFaces(m.cm, triNumber);
-	for (unsigned int i = 0; i < triNumber*3; i+=3, ++fi) {
-		fi->V(0) = ivp[triArray[i]];
-		fi->V(1) = ivp[triArray[i+1]];
-		fi->V(2) = ivp[triArray[i+2]];
+	if (triArray != nullptr) {
+		CMeshO::FaceIterator fi =
+				vcg::tri::Allocator<CMeshO>::AddFaces(m.cm, triNumber);
+		for (unsigned int i = 0; i < triNumber*3; i+=3, ++fi) {
+			fi->V(0) = ivp[triArray[i]];
+			fi->V(1) = ivp[triArray[i+1]];
+			fi->V(2) = ivp[triArray[i+2]];
+		}
+	}
+	else {
+		CMeshO::FaceIterator fi =
+				vcg::tri::Allocator<CMeshO>::AddFaces(m.cm, ivp.size()/3);
+		for (unsigned int i = 0; i < ivp.size(); i+=3, ++fi) {
+			fi->V(0) = ivp[i];
+			fi->V(1) = ivp[i+1];
+			fi->V(2) = ivp[i+2];
+		}
 	}
 }
 
+enum GLTF_ATTR_TYPE {POSITION, NORMAL, TEXCOORD_0, INDICES};
+const std::array<std::string, 4> GLTF_ATTR_STR {"POSITION", "NORMAL", "TEXCOORD_0"};
+
+template <typename Scalar>
+void populateAttr(
+		GLTF_ATTR_TYPE attr,
+		MeshModel&m,
+		std::vector<CMeshO::VertexPointer>& ivp,
+		const Scalar* array,
+		unsigned int number)
+{
+	switch (attr) {
+	case POSITION:
+		populateVertices(m, ivp, array, number); break;
+	case NORMAL:
+		populateVNormals(ivp, array, number); break;
+	case TEXCOORD_0:
+		break;
+	case INDICES:
+		populateTriangles(m, ivp, array, number/3); break;
+	}
+}
+
+void loadAttribute(
+		MeshModel& m,
+		std::vector<CMeshO::VertexPointer>& ivp,
+		const tinygltf::Model& model,
+		const tinygltf::Primitive& p,
+		GLTF_ATTR_TYPE attr)
+{
+	const tinygltf::Accessor* accessor = nullptr;
+
+	if (attr != INDICES) {
+		auto it = p.attributes.find(GLTF_ATTR_STR[attr]);
+
+		if (it != p.attributes.end()) {
+			accessor = &model.accessors[it->second];
+		}
+		else if (attr == POSITION) {
+			throw MLException("File has not 'Position' attribute");
+		}
+	}
+	else {
+		if (p.mode == 4 && p.indices >= 0 &&
+				(unsigned int)p.indices < model.accessors.size()) {
+			accessor = &model.accessors[p.indices];
+		}
+	}
+
+	if (accessor) {
+
+		const tinygltf::BufferView& posbw = model.bufferViews[accessor->bufferView];
+		const std::vector<unsigned char>& posdata = model.buffers[posbw.buffer].data;
+		unsigned int posOffset = posbw.byteOffset + accessor->byteOffset;
+
+
+		if (accessor->componentType == TINYGLTF_COMPONENT_TYPE_FLOAT) {
+			const float* posArray = (const float*) (posdata.data() + posOffset);
+			populateAttr(attr, m, ivp, posArray, accessor->count);
+		}
+		else if (accessor->componentType == TINYGLTF_COMPONENT_TYPE_DOUBLE) {
+			const double* posArray = (const double*) (posdata.data() + posOffset);
+			populateAttr(attr, m, ivp, posArray, accessor->count);
+		}
+		else if (accessor->componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE) {
+			const unsigned char* triArray = (const unsigned char*) (posdata.data() + posOffset);
+			populateAttr(attr, m, ivp, triArray, accessor->count);
+		}
+		else if (accessor->componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT) {
+			const unsigned short* triArray = (const unsigned short*) (posdata.data() + posOffset);
+			populateAttr(attr, m, ivp, triArray, accessor->count);
+		}
+		else if (accessor->componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT) {
+			const unsigned int* triArray = (const unsigned int*) (posdata.data() + posOffset);
+			populateAttr(attr, m, ivp, triArray, accessor->count);
+		}
+	}
+	else if (attr == INDICES) {
+		populateAttr<unsigned char>(attr, m, ivp, nullptr, 0);
+	}
+
+}
 
 //import
-void loadMesh(
+void loadMeshPrimitive(
 		MeshModel& m,
 		const tinygltf::Model& model,
 		const tinygltf::Primitive& p)
 {
-	const auto it = p.attributes.find("POSITION");
-
-	if (it == p.attributes.end())
-		throw MLException("File has not 'Position' attribute");
-	int positionAccessorID = it->second;
-
-	const tinygltf::Accessor& posAccessor = model.accessors[positionAccessorID];
-
-	if (posAccessor.type != TINYGLTF_TYPE_VEC3)
-		throw MLException("File positions are not 3D coordinates!");
-
-
-	const tinygltf::BufferView& posbw = model.bufferViews[posAccessor.bufferView];
-	const std::vector<unsigned char>& posdata = model.buffers[posbw.buffer].data;
-	unsigned int posOffset = posbw.byteOffset + posAccessor.byteOffset;
 	std::vector<CMeshO::VertexPointer> ivp;
+	loadAttribute(m, ivp, model, p, POSITION);
+	loadAttribute(m, ivp, model, p, NORMAL);
+	loadAttribute(m, ivp, model, p, INDICES);
+}
 
-	if (posAccessor.componentType == TINYGLTF_COMPONENT_TYPE_FLOAT) {
-		const float * posArray = (const float*) (posdata.data() + posOffset);
-		populateVertices(m, ivp, posArray, posAccessor.count);
-
-	}
-	else if (posAccessor.componentType == TINYGLTF_COMPONENT_TYPE_DOUBLE) {
-		const double * posArray = (const double*) (posdata.data() + posOffset);
-		populateVertices(m, ivp, posArray, posAccessor.count);
-	}
-	else {
-		throw MLException("Coord type not supported");
-	}
-
-	//triangles
-	if (p.mode == 4 && p.indices >= 0 &&
-			(unsigned int)p.indices < model.accessors.size()) {
-
-		const tinygltf::Accessor& triAccessor = model.accessors[p.indices];
-
-		if (triAccessor.type != TINYGLTF_TYPE_SCALAR)
-			throw MLException("Primitive indices must be scalars!");
-
-		const tinygltf::BufferView tribw = model.bufferViews[triAccessor.bufferView];
-		const std::vector<unsigned char>& tridata = model.buffers[tribw.buffer].data;
-		unsigned int triOffset = tribw.byteOffset;
-
-		if (triAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE) {
-			const unsigned char* triArray = (const unsigned char*) (tridata.data() + triOffset);
-			populateTriangles(m, ivp, triArray, triAccessor.count/3);
-		}
-		else if (triAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT) {
-			const unsigned short* triArray = (const unsigned short*) (tridata.data() + triOffset);
-			populateTriangles(m, ivp, triArray, triAccessor.count/3);
-		}
-		else if (triAccessor.componentType == TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT) {
-			const unsigned int* triArray = (const unsigned int*) (tridata.data() + triOffset);
-			populateTriangles(m, ivp, triArray, triAccessor.count/3);
-		}
-		else {
-			throw MLException("Malformed GLTF: wrong primitive indices component type.");
-		}
+void loadMesh(
+		MeshModel& m,
+		const tinygltf::Mesh& tm,
+		const tinygltf::Model& model)
+{
+	if (!tm.name.empty())
+		m.setLabel(QString::fromStdString(tm.name));
+	for (const tinygltf::Primitive& p : tm.primitives){
+		loadMeshPrimitive(m, model, p);
 	}
 }
 
