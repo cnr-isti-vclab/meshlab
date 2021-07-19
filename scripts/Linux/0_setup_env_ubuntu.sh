fddd83950ffd86199a1afab846c246084532b7c2
alemuntoni <muntoni.alessandro@gmail.com>
2021-07-19 16:41:40 +0200

    add required libraries on setup scripts

--- scripts/Linux/0_setup_env_ubuntu.sh
+++ scripts/Linux/0_setup_env_ubuntu.sh
@@ -7,4 +7,4 @@
 sudo apt-get update 
 sudo apt-get install -y qt5-default qttools5-dev-tools qtdeclarative5-dev
 sudo apt-get install -y mesa-common-dev libglu1-mesa-dev libglew-dev lib3ds-dev libeigen3-dev 
-sudo apt-get install -y libopenctm-dev libgmp-dev libqhull-dev patchelf cmake
+sudo apt-get install -y libopenctm-dev libgmp-dev libqhull-dev libcgal-dev libboost-all-dev patchelf cmake
