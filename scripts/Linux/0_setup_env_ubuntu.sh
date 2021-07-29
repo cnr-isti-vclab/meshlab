3327cebb9bfa57584f62daff9f2ed2ed1da6e539
alemuntoni <muntoni.alessandro@gmail.com>
2021-07-29 14:34:08 +0200

    using bundled xerces

--- scripts/Linux/0_setup_env_ubuntu.sh
+++ scripts/Linux/0_setup_env_ubuntu.sh
@@ -4,7 +4,7 @@
 #
 # Run this script if you never installed any of the MeshLab dependencies.
 
-sudo apt-get update 
+sudo apt-get update
 sudo apt-get install -y qt5-default qttools5-dev-tools qtdeclarative5-dev
-sudo apt-get install -y mesa-common-dev libglu1-mesa-dev libglew-dev lib3ds-dev libeigen3-dev 
-sudo apt-get install -y libopenctm-dev libgmp-dev libqhull-dev libcgal-dev libboost-all-dev patchelf cmake
+sudo apt-get install -y mesa-common-dev libglu1-mesa-dev
+sudo apt-get install -y libgmp-devlibcgal-dev libboost-all-dev patchelf cmake
