539725cdfdf99949169098511586e27996bbf8c3
Alessandro Muntoni <muntoni.alessandro@gmail.com>
2021-08-18 11:51:30 +0200

    deploy libio_e57 on linux

--- scripts/Linux/2_deploy.sh
+++ scripts/Linux/2_deploy.sh
@@ -35,6 +35,7 @@ $SCRIPTS_PATH/resources/linuxdeployqt $INSTALL_PATH/usr/share/applications/meshl
   -executable=$INSTALL_PATH/usr/lib/meshlab/plugins/libfilter_sketchfab.so \
   -executable=$INSTALL_PATH/usr/lib/meshlab/plugins/libio_3ds.so \
   -executable=$INSTALL_PATH/usr/lib/meshlab/plugins/libio_ctm.so \
+  -executable=$INSTALL_PATH/usr/lib/meshlab/plugins/libio_e57.so \
   -executable=$INSTALL_PATH/usr/lib/meshlab/plugins/libfilter_mesh_booleans.so
 
 
