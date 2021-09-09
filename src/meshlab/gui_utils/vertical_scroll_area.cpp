5c80061c7dafd47c10e8d990a4d4045b9f4851c0
alemuntoni <muntoni.alessandro@gmail.com>
2021-09-09 14:40:35 +0200

    copyright and refactor

--- src/meshlab/gui_utils/vertical_scroll_area.cpp
+++ src/meshlab/gui_utils/vertical_scroll_area.cpp
@@ -1,3 +1,25 @@
+/****************************************************************************
+ * VCGLib                                                            o o     *
+ * Visual and Computer Graphics Library                            o     o   *
+ *                                                                _   O  _   *
+ * Copyright(C) 2004-2021                                           \/)\/    *
+ * Visual Computing Lab                                            /\/|      *
+ * ISTI - Italian National Research Council                           |      *
+ *                                                                    \      *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+ * for more details.                                                         *
+ *                                                                           *
+ ****************************************************************************/
 #include "vertical_scroll_area.h"
 
 #include <QEvent>
@@ -6,18 +28,17 @@
 
 #include "vertical_scroll_area.h"
 
-VerticalScrollArea::VerticalScrollArea(QWidget *parent) :
-	QScrollArea(parent)
+VerticalScrollArea::VerticalScrollArea(QWidget* parent) : QScrollArea(parent)
 {
 	setWidgetResizable(true);
 	setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
 }
 
-bool VerticalScrollArea::eventFilter(QObject *o, QEvent *e)
+bool VerticalScrollArea::eventFilter(QObject* o, QEvent* e)
 {
 	// This works because QScrollArea::setWidget installs an eventFilter on the widget
-	if(o && o == widget() && e->type() == QEvent::Resize)
+	if (o && o == widget() && e->type() == QEvent::Resize)
 		setMinimumWidth(widget()->minimumSizeHint().width() + verticalScrollBar()->width());
 
 	return QScrollArea::eventFilter(o, e);
