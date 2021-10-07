bdb382459046482f5ce858de2df5d68305096a03
alemuntoni <muntoni.alessandro@gmail.com>
2021-10-07 14:52:48 +0200

    refactor directional geom preserv filter, and add two more filters to add custom point attributes

--- src/meshlabplugins/filter_unsharp/filter_unsharp.h
+++ src/meshlabplugins/filter_unsharp/filter_unsharp.h
@@ -1,31 +1,30 @@
-/****************************************************************************
-* MeshLab                                                           o o     *
-* A versatile mesh processing toolbox                             o     o   *
-*                                                                _   O  _   *
-* Copyright(C) 2007                                                \/)\/    *
-* Visual Computing Lab                                            /\/|      *
-* ISTI - Italian National Research Council                           |      *
-*                                                                    \      *
-* All rights reserved.                                                      *
-*                                                                           *
-* This program is free software; you can redistribute it and/or modify      *   
-* it under the terms of the GNU General Public License as published by      *
-* the Free Software Foundation; either version 2 of the License, or         *
-* (at your option) any later version.                                       *
-*                                                                           *
-* This program is distributed in the hope that it will be useful,           *
-* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
-* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
-* for more details.                                                         *
-*                                                                           *
-****************************************************************************/
-#ifndef FilterUnsharp_PLUGIN_H
-#define FilterUnsharp_PLUGIN_H
+/*****************************************************************************
+ * MeshLab                                                           o o     *
+ * A versatile mesh processing toolbox                             o     o   *
+ *                                                                _   O  _   *
+ * Copyright(C) 2007-2021                                           \/)\/    *
+ * Visual Computing Lab                                            /\/|      *
+ * ISTI - Italian National Research Council                           |      *
+ *                                                                    \      *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+ * for more details.                                                         *
+ *                                                                           *
+ ****************************************************************************/
+#ifndef FILTER_UNSHARP_PLUGIN_H
+#define FILTER_UNSHARP_PLUGIN_H
 
 #include <QObject>
 #include <common/plugins/interfaces/filter_plugin.h>
-
 
 class FilterUnsharp : public QObject, public FilterPlugin
 {
@@ -34,14 +33,11 @@ class FilterUnsharp : public QObject, public FilterPlugin
 	Q_INTERFACES(FilterPlugin)
 
 public:
-	/* naming convention :
-		 - FP -> Filter Plugin
-		 - name of the plugin separated by _
-	*/
 	enum {
 		FP_CREASE_CUT,
 		FP_LAPLACIAN_SMOOTH,
-		FP_DIRECTIONAL_PRESERVATION,
+		//FP_DIRECTIONAL_PRESERVATION_STORE,
+		FP_DIRECTIONAL_PRESERVATION_BLEND,
 		FP_DEPTH_SMOOTH,
 		FP_HC_LAPLACIAN_SMOOTH,
 		FP_SD_LAPLACIAN_SMOOTH,
@@ -61,29 +57,26 @@ public:
 		FP_RECOMPUTE_QUADFACE_NORMAL,
 		FP_LINEAR_MORPH,
 		FP_SCALAR_HARMONIC_FIELD
-	} ;
-	
-	/* default values for standard parameters' values of the plugin actions */
+	};
+
 	FilterUnsharp();
 	~FilterUnsharp();
 
-	QString pluginName() const;
-	QString filterName(ActionIDType filter) const;
-	QString filterInfo(ActionIDType filter) const;
-	FilterClass getClass(const QAction*) const;
-	int getRequirements(const QAction*);
+	QString                         pluginName() const;
+	QString                         filterName(ActionIDType filter) const;
+	QString                         filterInfo(ActionIDType filter) const;
+	FilterClass                     getClass(const QAction*) const;
+	int                             getRequirements(const QAction*);
 	std::map<std::string, QVariant> applyFilter(
-			const QAction* action,
-			const RichParameterList & parameters,
-			MeshDocument &md,
-			unsigned int& postConditionMask,
-			vcg::CallBackPos * cb);
-	RichParameterList initParameterList(const QAction* action, const MeshDocument &/*m*/);
-	int postCondition(const QAction* ) const;
-	int getPreConditions(const QAction*) const;
-	FilterArity filterArity(const QAction* filter) const;
-
+		const QAction*           action,
+		const RichParameterList& parameters,
+		MeshDocument&            md,
+		unsigned int&            postConditionMask,
+		vcg::CallBackPos*        cb);
+	RichParameterList initParameterList(const QAction* action, const MeshDocument& /*m*/);
+	int               postCondition(const QAction*) const;
+	int               getPreConditions(const QAction*) const;
+	FilterArity       filterArity(const QAction* filter) const;
 };
 
-
-#endif
+#endif // FILTER_UNSHARP_PLUGIN_H
