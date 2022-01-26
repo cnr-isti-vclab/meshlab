9c65b399b40a0de0e718b48866836885471ceff4
Dʳ Pierre-Édouard Cailliau <pierre-edouard@cailliau.nom.fr>
2022-01-26 14:45:44 +0100

    Correct mistake.

--- src/meshlabplugins/io_base/save_project.cpp
+++ src/meshlabplugins/io_base/save_project.cpp
@@ -23,7 +23,7 @@ QDomElement matrix44mToXML(const Matrix44m &m, bool binary, QDomDocument &doc)
 	}
 	else {
 		std::ostringstream matrix;
-		std::copy(std::begin(m.V()), std::end(m.V() + 16u),
+		std::copy(m.V(), m.V() + 16u,
 			std::ostream_iterator<Matrix44m::ScalarType>(matrix, " "));
 			//std::experimental::make_ostream_joiner(matrix, ", ")); // with <experimental/iterator>
 		nd = doc.createTextNode(matrix.str());
