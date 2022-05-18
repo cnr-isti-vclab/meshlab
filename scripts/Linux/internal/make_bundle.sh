fa964a8a18f3321ea8da4145e54c306607155d07
alemuntoni <muntoni.alessandro@gmail.com>
2022-05-18 10:41:02 +0200

    proper icon management on linux

--- scripts/Linux/internal/make_bundle.sh
+++ scripts/Linux/internal/make_bundle.sh
@@ -36,10 +36,36 @@ then
 fi
 
 mkdir -p $INSTALL_PATH/usr/share/doc/meshlab
-mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/512x512/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/256x256@2x/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/256x256/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/48x48@2x/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/48x48/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/32x32@2x/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/32x32/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/24x24@2x/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/24x24/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/22x22@2x/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/22x22/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/16x16@2x/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/16x16/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/8x8@2x/apps/
+mkdir -p $INSTALL_PATH/usr/share/icons/Yaru/8x8/apps/
 
 cp $RESOURCES_PATH/linux/meshlab.desktop $INSTALL_PATH/usr/share/applications/meshlab.desktop
-cp $RESOURCES_PATH/icons/meshlab.png $INSTALL_PATH/usr/share/icons/Yaru/512x512/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab512.png $INSTALL_PATH/usr/share/icons/Yaru/256x256@2x/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab256.png $INSTALL_PATH/usr/share/icons/Yaru/256x256/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab96.png $INSTALL_PATH/usr/share/icons/Yaru/48x48@2x/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab48.png $INSTALL_PATH/usr/share/icons/Yaru/48x48/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab64.png $INSTALL_PATH/usr/share/icons/Yaru/32x32@2x/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab32.png $INSTALL_PATH/usr/share/icons/Yaru/32x32/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab48.png $INSTALL_PATH/usr/share/icons/Yaru/24x24@2x/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab24.png $INSTALL_PATH/usr/share/icons/Yaru/24x24/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab44.png $INSTALL_PATH/usr/share/icons/Yaru/22x22@2x/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab22.png $INSTALL_PATH/usr/share/icons/Yaru/22x22/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab32.png $INSTALL_PATH/usr/share/icons/Yaru/16x16@2x/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab16.png $INSTALL_PATH/usr/share/icons/Yaru/16x16/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab16.png $INSTALL_PATH/usr/share/icons/Yaru/8x8@2x/apps/meshlab.png
+cp $RESOURCES_PATH/icons/meshlab8.png $INSTALL_PATH/usr/share/icons/Yaru/8x8/apps/meshlab.png
 cp $RESOURCES_PATH/LICENSE.txt $INSTALL_PATH/usr/share/doc/meshlab/
 cp $RESOURCES_PATH/privacy.txt $INSTALL_PATH/usr/share/doc/meshlab/
 cp $RESOURCES_PATH/readme.txt $INSTALL_PATH/usr/share/doc/meshlab/
