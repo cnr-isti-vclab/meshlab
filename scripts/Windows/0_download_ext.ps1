ade2491727be5bcc532b9b05000f1345d0b82337
alemuntoni <muntoni.alessandro@gmail.com>
2022-10-24 17:41:25 +0200

    fix try embree on windows

--- scripts/Windows/0_download_ext.ps1
+++ scripts/Windows/0_download_ext.ps1
@@ -22,21 +22,9 @@ Expand-Archive -Path CGAL-5.2.1-win64-auxiliary-libraries-gmp-mpfr.zip -Destinat
 Remove-Item CGAL-5.2.1-win64-auxiliary-libraries-gmp-mpfr.zip
 
 # embree
-Invoke-WebRequest -Uri "https://github.com/embree/embree/archive/refs/tags/v3.13.5.zip" -OutFile "embree-3.13.5.zip"
+Invoke-WebRequest -Uri "https://github.com/embree/embree/releases/download/v3.13.5/embree-3.13.5.x64.vc14.windows.zip" -OutFile "embree-3.13.5.zip"
 Expand-Archive -Path embree-3.13.5.zip -DestinationPath .\
 Remove-Item embree-3.13.5.zip
-
-# ispc
-Invoke-WebRequest -Uri "https://github.com/ispc/ispc/releases/download/v1.18.0/ispc-v1.18.0-windows.zip" -OutFile "ispc-1.18.0.zip"
-Expand-Archive -Path ispc-1.18.0.zip -DestinationPath .\
-Remove-Item ispc-1.18.0.zip
-
-# tbb
-Invoke-WebRequest -Uri "https://github.com/oneapi-src/oneTBB/releases/download/v2021.6.0/oneapi-tbb-2021.6.0-win.zip" -OutFile "tbb-2021.6.0.zip"
-Expand-Archive -Path tbb-2021.6.0.zip -DestinationPath .\embree-3.13.5\
-Rename-Item .\embree-3.13.5\oneapi-tbb-2021.6.0\ tbb
-Remove-Item tbb-2021.6.0.zip
-
 
 #going back to original location
 cd $DIR
--- scripts/Windows/0_download_ext.sh
+++ scripts/Windows/0_download_ext.sh
@@ -6,18 +6,22 @@ EXTERNAL_PATH=$SCRIPTS_PATH/../../src/external
 
 cd $EXTERNAL_PATH
 
+# boost
 wget https://boostorg.jfrog.io/artifactory/main/release/1.75.0/source/boost_1_75_0.zip
 unzip boost_1_75_0.zip
 rm boost_1_75_0.zip
 
+# cgal
 wget https://github.com/CGAL/cgal/releases/download/v5.2.1/CGAL-5.2.1.zip
 unzip CGAL-5.2.1.zip
 rm CGAL-5.2.1.zip
 
+# gmp and mpfr
 wget https://github.com/CGAL/cgal/releases/download/v5.2.1/CGAL-5.2.1-win64-auxiliary-libraries-gmp-mpfr.zip
 unzip -o CGAL-5.2.1-win64-auxiliary-libraries-gmp-mpfr.zip -d CGAL-5.2.1/
 rm CGAL-5.2.1-win64-auxiliary-libraries-gmp-mpfr.zip
 
-wget https://github.com/embree/embree/archive/refs/tags/v3.13.5.zip
-unzip -o embree-3.13.5.zip
-rm embree-3.13.5.zip
+# embree
+wget https://github.com/embree/embree/releases/download/v3.13.5/embree-3.13.5.x64.vc14.windows.zip
+unzip -o embree-3.13.5.x64.vc14.windows.zip
+rm embree-3.13.5.x64.vc14.windows.zip
