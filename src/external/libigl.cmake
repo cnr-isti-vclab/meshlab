0a1b5c25eba7fab9ebf8c0ee68f4f534e1fe545a
alemuntoni <muntoni.alessandro@gmail.com>
2022-10-30 14:43:51 +0100

    remove libigl source and download it using cmake

--- src/external/libigl.cmake
+++ src/external/libigl.cmake
@@ -2,12 +2,17 @@
 # Copyright 2019, 2021, Visual Computing Lab, ISTI - Italian National Research Council
 # SPDX-License-Identifier: BSL-1.0
 
-option(ALLOW_BUNDLED_LIBIGL "Allow use of bundled LIBIGL source" ON)
+option(MESHLAB_ALLOW_DOWNLOAD_SOURCE_LIBIGL "Allow download and use of LibIGL source" ON)
 
-set(LIBIGL_DIR ${CMAKE_CURRENT_LIST_DIR}/libigl-2.3.0)
+if(MESHLAB_ALLOW_DOWNLOAD_SOURCE_LIBIGL)
+	set(LIBIGL_DIR ${CMAKE_CURRENT_LIST_DIR}/libigl-2.4.0)
 
-if(ALLOW_BUNDLED_LIBIGL AND EXISTS "${LIBIGL_DIR}/include/igl/igl_inline.h")
-	message(STATUS "- libigl - using bundled source")
+	if (NOT EXISTS "${LIBIGL_DIR}/include/igl/igl_inline.h")
+		set(LIBIGL_LINK https://github.com/libigl/libigl/archive/refs/tags/v2.4.0.zip)
+		download_and_unzip(${LIBIGL_LINK} ${CMAKE_CURRENT_LIST_DIR} "LibIGL")
+	endif()
+
+	message(STATUS "- libigl - using downloaded source")
 	add_library(external-libigl INTERFACE)
 	target_include_directories(external-libigl INTERFACE "${LIBIGL_DIR}/include/")
 endif()
