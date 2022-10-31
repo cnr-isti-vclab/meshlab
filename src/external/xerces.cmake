0d822e5fc9b715ce9162ea0064ffab3666e40fba
alemuntoni <muntoni.alessandro@gmail.com>
2022-10-31 14:38:51 +0100

    fix xercesc build error

--- src/external/xerces.cmake
+++ src/external/xerces.cmake
@@ -16,26 +16,26 @@ if(MESHLAB_ALLOW_SYSTEM_XERCES AND TARGET XercesC::XercesC)
 	target_link_libraries(external-xerces INTERFACE XercesC::XercesC)
 
 elseif(MESHLAB_ALLOW_DOWNLOAD_SOURCE_XERCES)
+	set(XERCES_C_VER 3.2.4)
+	set(XERCES_C_DIR ${CMAKE_CURRENT_LIST_DIR}/xerces-c-${XERCES_C_VER})
 
-	set(XERCES_DIR ${CMAKE_CURRENT_LIST_DIR}/xerces-c-3.2.4)
-
-	if (NOT EXISTS ${XERCES_DIR}/CMakeLists.txt)
-		set(XERCES_C_LINK https://dlcdn.apache.org//xerces/c/3/sources/xerces-c-3.2.4.zip)
+	if (NOT EXISTS ${XERCES_C_DIR}/CMakeLists.txt)
+		set(XERCES_C_LINK https://dlcdn.apache.org//xerces/c/3/sources/xerces-c-${XERCES_C_VER}.zip)
 		download_and_unzip(${XERCES_C_LINK} ${CMAKE_CURRENT_LIST_DIR} "Xerces-C")
 	endif()
 
 	message(STATUS "- XercesC - using downloaded source")
 
 	set(MESSAGE_QUIET ON)
-	add_subdirectory(${XERCES_DIR} EXCLUDE_FROM_ALL)
+	add_subdirectory(${XERCES_C_DIR} EXCLUDE_FROM_ALL)
 	unset(MESSAGE_QUIET)
 
 	add_library(external-xerces INTERFACE)
 	target_link_libraries(external-xerces INTERFACE xerces-c)
 	target_include_directories(
 		external-xerces INTERFACE
-		${XERCES_DIR}/src
-		${CMAKE_CURRENT_BINARY_DIR}/xerces/src)
+		${XERCES_C_DIR}/src
+		${CMAKE_CURRENT_BINARY_DIR}/xerces-c-${XERCES_C_VER}/src)
 	add_library(XercesC::XercesC ALIAS external-xerces)
 
 endif()
