faf62a2fc1ad12c04aeac9626448539d340276b3
alemuntoni <muntoni.alessandro@gmail.com>
2022-11-03 16:16:39 +0100

    using qhull cmake configuration

--- src/external/qhull.cmake
+++ src/external/qhull.cmake
@@ -13,45 +13,21 @@ if(MESHLAB_ALLOW_SYSTEM_QHULL AND TARGET Qhull::qhull_r)
 	target_link_libraries(external-qhull INTERFACE Qhull::qhull_r)
 elseif(MESHLAB_ALLOW_DOWNLOAD_SOURCE_QHULL)
 	set(QHULL_VER 2020.2)
-	set(QHULL_DIR ${MESHLAB_EXTERNAL_DOWNLOAD_DIR}/qhull-${QHULL_VER}/src)
+	set(QHULL_DIR ${MESHLAB_EXTERNAL_DOWNLOAD_DIR}/qhull-${QHULL_VER})
 
-	if (NOT EXISTS "${QHULL_DIR}/libqhull_r/libqhull_r.h")
+	if (NOT EXISTS "${QHULL_DIR}/src/libqhull_r/libqhull_r.h")
 		set(QHULL_LINK https://github.com/qhull/qhull/archive/refs/tags/${QHULL_VER}.zip)
 		download_and_unzip(${QHULL_LINK} ${MESHLAB_EXTERNAL_DOWNLOAD_DIR} "Qhull")
 	endif()
 
 	message(STATUS "- qhull - using downloaded source")
-	add_library(
-		external-qhull STATIC
-		"${QHULL_DIR}/libqhull_r/geom2_r.c"
-		"${QHULL_DIR}/libqhull_r/io_r.c"
-		"${QHULL_DIR}/libqhull_r/io_r.h"
-		"${QHULL_DIR}/libqhull_r/mem_r.c"
-		"${QHULL_DIR}/libqhull_r/mem_r.h"
-		"${QHULL_DIR}/libqhull_r/merge_r.c"
-		"${QHULL_DIR}/libqhull_r/merge_r.h"
-		"${QHULL_DIR}/libqhull_r/poly_r.c"
-		"${QHULL_DIR}/libqhull_r/poly_r.h"
-		"${QHULL_DIR}/libqhull_r/poly2_r.c"
-		"${QHULL_DIR}/libqhull_r/libqhull_r.c"
-		"${QHULL_DIR}/libqhull_r/libqhull_r.h"
-		"${QHULL_DIR}/libqhull_r/qset_r.c"
-		"${QHULL_DIR}/libqhull_r/qset_r.h"
-		"${QHULL_DIR}/libqhull_r/stat_r.c"
-		"${QHULL_DIR}/libqhull_r/stat_r.h"
-		"${QHULL_DIR}/libqhull_r/geom_r.c"
-		"${QHULL_DIR}/libqhull_r/geom_r.h"
-		"${QHULL_DIR}/libqhull_r/user_r.c"
-		"${QHULL_DIR}/libqhull_r/user_r.h"
-		"${QHULL_DIR}/libqhull_r/qhull_ra.h"
-		"${QHULL_DIR}/libqhull_r/global_r.c"
-		"${QHULL_DIR}/libqhull_r/random_r.h"
-		"${QHULL_DIR}/libqhull_r/random_r.c"
-		"${QHULL_DIR}/libqhull_r/usermem_r.c"
-		"${QHULL_DIR}/libqhull_r/userprintf_r.c"
-		"${QHULL_DIR}/libqhull_r/rboxlib_r.c"
-		"${QHULL_DIR}/libqhull_r/userprintf_rbox_r.c"
-		)
-	target_include_directories(external-qhull INTERFACE "${QHULL_DIR}")
-	target_link_libraries(external-qhull PRIVATE external-disable-warnings)
+
+	set(MESSAGE_QUIET ON)
+	add_subdirectory(${QHULL_DIR} EXCLUDE_FROM_ALL)
+	unset(MESSAGE_QUIET)
+
+	add_library(external-qhull INTERFACE)
+	target_link_libraries(external-qhull INTERFACE qhull_r)
+	target_include_directories(external-qhull INTERFACE "${QHULL_DIR}/src")
+	install(TARGETS qhull_r DESTINATION ${MESHLAB_LIB_INSTALL_DIR})
 endif()
