5e215fd74bb36b21e39a6eb0cfb98363830018d4
alemuntoni <muntoni.alessandro@gmail.com>
2022-11-03 15:48:48 +0100

    remove qhull source and download it using cmake

--- src/external/qhull-2020.2/src/qvoronoi/qvoronoi.pro
+++
@@ -1,9 +0,0 @@
-# -------------------------------------------------
-# qvoronoi.pro -- Qt project file for qvoronoi.exe
-# -------------------------------------------------
-
-include(../qhull-app-c.pri)
-
-TARGET = qvoronoi
-
-SOURCES += qvoronoi.c
--- src/external/qhull.cmake
+++ src/external/qhull.cmake
@@ -2,19 +2,25 @@
 # Copyright 2019, 2020, Visual Computing Lab, ISTI - Italian National Research Council
 # SPDX-License-Identifier: BSL-1.0
 
-option(ALLOW_BUNDLED_QHULL "Allow use of bundled Qhull source" ON)
-option(ALLOW_SYSTEM_QHULL "Allow use of system-provided QHull" ON)
+option(MESHLAB_ALLOW_DOWNLOAD_SOURCE_QHULL "Allow download and use of Qhull source" ON)
+option(MESHLAB_ALLOW_SYSTEM_QHULL "Allow use of system-provided QHull" ON)
 
 find_package(Qhull 8 COMPONENTS qhull_r CONFIG)
-set(QHULL_DIR ${CMAKE_CURRENT_LIST_DIR}/qhull-2020.2/src)
 
-if(ALLOW_SYSTEM_QHULL AND TARGET Qhull::qhull_r)
+if(MESHLAB_ALLOW_SYSTEM_QHULL AND TARGET Qhull::qhull_r)
 	message(STATUS "- qhull - using system-provided library")
 	add_library(external-qhull INTERFACE)
 	target_link_libraries(external-qhull INTERFACE Qhull::qhull_r)
-	target_compile_definitions(external-qhull INTERFACE SYSTEM_QHULL)
-elseif(ALLOW_BUNDLED_QHULL AND EXISTS "${QHULL_DIR}/libqhull_r/libqhull_r.h")
-	message(STATUS "- qhull - using bundled source")
+elseif(MESHLAB_ALLOW_DOWNLOAD_SOURCE_QHULL)
+	set(QHULL_VER 2020.2)
+	set(QHULL_DIR ${MESHLAB_EXTERNAL_DOWNLOAD_DIR}/qhull-${QHULL_VER}/src)
+
+	if (NOT EXISTS "${QHULL_DIR}/libqhull_r/libqhull_r.h")
+		set(QHULL_LINK https://github.com/qhull/qhull/archive/refs/tags/${QHULL_VER}.zip)
+		download_and_unzip(${QHULL_LINK} ${MESHLAB_EXTERNAL_DOWNLOAD_DIR} "Qhull")
+	endif()
+
+	message(STATUS "- qhull - using downloaded source")
 	add_library(
 		external-qhull STATIC
 		"${QHULL_DIR}/libqhull_r/geom2_r.c"
@@ -47,6 +53,5 @@ elseif(ALLOW_BUNDLED_QHULL AND EXISTS "${QHULL_DIR}/libqhull_r/libqhull_r.h")
 		"${QHULL_DIR}/libqhull_r/userprintf_rbox_r.c"
 		)
 	target_include_directories(external-qhull INTERFACE "${QHULL_DIR}")
-	set_property(TARGET external-qhull PROPERTY FOLDER External)
 	target_link_libraries(external-qhull PRIVATE external-disable-warnings)
 endif()
