4a1eefa66ce5f8616cb067975286b2a71be8983d
lvk88 <15655519+lvk88@users.noreply.github.com>
2023-01-14 20:26:59 +0100

    Issue #1032: Add 3mf reader first concept

--- src/meshlabplugins/io_3mf/io_3mf.cpp
+++ src/meshlabplugins/io_3mf/io_3mf.cpp
@@ -23,6 +23,11 @@
 
 #include "io_3mf.h"
 
+#include "common/ml_document/mesh_model.h"
+#include "wrap/io_trimesh/io_mask.h"
+
+#include "lib3mf_implicit.hpp"
+
 Lib3MFPlugin::Lib3MFPlugin()
 {
 }
@@ -34,12 +39,12 @@ QString Lib3MFPlugin::pluginName() const
 
 std::list<FileFormat> Lib3MFPlugin::importFormats() const
 {
-  return{FileFormat{"3MF", tr("3MF")}};
+  return{FileFormat{"3MF File Format", tr("3MF")}};
 }
 
 std::list<FileFormat> Lib3MFPlugin::exportFormats() const
 {
-  return{FileFormat{"3MF", tr("3MF")}};
+  return{FileFormat{"3MF File Format", tr("3MF")}};
 }
 
 void Lib3MFPlugin::open(
@@ -50,6 +55,79 @@ void Lib3MFPlugin::open(
                         const RichParameterList& par,
                         vcg::CallBackPos* cb)
 {
+
+  using namespace vcg::tri::io;
+
+  const QString errorMsgFormat = "Error encountered while loading file:\n\"%1\"\n\nError details: %2";
+
+  if( !QFile::exists( fileName ) )
+  {
+    throw MLException(errorMsgFormat.arg(fileName, "File does not exist"));
+  }
+
+  if( cb != nullptr)
+  {
+    (*cb)(0, "Loading...");
+  }
+
+  mask = Mask::IOM_VERTCOORD | Mask::IOM_FACEINDEX;
+
+
+  const auto& wrapper = Lib3MF::CWrapper::loadLibrary();
+  if( wrapper == nullptr )
+  {
+    throw MLException(errorMsgFormat.arg(fileName, "Failed to initialize 3MF library"));
+  }
+
+  const auto& model = wrapper->CreateModel();
+  if( model == nullptr )
+  {
+    throw MLException(errorMsgFormat.arg(fileName, "Failed to create 3MF internal model"));
+  }
+
+  const auto& reader = model->QueryReader("3mf");
+  if( model == nullptr )
+  {
+    throw MLException(errorMsgFormat.arg(fileName, "Failed to create 3MF reader object"));
+  }
+
+  reader->ReadFromFile(fileName.toStdString());
+
+  const auto& object_iterator = model->GetMeshObjects();
+  if( object_iterator == nullptr )
+  {
+    throw MLException(errorMsgFormat.arg(fileName, "Failed to iterate over objects in file"));
+  }
+
+  while(object_iterator->MoveNext())
+  {
+    const auto& mesh_object = object_iterator->GetCurrentMeshObject();
+    const auto n_vertices  = mesh_object->GetVertexCount();
+    const auto n_triangles = mesh_object->GetTriangleCount();
+    auto vertex_iterator = vcg::tri::Allocator<decltype(m.cm)>::AddVertices(m.cm, n_vertices);
+    auto face_iterator = vcg::tri::Allocator<decltype(m.cm)>::AddFaces(m.cm, n_triangles);
+    m.cm.face.reserve(n_triangles);
+    for(size_t i = 0; i < n_vertices; ++i)
+    {
+      const auto& pos = mesh_object->GetVertex(i).m_Coordinates;
+      (*vertex_iterator).P()[0] = pos[0];
+      (*vertex_iterator).P()[1] = pos[1];
+      (*vertex_iterator).P()[2] = pos[2];
+      ++vertex_iterator;
+    }
+    for(size_t i = 0; i < n_triangles; ++i)
+    {
+      const auto& tri = mesh_object->GetTriangle(i).m_Indices;
+      (*face_iterator).V(0) = &m.cm.vert[tri[0]];
+      (*face_iterator).V(1) = &m.cm.vert[tri[1]];
+      (*face_iterator).V(2) = &m.cm.vert[tri[2]];
+      ++face_iterator;
+    }
+  }
+
+  m.enable(mask);
+
+  return;
 }
 
 void Lib3MFPlugin::save(
