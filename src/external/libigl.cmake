c6662d486c1f8e5c9c89389abd630d8218b0fcb9
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-09-16 15:42:59 +0200

    CMake: MESHLAB_ALLOW_SYSTEM_LIBIGL

--- src/external/libigl.cmake
+++ src/external/libigl.cmake
@@ -3,10 +3,17 @@
 # SPDX-License-Identifier: BSL-1.0
 
 option(MESHLAB_ALLOW_DOWNLOAD_SOURCE_LIBIGL "Allow download and use of LibIGL source" ON)
+option(MESHLAB_ALLOW_SYSTEM_LIBIGL "Allow use of system-provided LibIGL" ON)
 
 set(LIBIGL_VER 2.4.0)
 
-if(MESHLAB_ALLOW_DOWNLOAD_SOURCE_LIBIGL)
+find_path(libigl_INCLUDE_DIR NAMES igl_inline.h PATH_SUFFIXES igl PATHS ${libigl_PREFIX})
+
+if(MESHLAB_ALLOW_SYSTEM_LIBIGL AND libigl_INCLUDE_DIR)
+	message(STATUS "- LibIGL - using system-provided library")
+	add_library(external-libigl INTERFACE)
+	target_include_directories(external-libigl INTERFACE ${libigl_INCLUDE_DIR})
+elseif(MESHLAB_ALLOW_DOWNLOAD_SOURCE_LIBIGL)
 	set(LIBIGL_DIR "${MESHLAB_EXTERNAL_DOWNLOAD_DIR}/libigl-${LIBIGL_VER}")
 	set(LIBIGL_CHECK "${LIBIGL_DIR}/include/igl/igl_inline.h")
 
